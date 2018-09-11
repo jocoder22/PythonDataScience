a9c947705c8705c8e8ab0db2946f572fb1c99a6b
Okigbo <okigbookey@gmail.com>
2018-09-11 14:44:06 -0400

    Convert np array dtype

--- numpyArrays.py
+++ numpyArrays.py
@@ -4,3 +4,9 @@ import numpy as np
 
 Vones = np.ones((4), dtype=np.int8)
 Vones2 = np.ones((4, 6), dtype=np.int8)
+
+
+# converting dtype
+Vones3 = np.ones((9))
+Vones3.dtype  # dtype('float64')
+Vones3 = np.array(Vones3, dtype=np.int8)
