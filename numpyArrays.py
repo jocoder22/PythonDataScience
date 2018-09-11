43e65e93eefe738fff54ede25338c031ba7b43aa
Okigbo <okigbookey@gmail.com>
2018-09-11 14:53:54 -0400

    Create np array matrices

--- numpyArrays.py
+++ numpyArrays.py
@@ -10,3 +10,9 @@ Vones2 = np.ones((4, 6), dtype=np.int8)
 Vones3 = np.ones((9))
 Vones3.dtype  # dtype('float64')
 Vones3 = np.array(Vones3, dtype=np.int8)
+
+
+# Create matrix of random integers
+mat1 = np.random.randn(5, 5)
+mat2 = np.random.randn(2, 3, 2)
+
