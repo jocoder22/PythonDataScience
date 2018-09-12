ed8f321e782d58ccc7b3519a413c461099963427
Okigbo <okigbookey@gmail.com>
2018-09-12 15:41:33 -0400

    Add using indexing to subset array

--- AdvancedArray.py
+++ AdvancedArray.py
@@ -4,9 +4,16 @@ adlist = [[['January', 'February', 'March'],
           ['April', 'May', 'June'],
           ['July', 'August', 'September'],
           ['October', 'November', 'December']],
-          [['School', 'Church', 'Market'],
+          [['School', 'Cat', 'Market'],
            ['Pig', 'Dog', 'Cat'],
            ['Rain', 'Wind', 'Fire'],
-           ['Books', 'Pens', 'Papers']]]
+           ['Books', 'cat', 'Papers']]]
 
 ad = np.array(adlist)
+
+# subsetting np array
+adsub = ad[ad != 'Cat']
+
+adindex = ad != "Cat"
+
+adsub2 = ad[adindex]
