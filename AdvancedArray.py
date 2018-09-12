62104e47f1205d98ab829310f2d49e55bc11c0da
Okigbo <okigbookey@gmail.com>
2018-09-12 15:38:27 -0400

    Add new np array

---
+++ AdvancedArray.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+adlist = [[['January', 'February', 'March'],
+          ['April', 'May', 'June'],
+          ['July', 'August', 'September'],
+          ['October', 'November', 'December']],
+          [['School', 'Church', 'Market'],
+           ['Pig', 'Dog', 'Cat'],
+           ['Rain', 'Wind', 'Fire'],
+           ['Books', 'Pens', 'Papers']]]
+
+ad = np.array(adlist)
