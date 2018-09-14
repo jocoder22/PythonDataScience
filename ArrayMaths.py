54bb729c3adef8f5d7babc9b3bbf8038f0f141b8
Okigbo <okigbookey@gmail.com>
2018-09-14 15:27:12 -0400

    Update

--- ArrayMaths.py
+++ ArrayMaths.py
@@ -1,3 +1,8 @@
-import numpy as numpy
+import numpy as np
 from numpy.random import randn
 import numpy.linalg as ln
+
+
+radN = randn(2, 3, 4)
+arr1 = np.numpy([[3, 5, 7], [7, 9, 12], [5, 2, 5]])
+
