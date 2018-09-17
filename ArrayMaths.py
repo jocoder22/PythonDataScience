1fc325eaef6e5998ca89692232baf61fc556add0
Okigbo <okigbookey@gmail.com>
2018-09-17 11:34:13 -0400

    Update

--- ArrayMaths.py
+++ ArrayMaths.py
@@ -3,7 +3,7 @@ from numpy.random import randn
 import numpy.linalg as ln
 
 
-radN1 = randn(2, 3, 4)
-randN2 = randn(3, 4, 6)
+radN1 = np.arry(randn(2, 3, 4) * 8, dtype=np.int64)
+randN2 = np.array(randn(3, 4, 6) * 9, dtype=np.int64)
 arr1 = np.numpy([[3, 5, 7], [7, 9, 12], [5, 2, 5]])
 arr2 = ln(90)
