7b5618b0791b8433b88ca387032eb203a2f8d190
Okigbo <okigbookey@gmail.com>
2018-09-17 11:45:16 -0400

    Update

--- ArrayMaths.py
+++ ArrayMaths.py
@@ -4,6 +4,13 @@ import numpy.linalg as ln
 
 
 radN1 = np.arry(randn(2, 3, 4) * 8, dtype=np.int64)
-randN2 = np.array(randn(3, 4, 6) * 9, dtype=np.int64)
+radN2 = np.array(randn(3, 4, 6) * 9, dtype=np.int64)
 arr1 = np.numpy([[3, 5, 7], [7, 9, 12], [5, 2, 5]])
 arr2 = ln(90)
+
+
+# Array maths
+arr3 = radN2 + 10
+arr4 = radN1 / 6
+arr5 = radN2 ** 2
+
