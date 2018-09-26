e1b88dc187c65e9469a27db22cbde72cb3d1fd60
Okigbo <okigbookey@gmail.com>
2018-09-26 12:20:36 -0400

    Add simple array manipulations

--- Arithmetic_LinearAlgebra.py
+++ Arithmetic_LinearAlgebra.py
@@ -2,3 +2,8 @@ import numpy as np
 from numpy.random import randn
 
 myarr1 = np.array(randn(5, 5) * 20, dtype=np.int8)
+print(myarr1)
+
+myarr1.tolist() # turn the arrray into a list
+myarr1.flatten() # turn array into a single list
+
