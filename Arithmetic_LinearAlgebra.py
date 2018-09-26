4260c647e508a25bea87e1e6cb61d0d590308895
Okigbo <okigbookey@gmail.com>
2018-09-26 12:24:47 -0400

    fill array with value

--- Arithmetic_LinearAlgebra.py
+++ Arithmetic_LinearAlgebra.py
@@ -4,6 +4,12 @@ from numpy.random import randn
 myarr1 = np.array(randn(5, 5) * 20, dtype=np.int8)
 print(myarr1)
 
-myarr1.tolist() # turn the arrray into a list
-myarr1.flatten() # turn array into a single list
+myarr1.tolist()  # turn the arrray into a list
+myarr1.flatten()  # turn array into a single list
 
+
+# Create empty array and file it with value;
+EmptyArray = np.empty((3, 4), dtype=np.dtype('<U16'))
+
+# fill array with value;
+EmptyArray.fill('Goodies')
