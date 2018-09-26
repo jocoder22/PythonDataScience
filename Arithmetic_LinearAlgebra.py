834891ce99270a7237a857b5bc7646ca871bff43
Okigbo <okigbookey@gmail.com>
2018-09-26 12:49:33 -0400

    Add cumsum function

--- Arithmetic_LinearAlgebra.py
+++ Arithmetic_LinearAlgebra.py
@@ -19,3 +19,12 @@ EmptyArray.fill('Goodies')
 myarr1.sum()  # sum all the values
 myarr1.sum(axis=0)  # sum along the rows
 myarr1.sum(axis=1)  # sum along the columns
+
+
+# Cumsum function;
+myarr1.fill(100)  # fill myarr1 with 100
+
+myarr1.cumsum(axis=0)  # do running sum along the rows
+myarr1.cumsum(axis=1)  # do running sum along the columns
+
+
