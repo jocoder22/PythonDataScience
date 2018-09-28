354c18d7523f60533a60fdad512af62478092bd6
Okigbo <okigbookey@gmail.com>
2018-09-28 15:09:32 -0400

    Update

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,5 +1,6 @@
 #  *args for passing non-keyword list of argument to a function;
-# this makes the *args iterable;
+# this makes the *args iterable and optional arguments
+# *args can accept list as argument
 
 
 def addall(a, *args):
@@ -9,3 +10,5 @@ def addall(a, *args):
     return total
 
     
+# **kwargs for passing keyword list ( or dictionary) to a function
+# This makes **kwargs iterable, optional and can accept dictionary as argument;
