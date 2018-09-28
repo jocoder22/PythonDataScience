a677ebcc0ba704822d843ea46e78a02a74ff5964
Okigbo <okigbookey@gmail.com>
2018-09-28 15:38:58 -0400

    Update

--- args_kwargs.py
+++ args_kwargs.py
@@ -12,3 +12,18 @@ def addall(a, *args):
     
 # **kwargs for passing keyword list ( or dictionary) to a function
 # This makes **kwargs iterable, optional and can accept dictionary as argument;
+
+def addall2(a, *agrs, **kwargs):
+    total = 0
+    for i in agrs:
+        total += (i ** 3)
+    for key, value in kwargs.items():
+        print("The key is {} and the value is {}".format(key, value))
+    
+    for i in kwargs.keys():
+        print("The key is {}".format(i))
+    
+    print(total)
+    return "The total amonunt is ${}".format(total) 
+
+
