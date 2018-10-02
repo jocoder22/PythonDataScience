3ad0a6313a2c8c78d2cf3002925237485daf1653
Okigbo <okigbookey@gmail.com>
2018-10-02 12:04:00 -0400

    load file using numpy

--- pandas1.py
+++ pandas1.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
 
+schema = np.dtype([('Name', '<U16'),
+                   ('Age',  np.float16),
+                   ('Gender', '<U16'),
+                   ('City', '<U16')])
+data4 = np.loadtxt("/Documents/test1.txt",
+                   skiprows=1, dtype=schema, delimiter=',')
+
