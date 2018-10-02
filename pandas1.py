a4ba86871fea546e50a163968c2887e2f6157db9
Okigbo <okigbookey@gmail.com>
2018-10-02 12:16:41 -0400

    Do numpy slicing

--- pandas1.py
+++ pandas1.py
@@ -5,6 +5,15 @@ schema = np.dtype([('Name', '<U16'),
                    ('Age',  np.float16),
                    ('Gender', '<U16'),
                    ('City', '<U16')])
-data4 = np.loadtxt("/Documents/test1.txt",
-                   skiprows=1, dtype=schema, delimiter=',')
+data = np.loadtxt("/Documents/test1.txt",
+                  skiprows=1, dtype=schema, delimiter=',')
 
+data  # this is a one dimensional numpy array
+data.shape
+type(data)
+
+
+# Slicing;
+data[:5]
+data[:5]['Name']
+data[:5][['Name', 'Age']]
