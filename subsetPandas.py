514b8f6db7faec85a93fa5d721c1676d33ae2aad
Okigbo <okigbookey@gmail.com>
2018-10-05 11:46:55 -0400

    Update

--- subsetPandas.py
+++ subsetPandas.py
@@ -18,21 +18,21 @@ ser100 > 3
 ser200 = Series(['Zero', 'One', 'Two', 'Three', 'Four'],
                 index=[3, 2, 1, 0, 4])
 
-ser200[2:4] 
+ser200[2:4]
 """
 1      Two
 0    Three
 dtype: object
 """
 
-ser200.iloc[2:4]  
+ser200.iloc[2:4]
 """
 1      Two
 0    Three
 dtype: object
 """
 
-ser200.loc[2:4]  
+ser200.loc[2:4]
 """
 2      One
 1      Two
@@ -40,3 +40,18 @@ ser200.loc[2:4]
 4     Four
 dtype: object
 """
+
+
+dframe = DataFrame(np.arange(15).reshape(5, 3),
+                   columns=["Price", "Discount", "Sales"],
+                   index=["AAA", "BBB", "CCC", "DDD", "EEE"])
+
+dframe.Price  # column selection
+dframe.Sales  # column selection
+dframe['Sales']  # column selection
+dframe['Price']  # column selection
+
+dframe[['Price', 'Sales']]
+dframe.iloc[1:3, 1:2]
+
+
