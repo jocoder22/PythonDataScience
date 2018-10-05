6a6168925aadaff7d904b06836d77eb8c4a8f7b1
Okigbo <okigbookey@gmail.com>
2018-10-05 10:59:26 -0400

    Update

--- subsetPandas.py
+++ subsetPandas.py
@@ -2,6 +2,11 @@ import pandas as pd
 from pandas import Series, DataFrame
 import numpy as np
 
-ser100 = Series(np.arange(4),
-                index=['Tv', 'Radio', 'Cellphone', 'Laptop'])
+ser100 = Series(np.arange(6),
+                index=['Tv', 'Radio', 'Cellphone',
+                       'Laptop', 'Monitor', 'Heater'])
 
+# Subsetting;
+ser100[:2]
+ser100[['Radio', 'Monitor']]
+ser100['Radio': 'Monitor']
