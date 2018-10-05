11e3bf23933768169369a596f7f7347856844125
Okigbo <okigbookey@gmail.com>
2018-10-05 11:14:21 -0400

    Update

--- subsetPandas.py
+++ subsetPandas.py
@@ -13,3 +13,30 @@ ser100['Radio': 'Monitor']
 ser100[ser100 > 3]
 ser100 > 3
 
+
+# index selection;
+ser200 = Series(['Zero', 'One', 'Two', 'Three', 'Four'],
+                index=[3, 2, 1, 0, 4])
+
+ser200[2:4] 
+"""
+1      Two
+0    Three
+dtype: object
+"""
+
+ser200.iloc[2:4]  
+"""
+1      Two
+0    Three
+dtype: object
+"""
+
+ser200.loc[2:4]  
+"""
+2      One
+1      Two
+0    Three
+4     Four
+dtype: object
+"""
