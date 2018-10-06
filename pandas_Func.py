140c879b7db6c21e39f4b22c30aa6030d2311065
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-06 13:42:54 -0400

    Check uncommon index

--- pandas_Func.py
+++ pandas_Func.py
@@ -6,3 +6,32 @@ fser100 = Series(np.arange(6))
 fser200 = Series([90, 34, 67, 12, 100, 79])
 fser300 = Series([50, 24, 56, 89, 33],
                  index=[0, 1, 2, 3, 5])
+fser400 = Series(np.arange(5))
+
+print(fser100)
+print(fser200)
+print(fser300)
+
+
+fser100 + fser300
+"""
+0    50.0
+1    25.0
+2    58.0
+3    92.0
+4     NaN
+5    38.0
+dtype: float64
+"""
+
+fser400 + fser300
+"""
+0    50.0
+1    25.0
+2    58.0
+3    92.0
+4     NaN
+5     NaN
+dtype: float64
+"""
+
