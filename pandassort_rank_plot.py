0623a9ff7c004a175bd6480ffca5a2d342b5a98b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-07 23:50:08 -0400

    Update

--- pandassort_rank_plot.py
+++ pandassort_rank_plot.py
@@ -1,3 +1,9 @@
 import numpy as np 
 import pandas as pd 
 from pandas import Series, DataFrame 
+
+
+data = DataFrame(np.round(np.random.randn(7,3) *12),
+                 columns=['Box', 'Pen', 'Books'],
+                 index=list('defcagb'))
+print(data)
