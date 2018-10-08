292e2aa3ad2726cb3a897485867968aa07441f64
Okigbo <okigbookey@gmail.com>
2018-10-08 14:13:37 -0400

    Update

--- pandasMultiindex.py
+++ pandasMultiindex.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
-multiindex = pd.MultiIndex([['AA', 'BB'], ['Male', 'Female'], ['Youth', 'Old']],
+multiindex = pd.MultiIndex([['AA', 'BB'], ['Male', 'Female'], 
+                            ['Youth', 'Old']],
                            [[0, 0, 0, 0, 1, 1, 1, 1],
                            [0, 0, 1, 1, 0, 0, 1, 1],
                            [0, 1, 0, 1, 0, 1, 0, 1]])
