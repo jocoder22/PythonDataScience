6c10b1f4c7e512777f2261295abb30601fb75cba
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:54:33 -0400

    Update

--- gettingData.py
+++ gettingData.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 print(os.getcwd())
 
-os.chdir('C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1')
+os.chdir('C:/Users/.../Code/Section 1')
 data = pd.read_csv('PopPyramids.csv')
 data.head()
 
