fc545c030dc77a8874c549d1a9f7bafc1cec9f94
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:39:24 -0400

    load csv file

--- gettingData.py
+++ gettingData.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 import os
 print(os.getcwd())
 
-filename = "C:\Users\Jose\Documents\PythonDataScience1\Code\Code\Section 1\PopPyramids.csv"
-data = pd.read_csv()
+filename1 = "C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1/PopPyramids.csv"
+data = pd.read_csv(filename1)
+data.head()
