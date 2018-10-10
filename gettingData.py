aceee77fb90107000ed738b2c3b4404443a87da7
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-10 02:03:58 -0400

    Update

--- gettingData.py
+++ gettingData.py
@@ -35,4 +35,15 @@ data3 = pd.read_excel('PopPyramids.xlsx', sheet_name=0)
 # Read html file  -- must install lxml;
 data4 = pd.read_html('PopPyramids.html')  # return a list
 print(data4)
-data[0].head()
+data4[0].head()
+type(data4)  # <class 'list'>
+
+# form dataframe
+data5 = pd.read_html('PopPyramids.html', 
+                    attrs={'id': 'PopData'},
+                    index_col=[1, 2, 3])[0]
+type(data5)  # <class 'pandas.core.frame.DataFrame'>
+data5.drop('Region', axis=1, inplace=True)
+data5.sort_index(inplace=True)
+data5.head()
+
