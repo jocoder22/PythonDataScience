015de9cb07692da305addf4a8c1a2e9062ea7fc7
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:53:00 -0400

    set index columns

--- gettingData.py
+++ gettingData.py
@@ -8,3 +8,7 @@ print(os.getcwd())
 os.chdir('C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1')
 data = pd.read_csv('PopPyramids.csv')
 data.head()
+
+# Setting index columns;
+data = pd.read_csv('PopPyramids.csv',
+                   index_col=['Country', 'Year', 'Age'])
