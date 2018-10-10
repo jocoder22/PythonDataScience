f690e91becd41f13f74c0accada0177a2bb0cca8
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-10 01:09:10 -0400

    read excel file

--- gettingData.py
+++ gettingData.py
@@ -20,6 +20,13 @@ data.drop('Region', axis=1, inplace=True)
 data.sort_index(inplace=True)
 data.head()
 
+
 # Subsetting for United States;
 data.loc[('UnitedStates', 2013), :]
 data.loc[('UnitedStates', 2013), :].head()
+
+
+
+data2 = pd.read_excel('PopPyramids.xlsx', sheet_name='Sheet1')
+# sheet_name starts at zero - zero indexed;
+data3 = pd.read_excel('PopPyramids.xlsx', sheet_name=0)
