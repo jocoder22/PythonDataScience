e9cc72dc458a4f85379d4379dae572b7d72dcf8e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:57:04 -0400

    Drop column and sort data

--- gettingData.py
+++ gettingData.py
@@ -13,3 +13,8 @@ data.head()
 # Setting index columns;
 data = pd.read_csv('PopPyramids.csv',
                    index_col=['Country', 'Year', 'Age'])
+
+# drop column (Region) and sort data in place;
+data.drop('Region', axis=1, inplace=True)
+data.sort_index(inplace=True)
+pop.head()
