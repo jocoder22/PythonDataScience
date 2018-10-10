1bc0f98c0483d6045ffa81a6cf9b05b9cb732693
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:53:52 -0400

    Update

--- gettingData.py
+++ gettingData.py
@@ -9,6 +9,7 @@ os.chdir('C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1')
 data = pd.read_csv('PopPyramids.csv')
 data.head()
 
+
 # Setting index columns;
 data = pd.read_csv('PopPyramids.csv',
                    index_col=['Country', 'Year', 'Age'])
