50dde8259c81b7472daac6f37b6be32249c274bc
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-09 23:45:44 -0400

    change working directory

--- gettingData.py
+++ gettingData.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 import os
 print(os.getcwd())
 
-filename1 = "C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1/PopPyramids.csv"
-data = pd.read_csv(filename1)
+os.chdir('C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1')
+data = pd.read_csv('PopPyramids.csv')
 data.head()
