8d8b8c70a546f98b6554c5e094062cdef18b0274
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-13 14:10:54 -0400

    Update

--- mysqlReview.sql
+++ mysqlReview.sql
@@ -19,6 +19,7 @@ create database House;
 show databases;
 
 -- create new user with password and grant new user all priviledges
-grant all on House.* to 'josh'@'localhost' identified by 'kelly22';
+creat user 'josh'@'localhost' identified by 'kelly22';
+grant all on House.* to 'josh'@'localhost';
 
 
