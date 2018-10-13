6b8120c0e8dc6aa67e5077b9262bdfde7061dee1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-13 13:25:07 -0400

    create new user with priviledge

--- mysqlReview.sql
+++ mysqlReview.sql
@@ -14,9 +14,11 @@ mysql -uroot -ppass23
 
 -- or msql --user=root --password=pass23
 
+-- create new database
 create database House;
 show databases;
 
-
+-- create new user with password and grant new user all priviledges
+grant all on House.* to 'josh'@'localhost' identified by 'kelly22';
 
 
