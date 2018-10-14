a9eff06e2beb553463ec63d733df22ad1a24cc7f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-14 17:49:50 -0400

    Update

--- sqlAlchemyconnect.py
+++ sqlAlchemyconnect.py
@@ -5,3 +5,8 @@ def sql_str_generator(user, pword, host, dbase):
     Return string use for connection
     """
     return "mysql+pymysql://" + user + ":" + pword + "@" + host + "/" + dbase
+
+
+connstr = sql_str_generator("root","pass23","localhost","world")
+conn = create_engine(connstr).connect()
+conn.close()
