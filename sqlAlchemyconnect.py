a15524e451b27794e1fce16543169d6ab11254f7
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-14 15:42:11 -0400

    Update

--- sqlAlchemyconnect.py
+++ sqlAlchemyconnect.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 
-def sqL_str_generator(user, pword, host, dbase):
+def sql_str_generator(user, pword, host, dbase):
     """
     Return string use for connection
     """
