865f072ee032ab516aaa424b89b86d0e3b6f1cf8
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-14 18:05:01 -0400

    Update

--- sqlAlchemyconnect.py
+++ sqlAlchemyconnect.py
@@ -1,4 +1,9 @@
+import os
+import pandas as pd 
+from pandas import DataFrame
 from sqlalchemy import create_engine
+
+os.chdir("C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1")
 
 def sql_str_generator(user, pword, host, dbase):
     """
@@ -10,3 +15,7 @@ def sql_str_generator(user, pword, host, dbase):
 connstr = sql_str_generator("root","pass23","localhost","world")
 conn = create_engine(connstr).connect()
 conn.close()
+
+
+data = pd.read_csv('PopPyramids.csv')
+data.head()
