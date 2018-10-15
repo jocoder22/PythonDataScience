f1d57a969290013621ac4fbc63a36d4b786dd1af
Okigbo <okigbookey@gmail.com>
2018-10-15 17:23:34 -0400

    Add query 1

--- queryData.py
+++ queryData.py
@@ -2,3 +2,19 @@ import pymysql
 from sqlalchemy import create_engine
 from pandas import pd
 from pandas import DataFrame
+
+
+def connector_string(user, passwd, host, dbase):
+    return "mysql+pymysql://" + user + ";" + passwd + "@" + host + "/" + dbase
+
+
+conn = create_engine(connector_string('root',
+                     pssd, 'localhost', 'pop')).connect()
+
+
+# query the database
+pop1_ = pd.read_sql('SELECT * FROM pop  WHERE country ="United States" AND year=2018;',
+                    con=conn,
+                    index_col=["country", "year", "age"])
+
+            
