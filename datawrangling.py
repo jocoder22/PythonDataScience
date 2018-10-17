2c549b487cf808d36d91326797a7de61f6743007
Okigbo <okigbookey@gmail.com>
2018-10-17 15:52:15 -0400

    Convert index to common datetime format

--- datawrangling.py
+++ datawrangling.py
@@ -22,6 +22,11 @@ plt.show()
 # Merging datas - Inner, Outer, Right, Left
 # Pandas use join() method
 # Note the indexes - date - of both dataset are not of the same format
-data1.index   # '2015-01-02', '2015-01-05', '2015-01-06',
-data2.index   #  4/3/2017', '12/30/2016', '9/26/2016', '6/27/2016'
+data1.index   # '2015-01-02', '2015-01-05', '2015-01-06'
+data2.index   #  '4/3/2017', '12/30/2016', '9/26/2016', '6/27/2016'
+
+
+# convert to common datetime format
+pd.to_datetime(data1.index)
+pd.to_datetime(data2.index)
 
