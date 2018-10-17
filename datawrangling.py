0cd7b85f5328ce2ddfbc2a484af88d5155233c3f
Okigbo <okigbookey@gmail.com>
2018-10-17 15:34:19 -0400

    plot data

--- datawrangling.py
+++ datawrangling.py
@@ -6,7 +6,7 @@ from pandas import Series, DataFrame
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 2")
 
 
-data1 = pd.read_cvs("AAPL-Close-15-17.cvs", index_col="date")
+data1 = pd.read_csv("AAPL-Close-15-17.csv", index_col="date")
 data1.head()
 plt.plot(data1)
 plt.show()
