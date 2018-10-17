207e41d43de07f971032f60166dbd077f0b760e6
Okigbo <okigbookey@gmail.com>
2018-10-17 15:30:36 -0400

    Plot data

--- datawrangling.py
+++ datawrangling.py
@@ -1,6 +1,13 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from pandas import Sereies, DataFrame
 
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 2")
+
+
+data1 = pd.read_cvs("AAPL-Close-15-17.cvs", index_col="date")
+data1.head()
+plt.plot(data1)
+plt.show()
 
