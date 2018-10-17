03b4db79c961f15d5317811265e13564e9a28ad9
Okigbo <okigbookey@gmail.com>
2018-10-17 15:42:05 -0400

    Read data2

--- datawrangling.py
+++ datawrangling.py
@@ -6,8 +6,13 @@ from pandas import Series, DataFrame
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 2")
 
 
+# Read data1
 data1 = pd.read_csv("AAPL-Close-15-17.csv", index_col="date")
 data1.head()
 plt.plot(data1)
 plt.show()
+
+
+# Read data2
+data2 = pd.read_csv("AAPL-EPS-15-17.csv", index_col="Date")
 
