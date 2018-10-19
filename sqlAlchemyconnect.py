3dd06bcf6cdd291a4289187fd20e65bd8521f279
Okigbo <okigbookey@gmail.com>
2018-10-19 16:11:15 -0400

    Update

--- sqlAlchemyconnect.py
+++ sqlAlchemyconnect.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from pandas import DataFrame
+# from pandas import DataFrame
 from sqlalchemy import create_engine
 
 os.chdir("C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1")
@@ -13,7 +13,7 @@ def sql_str_generator(user, pword, host, dbase):
     return "mysql+pymysql://" + user + ":" + pword + "@" + host + "/" + dbase
 
 
-connstr = sql_str_generator("root","pass23","localhost","world")
+connstr = sql_str_generator("root", "pass23", "localhost", "world")
 conn = create_engine(connstr).connect()
 
 
@@ -22,7 +22,8 @@ data.head()
 
 
 # change column name to lower case and replace space with underscore
-data.columns = pd.Index(pd.Series(data.columns).map(lambda x: x.lower().replace(" ","_")))
+data.columns = pd.Index(pd.Series(data.columns)
+                        .map(lambda x: x.lower().replace(" ", "_")))
 data.index.rename([n.lower() for n in data.index.names], inplace=True)
 data.head()
 
@@ -43,7 +44,7 @@ make_table = """CREATE TABLE 'populations'(
                 );"""
 
 conn.execute(make_table)
-data.to_sql('population',con=conn, if_exists='append')
+data.to_sql('population', con=conn, if_exists='append')
 
 
 conn.close()
