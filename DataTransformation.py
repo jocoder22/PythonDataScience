ba497a093ec07b3e5177f0508d16bc7c13270bd6
Okigbo <okigbookey@gmail.com>
2018-10-22 15:22:27 -0400

    Binning dataset

--- DataTransformation.py
+++ DataTransformation.py
@@ -15,3 +15,13 @@ pyramids_data.columns.tolist()
 # Drop column region
 pyramids_data.drop("Region", 1, inplace=True)
 pyramids_data.head()
+
+# select two columns and change index
+pyramids_data = pyramids_data.loc[:, ['Male Population', 'Female Population']]
+pyramids_data.columns = pd.Index(["Male", "Female"])
+pyramids_data.head()
+
+
+# Bining data
+popbin = pd.cut(pyramids_data.Male, 10)
+popbin
