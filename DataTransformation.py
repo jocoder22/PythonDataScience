7ba6ef8cc85f3b2ca0ffd1e9bad96a0806f8141c
Okigbo <okigbookey@gmail.com>
2018-10-22 15:05:28 -0400

    Explore dataset

--- DataTransformation.py
+++ DataTransformation.py
@@ -5,3 +5,9 @@ os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
 
 pyramids_data = pd.read_csv("PopPyramids.csv",
                             index_col=["Country", "Age", "Year"])
+
+
+# Explore the data
+pyramids_data.head()
+pyramids_data.columns
+pyramids_data.columns.tolist()
