e2f9e606973573319ff677aa65ddcf5e92f27eb4
Okigbo <okigbookey@gmail.com>
2018-10-25 11:51:04 -0400

    Add upper() and lower() methods

---
+++ stringManipulation.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame
+import re, string
+from datetime import datetime
+
+eassy = """this is the begining of time. But with all good
+           intention comes failure, only work hard as you
+            can and expect the best. Be careful while going
+            on a journey of life."""
+
+print(eassy.upper())
+print(eassy.lower())
