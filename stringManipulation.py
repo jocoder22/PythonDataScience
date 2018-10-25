1a7ae633ff1edd5c1d64e8ba26d411ca31d85c00
Okigbo <okigbookey@gmail.com>
2018-10-25 11:53:02 -0400

    Update

--- stringManipulation.py
+++ stringManipulation.py
@@ -2,7 +2,7 @@ from pandas import DataFrame
 import re, string
 from datetime import datetime
 
-eassy = """this is the begining of time. But with all good
+eassy = """This is the begining of time. But with all good
            intention comes failure, only work hard as you
             can and expect the best. Be careful while going
             on a journey of life."""
