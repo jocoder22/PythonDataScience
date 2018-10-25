9daeb1932a90e325db9fa266cd7423047eeb6fc5
Okigbo <okigbookey@gmail.com>
2018-10-25 12:01:57 -0400

    Split string

--- stringManipulation.py
+++ stringManipulation.py
@@ -2,10 +2,13 @@ from pandas import DataFrame
 import re, string
 from datetime import datetime
 
-eassy = """This is the begining of time. But with all good
-           intention comes failure, only work hard as you
-            can and expect the best. Be careful while going
+eassy = """This is the begining of time. But with all good and noble
+           intention comes failure, only work hard as you and others
+            can and expect the best. Be careful while going slowly
             on a journey of life."""
 
 print(eassy.upper())
 print(eassy.lower())
+
+eassy_split = eassy.lower().split(" ")
+eassy_split[:10]
