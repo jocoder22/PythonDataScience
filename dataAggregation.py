722e62a7ec5102dfcba6ebea8df42d55847fdc7b
Okigbo <okigbookey@gmail.com>
2018-10-26 16:14:59 -0400

    Update

--- dataAggregation.py
+++ dataAggregation.py
@@ -2,11 +2,9 @@ import os
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-
 import matplotlib.pyplot as plt
 
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
-
 pyramids_data = pd.read_csv("PopPyramids.csv",
                             index_col=["Country", "Age", "Year"])
 
