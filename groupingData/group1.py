274f3054c0b5ac79d54a8dc97e921f15c6b15c48
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-29 21:21:52 -0400

    Select and sort dataset

--- groupingData/group1.py
+++ groupingData/group1.py
@@ -7,4 +7,11 @@ import matplotlib.pyplot as plt
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
 
 pyramids_data = pd.read_csv("PopPyramids.csv",
-                            index_col=["Country", "Age", "Year"])
+                            index_col=["Country", "Year", "Age"])
+
+# select two columns and change index
+pyramids_data = pyramids_data.loc[:, ['Male Population', 'Female Population']].drop("Total",
+                                    axis=0, level="Age")
+pyramids_data.columns = pd.Index(["Male", "Female"])
+pyramids_data.sort_index(inplace=True)
+pyramids_data.head()
