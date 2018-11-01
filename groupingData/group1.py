10e252f75324926dfd5c6d60c05182d9d3115289
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-10-31 23:49:53 -0400

    Create age categories

--- groupingData/group1.py
+++ groupingData/group1.py
@@ -59,6 +59,11 @@ countrygroup.sum().sort_values("Population", ascending=False)
 agegroup.sum()
 agesexgroup.sum()
 
+# create age categories
+agecats = pd.categorical(['0-4', '5-9', '10-14', '15-19', '20-24', '25-29',
+                          '30-34', '35-39', '40-44', '45-49', '50-54', '55-59',
+                          '60-64', '65-69', '70-74', '75-79', '80-84', '85-89', 
+                          '90-94', '95-99', '100+'])
 
 # Group using index
 yeargroup = pyramids_data.groupby(level="Year")
