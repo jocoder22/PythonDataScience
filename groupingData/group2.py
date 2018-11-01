21c974d792cc247033728b70571fddcc46d93a27
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-01 19:58:35 -0400

    Update

---
+++ groupingData/group2.py
@@ -0,0 +1,9 @@
+mport os
+import pandas as pd
+import numpy as np
+from numpy.random import randn
+import matplotlib.pyplot as plt
+
+os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
+pyramids_data = pd.read_csv("PopPyramids.csv",
+                            index_col=["Country", "Year", "Age"])
