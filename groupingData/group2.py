6dc2d7238a6c8a5945605d335b1071b049dfd84f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-01 20:07:25 -0400

    Update

--- groupingData/group2.py
+++ groupingData/group2.py
@@ -8,3 +8,5 @@ os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
 pyramids_data = pd.read_csv("PopPyramids.csv")
 pyramids_data = pyramids_data.loc[:, ["Year", "Country", "Age", "Male Population",
                                    "Female Population"]]
+pyramids_data.columns = pd.Index(["Year", "Country", "Age", "Male", "Female"])
+pyramids_data = pyramids_data.loc[pyramids_data.Age != "Total"]
