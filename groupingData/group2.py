ccdc8f380c9a731d8a09aa53240d90e0a998f975
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-01 20:23:35 -0400

    Update

--- groupingData/group2.py
+++ groupingData/group2.py
@@ -4,7 +4,9 @@ import numpy as np
 from numpy.random import randn
 import matplotlib.pyplot as plt
 
-os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
+# C:\Users\Jose\Documents\PythonDataScience1\Code\Code\Section 1
+# C:\Users\Jose\Documents\PythonDataScience1\Code\Code\Section 1
+os.chdir("C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1")
 pyramids_data = pd.read_csv("PopPyramids.csv")
 pyramids_data = pyramids_data.loc[:, ["Year", "Country", "Age", "Male Population",
                                    "Female Population"]]
@@ -12,4 +14,5 @@ pyramids_data.columns = pd.Index(["Year", "Country", "Age", "Male", "Female"])
 pyramids_data = pyramids_data.loc[pyramids_data.Age != "Total"]
 pyramids_data = pd.melt(pyramids_data, id_vars=["Year", "Country", "Age"],
                         value_name="Population", var_name="Sex")
-pyramids_data.head()
+pyramids_data.head(20)
+pyramids_data.tail(20)
