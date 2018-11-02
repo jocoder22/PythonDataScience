fe8d10b30da09fccd69f8ff6cab77572f5d23290
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-01 20:31:13 -0400

    Look at the tail of the dataset

--- groupingData/group2.py
+++ groupingData/group2.py
@@ -9,10 +9,12 @@ import matplotlib.pyplot as plt
 os.chdir("C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 1")
 pyramids_data = pd.read_csv("PopPyramids.csv")
 pyramids_data = pyramids_data.loc[:, ["Year", "Country", "Age", "Male Population",
-                                   "Female Population"]]
+                                      "Female Population"]]
+
 pyramids_data.columns = pd.Index(["Year", "Country", "Age", "Male", "Female"])
 pyramids_data = pyramids_data.loc[pyramids_data.Age != "Total"]
 pyramids_data = pd.melt(pyramids_data, id_vars=["Year", "Country", "Age"],
                         value_name="Population", var_name="Sex")
+
 pyramids_data.head(20)
-pyramids_data.tail(20)
+pyramids_data.tail(29)
