b704a53af0f7a24ce77dc75b554813b821bc187d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-03 23:59:47 -0400

    Update

--- groupingData/group1.py
+++ groupingData/group1.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.random import randn
 import matplotlib.pyplot as plt
 
+
 os.chdir("C:/Users/okigboo/Documents/Code/Code/Code/Section 1")
 pyramids_data = pd.read_csv("PopPyramids.csv",
                             index_col=["Country", "Year", "Age"])
