a197dd2708cbd0ef970b5e336ea23f6822b0d600
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-04 22:33:48 -0500

    Explore iris dataset

--- groupingData/groupWise.py
+++ groupingData/groupWise.py
@@ -5,3 +5,7 @@ import pandas as pd
 
 os.chdir("C:/Users/Jose/Documents/PythonDataScience1/Code/Code/Section 3")
 irisdata = pd.read_csv("iris.csv")
+
+# Explore dataset
+irisdata.head()
+irisdata.tail()
