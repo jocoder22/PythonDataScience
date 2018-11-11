e10a1cf9c993faac6c2a6ffc8666af7df70d5425
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-10 21:45:20 -0500

    session set-up

--- beautifulSoup/session.py
+++ beautifulSoup/session.py
@@ -2,3 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 session = requests.Session()
 # to see more https://www.whatismybrowser.com/
+
+url = "https://en.wikipedia.org/wiki/List_of_Nobel_laureates"
+
+header = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Connection": "keep-alive",
+          "Referrer": "https://www.goggle.com/",
+          "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686, rv:54.8) Gecko/20180101 Firefox/54.0"}
+          
