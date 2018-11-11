3ec94bacb57d460ea0732f6fe397c85fa24423d4
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-11 02:13:51 -0500

    search for tables on the webpage

--- beautifulSoup/session2.py
+++ beautifulSoup/session2.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as Bs
+
+# Begin a session
 session = requests.Session()
 # to see more https://www.whatismybrowser.com/
 
 url = "https://en.wikipedia.org/wiki/List_of_Nobel_laureates"
 
 page = session.get(url).text
+
+# make a BeautifulSoup object
+nobelist = Bs(page)
+nobeltable = nobelist.findAll("table", {"class":['wikitable', 'sortable']})
