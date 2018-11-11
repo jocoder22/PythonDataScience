23a6f0b25ee2c61ef19109dff06c0660f9ed8939
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-11 02:38:05 -0500

    scrap the first webpage

--- beautifulSoup/session3.py
+++ beautifulSoup/session3.py
@@ -6,3 +6,28 @@ from pandas import DataFrame
 from bs4 import BeautifulSoup as Bs
 
 
+# Begin a session
+session = requests.Session()
+# to see more https://www.whatismybrowser.com/
+
+url = "https://en.wikipedia.org/wiki/List_of_Nobel_laureates"
+
+page = session.get(url).text
+
+
+# make a BeautifulSoup object
+nobelist = Bs(page)
+nobeltable = nobelist.find("table", {"class":['wikitable', 'sortable']})
+
+tabledata = nobeltable.findAll("td")
+
+for data in tabledata:
+    print(data.a)
+
+# Add the information inside a dictionary
+links = dict()
+
+for alinks in tabledata:
+    if alinks.a != None:
+        links[alinks.a.contents[0]] = alinks.a.attrs["href"]
+links
