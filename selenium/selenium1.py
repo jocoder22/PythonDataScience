d3e1ae970ae3600bef37f94f74a9018c52e2662f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-16 00:00:23 -0500

    Update

--- selenium/selenium1.py
+++ selenium/selenium1.py
@@ -6,5 +6,6 @@ driver = webdriver.Chrome(executable_path=path)
 sleep(12)
 driver.get("http://pycoders.com/archive/")
 sleep(12)
-print.driver.title
+print(driver.title)
+print(drive.current_url)
 driver.close()
