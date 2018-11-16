93191eb3485c3939476bc45533d3655e8b5019a3
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-15 23:53:19 -0500

    Open chrome brower using selenium

---
+++ selenium/selenium1.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from time import sleep
+
+path = "C:/webdrivers/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=path)
+sleep(10)
+driver.get("http://pycoder.com/archive/")
+sleep(10)
