2d568b622e286fbe8a520a9302d634d853a3be7a
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-16 00:24:13 -0500

    Update

--- selenium/selenium1.py
+++ selenium/selenium1.py
@@ -7,5 +7,19 @@ sleep(12)
 driver.get("http://pycoders.com/archive/")
 sleep(12)
 print(driver.title)
-print(drive.current_url)
+print(driver.current_url)
+driver.close()
+
+
+path2 = "C:/webdrivers/geckodriver.exe"
+driver = webdriver.Firefox(executable_path=path2)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+print(driver.title)
+print(driver.current_url)
 driver.close()
