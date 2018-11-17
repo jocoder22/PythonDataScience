bbc4d23850ec4d6dd28606a899aeebf4987841e8
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-17 10:25:51 -0500

    Using BeautifulSoup to scrap the webpage

--- selenium/selenium3.py
+++ selenium/selenium3.py
@@ -4,3 +4,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from bs4 import BeautifulSoup
 import requests
+
+session = requests.Session()
+url = "http://pycoders.com/achive/"
+page = session.get(url).text
+bsObj = BeautifulSoup(page)
+
+link1 = dict()
+for a in bsObj.findAll("a"):
+    link1[a.contents[0]] = a.attrs["href"]
+
+link1
