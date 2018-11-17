6fd1ac9c42ed698bcc53753a177a629884e64b9c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-17 11:24:04 -0500

    Update

--- selenium/selenium3.py
+++ selenium/selenium3.py
@@ -4,33 +4,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from bs4 import BeautifulSoup
 import requests
+import itertools
 
 session = requests.Session()
 url = "http://pycoders.com/archive/"
 page = session.get(url).text
 bsObj = BeautifulSoup(page)
 
-link1 = dict()
-for a in bsObj.findAll("a"):
-    link1[a.contents[0]] = a.attrs["href"]
+bsdiv = bsObj.find("div", {"class": "mb-3"})
 
-link1
+link = dict()
+url2 = "https://pycoders.com"
+for a in bsdiv.findAll("a"):
+    link[a.contents[0]] = url2 + a.attrs["href"]
 
+dict(itertools.islice(link.items(), 5))
+
+
+# using selenium WebDriverWait
 path = "chromedriver.exe"
 driver = webdriver.Chrome(executable_path=path)
 driver.get(url)
 
-wait = WebDriverWait(driver, 60, 1)
+wait = WebDriverWait(driver, 20, 1)
 wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "display_archive")))
 
 bsObj = BeautifulSoup(driver.page_source)
+bsdiv2 = bsObj.find("div", {"class": "mb-3"})
 
 link2 = dict()
-url2 = "https://pycoders.com"
-for a in bsObj.findAll("a"):
+for a in bsdiv2.findAll("a"):
     link2[a.contents[0]] = url2 + a.attrs["href"]
 
-link2
 
-import itertools
-dict(itertools.islice(link2.items(), 2))
+dict(itertools.islice(link2.items(), 4))
