4e891b9c37911309b4cf91465d5a027a67bb1cd6
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-22 07:53:03 -0500

    Update

--- datascience/datamunging2.py
+++ datascience/datamunging2.py
@@ -9,9 +9,18 @@ dataset.head()
 
 # Loading in chunks: loaded dataset at mydata.csv
 bookChunk10 = pd.read_csv(u'~/Desktop/mydata.csv', header=None, 
-                          names=['int0','int1','int2','int3','int4','int5','int6'], 
+                          names=['int0','int1','int2','int3','int4','int5','int6'],
                           chunksize=10) 
 
 for chunk in bookChunk10: 
     print ('Shape:', chunk.shape) 
     print (chunk,'\n')
+
+# Loading as iterator
+bookIter = pd.read_csv(u'~/Desktop/mydata.csv', header=None, 
+                       names=['int0','int1','int2','int3','int4','int5','int6'],
+                       iterator=True)
+
+print(bookIter.get_chunk(10).shape)
+next20 = bookIter.get_chunk(20)  
+next20
