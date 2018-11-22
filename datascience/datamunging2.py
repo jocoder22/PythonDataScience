840f0aef88916ca5f38228ffe14bf8d784af0751
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-22 07:49:47 -0500

    Add new file datamunging2.py

---
+++ datascience/datamunging2.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+from urllib.request import urlopen
+
+site = 'http://mldata.org/repository/data/download/csv/book-evaluation-complete/'
+datasiteOpen = urlopen(site)
+dataset = pd.read_csv(datasiteOpen, header=None, names=['int0','int1','int2','int3','int4','int5','int6'])
+dataset.head()
+
+
+# Loading in chunks: loaded dataset at mydata.csv
+bookChunk10 = pd.read_csv(u'~/Desktop/mydata.csv', header=None, 
+                          names=['int0','int1','int2','int3','int4','int5','int6'], 
+                          chunksize=10) 
+
+for chunk in bookChunk10: 
+    print ('Shape:', chunk.shape) 
+    print (chunk,'\n')
