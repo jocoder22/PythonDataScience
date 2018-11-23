74cecf21b7464e8c4dce4149bc0c292b3e7edb9b
Okigbo <okigbookey@gmail.com>
2018-11-23 13:08:35 -0500

    Compare memory sizes of different np.dtypes

--- NumericalComputation/numpy1.py
+++ NumericalComputation/numpy1.py
@@ -8,3 +8,18 @@ print("Our array has total size of", x.size)
 print("with dimension of", x.ndim)
 print("The data type is", x.dtype)
 print("Memory size is", x.nbytes, "bytes")
+
+
+# compare memory sizes
+xint32 = np.array([[2, 2, 5, 6], [6, 8, 10, 4]], dtype=np.int32)
+print(xint32, "Memory size of", xint32.nbytes, "bytes")
+xint64 = np.array([[2, 2, 5, 6], [6, 8, 10, 4]], dtype=np.int64)
+print(xint64, "Memory size of", xint64.nbytes, "bytes")
+xfloat = np.array([[2, 2, 5, 6], [6, 8, 10, 4]], dtype=np.float)
+print(xfloat, "Memory size of", xfloat.nbytes, "bytes")
+xcomplex = np.array([[2, 2, 5, 6], [6, 8, 10, 4]], dtype=np.complex)
+print(xcomplex, "Memory size of", xcomplex.nbytes, "bytes")
+xuint32 = np.array([[2, 2, 5, 6], [6, 8, 10, 4]], dtype=np.uint32)
+print(xuint32, "Memory size of", xuint32.nbytes, "bytes")
+
+
