a180013b89071a724cc3f8c512636cbdc9851257
Okigbo <okigbookey@gmail.com>
2018-11-23 16:22:59 -0500

    Update

--- NumericalComputation/numpy.2.py
+++ NumericalComputation/numpy.2.py
@@ -36,4 +36,48 @@ print(ones, ones.dtype)
 # vector operations
 myarray - zeroarray6
 zeroarray6 / myarray
+myarray - I_array7
+I_array7 / myarray
 
+# range, geomspace, logspace
+Xarange = np.arange(4, 14, 2)
+Xlinspace = np.linspace(2, 30, num=40)
+print(Xlinspace)
+Xlinspace2 = np.linspace(2, 30, num=29)
+print(Xlinspace2)
+
+Xgeomspace = np.geomspace(2, 5630, num=10)
+print(Xgeomspace)
+
+Xlogspace = np.logspace(2, 3, num=40)
+print(Xlogspace)
+
+
+np.logspace(3, 4, num=5)
+np.logspace(np.log10(3), np.log10(4), num=5)
+
+
+# logical operations
+x = np.array([0, 1, 0, 0, 1], dtype=bool)
+y = np.array([1, 1, 0, 1, 0], dtype=bool)
+print(np.logical_or(x, y))
+
+print(np.logical_and(x, y))
+
+x = np.array([16, 34, 57, 17, 29])
+print(np.logical_or(x < 18, x > 40))
+
+longdis = np.arange(15).reshape(5, 3)
+
+
+# some basic statistics
+np.sum(longdis)
+np.amin(longdis)
+np.amax(longdis)
+np.amin(longdis, axis=0)
+np.amin(longdis, axis=1)
+np.percentile(longdis, 75)
+np.percentile(longdis, 95, axis=1)
+
+np.argmax(longdis)
+np.argmin(longdis)
