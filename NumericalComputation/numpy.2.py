95b241f1754ef7fc40ba1d147898e31daa63f6dc
Okigbo <okigbookey@gmail.com>
2018-11-23 14:44:49 -0500

    Update

--- NumericalComputation/numpy.2.py
+++ NumericalComputation/numpy.2.py
@@ -5,7 +5,7 @@ list1 = [3, 6, 9, 12]
 myarray = np.array(list1)
 print(type(myarray))
 
-# Scalar arthmetics
+# Scalar arithmetics
 print(myarray + 2)
 print(myarray - 1)
 print(myarray * 4)
@@ -16,3 +16,18 @@ print((myarray + 2).dtype)
 print((myarray - 1).dtype)
 print((myarray * 4).dtype)
 print((myarray / 3).dtype)
+
+# zero array
+zeroarray = np.zeros(4)
+zeroarray6 = np.zeros(4) + 6
+print(zeroarray, zeroarray6)
+print(zeroarray.dtype, zeroarray6.dtype)
+
+# Indentity
+I_array = np.identity(4)
+print(I_array, I_array.dtype)
+
+# ones
+ones = np.ones(5)
+print(ones, ones.dtype)
+
