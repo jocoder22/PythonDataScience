3328092c9691a70e6fdfb7dd02f80b425e68510c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-24 21:24:23 -0500

    Update

--- NumericalComputation/linearAlgebra.py
+++ NumericalComputation/linearAlgebra.py
@@ -13,3 +13,23 @@ x1@x2
 
 multi_dot([x1, x2])
 multi_dot([x1, x2, x3, x4])
+
+# inner and outer products
+y = np.arange(12).reshape(4, 3)
+z = np.arange(3)
+
+np.inner(y, z)
+np.outer(y, z)
+
+
+y = np.arange(12)
+np.ndim(y)  # this is one dimension now
+np.outer(y, z)
+
+# higher dimension product
+a = np.arange(12).reshape(2,3,2) 
+b = np.arange(48).reshape(3,2,8) 
+c = np.tensordot(a,b, axes =([1,0],[0,1])) 
+print(a) 
+print(b)
+print(c)
