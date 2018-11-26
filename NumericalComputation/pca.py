de85282857a2baa221bb979dfd9f79d4b2eddd23
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-11-25 20:31:55 -0500

    Update

--- NumericalComputation/pca.py
+++ NumericalComputation/pca.py
@@ -5,3 +5,12 @@ from sklearn.preprocessing import StandardScaler
 datasets = datasets.load_breast_cancer() 
 cancerdataset = datasets.data
 cancerdataset = StandardScaler().fit_transform(cancerdataset)
+cancerdataset.shape
+
+
+coval_matrix = np.cov(cancerdataset, rowvar=False)
+coval_matrix.shape
+
+eig_value, eig_vector = np.linalg.eig(coval_matrix)
+eig_pairs = [(np.abs(eig_value[i]), eig_vector[:,i]) for i in
+             range(len(eig_value))]
