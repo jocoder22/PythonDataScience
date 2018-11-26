6309e5f48a1b3051249b54f14cd6720e76ba7f6d
Okigbo <okigbookey@gmail.com>
2018-11-26 16:02:41 -0500

    Solver linear equations

---
+++ NumericalComputation/solveGradient.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+a = np.array([[3, 6, 8], [0, 1, 5], [2, 9, 8]])
+b = np.array([3, 8, 6])
+
+# Using the inverse method
+a_inverse = np.linalg.inv(a)
+solution = np.dot(a_inverse, b)
+
+# Using the solve function
+solution2 = np.linalg.solve(a, b)
+
+# check for equality
