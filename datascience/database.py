ca43ea54d07d873c4ce1905a538eea95a4872c3d
Okigbo <okigbookey@gmail.com>
2018-11-27 17:44:12 -0500

    Insert data into database

--- datascience/database.py
+++ datascience/database.py
@@ -11,3 +11,15 @@ connection = sqlite3.connect("Test01.db")
 connection.execute(droptable)
 connection.execute(createtable)
 connection.commit()
+
+
+# get the data
+mydata = [(20181109, "New York", "Winter", 23.9, 2),
+          (20181112, "Brookly", "Winter", 25.0, 2),
+          (20181114, "Boston",  "Winter", 33.9, 3),
+          (20181116, "Queens", "Winter", 15.9, 1),
+          (20181114, "Newark",  "Winter", 13.9, 1),
+          (20181116, "Hicksvill", "Winter", 10.9, 1)]
+insert_query = "INSERT INTO WeatherData VALUES(?, ?, ?, ?)"
+connection.executemany(insert_query, mydata)
+connection.commit()
