66dac98388a5701e4d2927e766a1b13a48028c65
Okigbo <okigbookey@gmail.com>
2018-11-27 18:02:03 -0500

    Update

--- datascience/database.py
+++ datascience/database.py
@@ -3,9 +3,9 @@ import pandas as pd
 import sqlite3
 
 
-droptable = "DROP TABLE IF EXISTS temp_data;"
+droptable = "DROP TABLE IF EXISTS WeatherData;"
 createtable = "CREATE TABLE WeatherData \
-                (Date INTEGER, City VARCHAR(40), Season VARCHAR(40) \
+                (Date INTEGER, City VARCHAR(40), Season VARCHAR(40), \
                 Temperature REAL, Grade INTEGER);"
 connection = sqlite3.connect("Test01.db")
 connection.execute(droptable)
@@ -19,9 +19,9 @@ mydata = [(20181109, "New York", "Winter", 23.9, 2),
           (20181114, "Boston",  "Winter", 33.9, 3),
           (20181116, "Queens", "Winter", 15.9, 1),
           (20181114, "Newark",  "Winter", 13.9, 1),
-          (20181116, "Hicksvill", "Winter", 10.9, 1)]
-insert_query = "INSERT INTO WeatherData VALUES(?, ?, ?, ?)"
-connection.executemany(insert_query, mydata)
+          (20181116, "Hicksville", "Winter", 10.9, 1)]
+inserttable1 = "INSERT INTO WeatherData VALUES(?, ?, ?, ?, ?)"
+connection.executemany(inserttable1, mydata)
 connection.commit()
 
 # select query
@@ -31,4 +31,5 @@ connection.close()
 
 workingdata.Date.dtype
 workingdata['Date'] = pd.to_datetime(workingdata['Date'].astype(str), format='%Y%m%d')
-
+workingdata.head()
+workingdata.shape
