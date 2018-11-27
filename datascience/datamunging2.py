65aa5575bfbaea1296985632fcdcff3c0ce312d5
Okigbo <okigbookey@gmail.com>
2018-11-27 16:24:40 -0500

    Update

--- datascience/datamunging2.py
+++ datascience/datamunging2.py
@@ -1,29 +1,33 @@
-import pandas as pd 
+import pandas as pd
 from urllib.request import urlopen
 import csv
 
 site = 'http://mldata.org/repository/data/download/csv/book-evaluation-complete/'
 datasiteOpen = urlopen(site)
-dataset = pd.read_csv(datasiteOpen, header=None, names=['int0','int1','int2','int3','int4','int5','int6'])
+dataset = pd.read_csv(datasiteOpen, header=None,
+                      names=['int0', 'int1', 'int2', 'int3',
+                             'int4', 'int5', 'int6'])
 dataset.head()
 
 
 # Loading in chunks: loaded dataset at mydata.csv
-bookChunk10 = pd.read_csv(u'~/Desktop/mydata.csv', header=None, 
-                          names=['int0','int1','int2','int3','int4','int5','int6'],
-                          chunksize=10) 
+bookChunk10 = pd.read_csv(u'~/Desktop/mydata.csv', header=None,
+                          names=['int0', 'int1', 'int2', 'int3',
+                                 'int4', 'int5', 'int6'],
+                          chunksize=10)
 
-for chunk in bookChunk10: 
-    print ('Shape:', chunk.shape) 
-    print (chunk,'\n')
+for chunk in bookChunk10:
+    print('Shape:', chunk.shape)
+    print(chunk, '\n')
 
 # Loading as iterator
-bookIter = pd.read_csv(u'~/Desktop/mydata.csv', header=None, 
-                       names=['int0','int1','int2','int3','int4','int5','int6'],
+bookIter = pd.read_csv(u'~/Desktop/mydata.csv', header=None,
+                       names=['int0', 'int1', 'int2', 'int3',
+                              'int4', 'int5', 'int6'],
                        iterator=True)
 
 print(bookIter.get_chunk(10).shape)
-next20 = bookIter.get_chunk(20)  
+next20 = bookIter.get_chunk(20)
 next20
 
 
@@ -32,7 +36,10 @@ with open(r'C:/Users/Jose/Desktop/book.csv', 'rt') as stream:
     mydict = dict()
     mylist = list()
     for n, row in enumerate(csv.DictReader(stream,
-        fieldnames = ['int0','int1','int2','int3','int4','int5','int6'],dialect='excel')):
+                                           fieldnames=['int0', 'int1', 'int2',
+                                                       'int3', 'int4', 'int5',
+                                                       'int6'],
+                                           dialect='excel')):
         for (k, v) in row.items():
             mydict[k] = v
             mylist.append(mydict)
@@ -47,19 +54,20 @@ with open(r'C:/Users/Jose/Desktop/book.csv', 'rt') as stream:
     mydict = dict()
     mylist = list()
     for n, row in enumerate(csv.DictReader(stream,
-        fieldnames = ['int0','int1','int2','int3','int4','int5','int6'],dialect='excel')):
+                                           fieldnames=['int0', 'int1', 'int2',
+                                                       'int3', 'int4', 'int5',
+                                                       'int6'],
+                                           dialect='excel')):
         if n <= 3:
-            print (n,row)
+            print(n, row)
         else:
             break
-
-
 
 
 # Using csv.reader
 with open(r'C:/Users/Jose/Desktop/book.csv', 'rt') as dstream:
     for n, row in enumerate(csv.reader(dstream, dialect='excel')):
             if n <= 3:
-                print (n,row)
+                print(n, row)
             else:
                 break
