098834c34ebedf07211b4b847648eb4c829a77bf
Okigbo <okigbookey@gmail.com>
2018-11-27 14:58:16 -0500

    Get basic statistics

--- NumericalComputation/exploratoryDataAnalysis/explore1.py
+++ NumericalComputation/exploratoryDataAnalysis/explore1.py
@@ -1,11 +1,33 @@
 import numpy as np
+from scipy import stats
 from sklearn.datasets import load_boston
 
 dataset = load_boston()
-mydata, mylabel, feature_name = dataset.data, dataset.target, dataset.feature_names
+mydata, = dataset.data
+mylabel, feature_names = dataset.target, dataset.feature_names
 
 print(mydata.dtype)
 print(mylabel.dtype)
 
 np.isnan(np.sum(mydata))
 np.isnan(np.sum(mylabel))
+
+min = np.round(np.amin(mydata, axis=0), decimals=3)
+max = np.round(np.amax(mydata, axis=0), decimals=3)
+range = np.round(np.ptp(mydata, axis=0), decimals=3)
+mean = np.round(np.mean(mydata, axis=0), decimals=3)
+median = np.round(np.median(mydata, axis=0), decimals=3)
+variance = np.round(np.var(mydata, axis=0), decimals=3)
+percentile10 = np.round(np.percentile(mydata, 10, axis=0), decimals=3)
+percentile90 = np.round(np.percentile(mydata, 90, axis=0), decimals=3)
+
+
+mystatics = np.vstack((min, max, range, mean, median,
+                       variance, percentile10, percentile90))
+
+statlabels = ["min", "max", "range", "mean",
+              "median", "variance", "10%Tile", "90%Tile"]
+
+statlabels2 = ["minn", "maxx", "rang", "mean",
+               "medi", "vari", "10%T", "90%T"]
+
