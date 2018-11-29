17c94e52d58ee21b98b0abf1dd37e0c6e66ff5cf
Okigbo <okigbookey@gmail.com>
2018-11-29 15:32:38 -0500

    Update

--- datascience/dataTypes.py
+++ datascience/dataTypes.py
@@ -3,7 +3,10 @@ import pandas as pd
 from sklearn.preprocessing import oneHotEndocer
 from sklearn.preprocessing import LabelEncoder
 
-categoricalFeature = pd.series(['Monday', 'Tuesdau', 'Wednesday', 'Thursday',
-                                'Friday', 'Saturday', 'Sunday'])\
+categoricalFeature = pd.Series(['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                                'Friday', 'Saturday', 'Sunday'])
 
 catmapping = pd.get_dummies(categoricalFeature)
+
+catmapping['Friday']
+catmapping['Tuesday']
