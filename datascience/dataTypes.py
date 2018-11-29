a0185fc2e2f22874ab6b2c5894b37a47a9d87453
Okigbo <okigbookey@gmail.com>
2018-11-29 15:57:58 -0500

    Create dummies

--- datascience/dataTypes.py
+++ datascience/dataTypes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import oneHotEndocer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
 categoricalFeature = pd.Series(['Monday', 'Tuesday', 'Wednesday', 'Thursday',
@@ -10,3 +10,16 @@ catmapping = pd.get_dummies(categoricalFeature)
 
 catmapping['Friday']
 catmapping['Tuesday']
+
+
+# Using sklearn
+label1 = LabelEncoder()
+hot1 = OneHotEncoder()
+weekdays = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+fit_data = label1.fit_transform(weekdays)
+hot1.fit([[fit_data[0]], [fit_data[1]], [fit_data[2]],
+          [fit_data[3]], [fit_data[4]]])
+
+print(hot1.transform([label1.transform(['Monday'])]).toarray())
+print(hot1.transform([label1.transform(['Tuesday'])]).toarray())
