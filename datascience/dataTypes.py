8fc14a7750646c93c6832ca4b73195a06a1ab823
Okigbo <okigbookey@gmail.com>
2018-11-29 15:58:59 -0500

    Update

--- datascience/dataTypes.py
+++ datascience/dataTypes.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import fetch_20newsgroups
 
 categoricalFeature = pd.Series(['Monday', 'Tuesday', 'Wednesday', 'Thursday',
                                 'Friday', 'Saturday', 'Sunday'])
@@ -23,3 +24,9 @@ hot1.fit([[fit_data[0]], [fit_data[1]], [fit_data[2]],
 
 print(hot1.transform([label1.transform(['Monday'])]).toarray())
 print(hot1.transform([label1.transform(['Tuesday'])]).toarray())
+
+
+
+# Working with text
+categories = ['sci.med', 'sci.space']
+twenty_sci_news = fetch_20newsgroups(categories=categories)
