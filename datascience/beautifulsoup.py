e3b779e24abd09c29db44aec45f86022bfbaac37
Okigbo <okigbookey@gmail.com>
2018-12-03 11:50:52 -0500

    find link in category section

--- datascience/beautifulsoup.py
+++ datascience/beautifulsoup.py
@@ -1,4 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-url = 'https://en.wikipedia.org/wiki/William_Shakespeare'
 
+url = 'https://en.wikipedia.org/wiki/William_Shakespeare'
+response = urlopen(url)
+bsObject = BeautifulSoup(response, 'html.parser')
+bsObject.title
+
+section = bsObject.find_all(id='catlinks')[0]
+for catlink in section.find_all("a")[1:]:
+    print(catlink.get("title"), "->", catlink.get("href"))
