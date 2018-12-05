5610f088b413bddfc08df1ef5138e470a0350276
Okigbo <okigbookey@gmail.com>
2018-12-05 14:12:35 -0500

    Update

--- datascience/pipeline/EDA.py
+++ datascience/pipeline/EDA.py
@@ -2,8 +2,8 @@ from urllib.request import urlopen
 import pandas as pd
 
 irisurl = "http://aima.cs.berkeley.edu/data/iris.csv"
-iris_open = urllib.request.urlopen(irisurl)
+iris_open = urlopen(irisurl)
 irisdata = pd.read_csv(iris_open, sep=',', decimal='.', header=None,
-                        names=['sepal_length', 'sepal_width', 'petal_length',
-                               'petal_width', 'target'])
+                       names=['sepal_length', 'sepal_width', 'petal_length',
+                              'petal_width', 'target'])
  
