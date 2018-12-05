37649312c5c8ff79d4b93245fe88fc1fc9e928ae
Okigbo <okigbookey@gmail.com>
2018-12-05 14:23:56 -0500

    Update

--- datascience/pipeline/EDA.py
+++ datascience/pipeline/EDA.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+import matplotlib.pyplot as plt
 import pandas as pd
 
 irisurl = "http://aima.cs.berkeley.edu/data/iris.csv"
@@ -7,3 +8,9 @@ irisdata = pd.read_csv(iris_open, sep=',', decimal='.', header=None,
                        names=['sepal_length', 'sepal_width', 'petal_length',
                               'petal_width', 'target'])
  
+# Explore the dataset
+irisdata.describe()
+
+# get a boxplot
+boxes = irisdata.boxplot(return_type='axes')
+plt.show()
