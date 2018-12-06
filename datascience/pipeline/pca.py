020cea347d12d485de551294faa0f9c90b114c14
Okigbo <okigbookey@gmail.com>
2018-12-06 12:33:39 -0500

    Update

--- datascience/pipeline/pca.py
+++ datascience/pipeline/pca.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 import numpy as np
 
 iris = load_iris()
@@ -8,3 +10,6 @@ print(covdata)
 
 
 # pca
+pca2 = PCA(n_components=2)
+pca2comp = pca2.fit_transform(iris.data)
+pca2comp.shape 
