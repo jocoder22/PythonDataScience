611933df4c085119be8f63fbffbda21d13aa0747
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-09 21:12:50 -0500

    fit svm models

--- datascience/pipeline/testing.py
+++ datascience/pipeline/testing.py
@@ -1,6 +1,14 @@
 from sklearn.datasets import load_digits
+from sklearn import svm
 
 digits = load_digits()
 print(digits.DESCR)
 ddata = digits.data
 dtarget = digits.target
+
+ddata.shape
+
+from sklearn import svm
+h1 = svm.LinearSVC(C=1.0)
+h2 = svm.SVC(kernel='rbf', degree=3, gamma=0.001, C=1.0)
+h3 = svm.SVC(kernel='poly', degree=3, C=1.0)
