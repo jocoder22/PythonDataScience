e71e738b0bd3208bf692a060bb86825f60285f07
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-09 21:15:08 -0500

    svm digits dataset

--- datascience/pipeline/testing.py
+++ datascience/pipeline/testing.py
@@ -8,7 +8,10 @@ dtarget = digits.target
 
 ddata.shape
 
-from sklearn import svm
+# svm classification
 h1 = svm.LinearSVC(C=1.0)
 h2 = svm.SVC(kernel='rbf', degree=3, gamma=0.001, C=1.0)
 h3 = svm.SVC(kernel='poly', degree=3, C=1.0)
+
+h1.fit(ddata, dtarget)
+print(h1.score(ddata, dtarget))
