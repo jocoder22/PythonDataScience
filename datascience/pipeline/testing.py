9134d47cdcb65243c3f638f65aee23290b6c2aab
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-09 22:07:03 -0500

    Update

--- datascience/pipeline/testing.py
+++ datascience/pipeline/testing.py
@@ -21,9 +21,30 @@ print(h1.score(ddata, dtarget))
 chosen_random_state = 1
 Xtrain, Xtest, ytrain, ytest = train_test_split(ddata, dtarget, test_size=0.40, 
                                                 random_state=chosen_random_state)
-print ("(Xtrain shape {}, Xtest shape {},  ytrain shape {}, \
+print ("(Xtrain shape {}, Xtest shape {},  \nytrain shape {}, \
         ytest shape {})".format (Xtrain.shape, Xtest.shape, 
                             ytrain.shape, ytest.shape))
 
 h1.fit(Xtrain,ytrain)
 print (h1.score(Xtest,ytest)) 
+
+# Do validation test
+chosen_random_state = 1
+Xtrain, Xvalidation_test, ytrain, yvalidation_test = train_test_split(ddata, dtarget, 
+                                    test_size=.40, random_state=chosen_random_state)
+Xvalidation, Xtest, yvalidation, ytest = train_test_split(Xvalidation_test, yvalidation_test, 
+                                test_size=.50, 
+                                random_state=chosen_random_state)
+print ("X train shape, {}, X validation shape {}, X test shape {}, \
+        /ny train shape {}, y validation shape {}, y test shape {}/n".format
+        (Xtrain.shape, Xvalidation.shape, Xtest.shape,  
+        ytrain.shape, yvalidation.shape, ytest.shape))
+for hypothesis in [h1, h2, h3]:
+    hypothesis.fit(Xtrain,ytrain)
+    print ("{} -> validation mean accuracy = {:.3f}".format(hypothesis,  
+            hypothesis.score(Xvalidation,yvalidation)))
+
+h2.fit(X_train,y_train)
+print ("n{} -> test mean accuracy = {:.3f}".format(h2,   
+       h2.score(Xtest,ytest)))
+
