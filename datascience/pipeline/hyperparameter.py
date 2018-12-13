7c847a0c99dfb42627a95f9ca4b44e73d9c18947
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-13 04:53:01 -0500

    Set up model search function

--- datascience/pipeline/hyperparameter.py
+++ datascience/pipeline/hyperparameter.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_digits
 from sklearn import svm
+from sklearn import model_selection
 from sklearn.model_selection import train_test_split
 
 digits = load_digits()
@@ -10,3 +11,17 @@ ddata.shape
 # set up support vector machine algorithm
 hs = svm.SVC()
 hprob = svm.SVC(probability=True, random_state=1)
+
+
+# set the search grid for model selection
+search_grid = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'gamma': [0.01, 0.001, 0.0001], 
+        'kernel': ['rbf']},
+        ]
+scorer = 'accuracy'
+
+# set up the model searh function
+searchFunc = model_selection.GridSearchCV(estimator=hs,  
+                                param_grid=search_grid, scoring=scorer, 
+                                n_jobs=-1, iid=False, refit=True, cv=10)
