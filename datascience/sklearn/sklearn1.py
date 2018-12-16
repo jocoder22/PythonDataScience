de4d45bfa0f403bac2c798a9795b79088b9bcfc5
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-16 09:33:59 -0500

    Update

--- datascience/sklearn/sklearn1.py
+++ datascience/sklearn/sklearn1.py
@@ -12,3 +12,13 @@ print(np.unique(target_))
 X_train, X_test, y_train, y_test = spp(features_, target_, test_size=0.3,
                                        random_state=1, stratify=target_)
 
+print('Labels counts in target:', np.bincount(target_))
+print('Labels counts in y_train:', np.bincount(y_train))
+print('Labels counts in y_test:', np.bincount(y_test))
+print('Labels counts in y_test:', np.bincount(X_test))
+print('Labels counts in y_test:', np.bincount(X_train))
+
+
+y = [y_train, y_test]
+for item in y:
+    print(np.bincount(item))
