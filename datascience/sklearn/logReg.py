d0af00d6a7e8a5ab3fdd319ec5808ad484fdee15
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-18 20:52:57 -0500

    Update

--- datascience/sklearn/logReg.py
+++ datascience/sklearn/logReg.py
@@ -2,25 +2,24 @@
 import numpy as np 
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Perceptron
 from sklearn.model_selection import train_test_split as splitit
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
+
 
 irisdata = datasets.load_iris()
 features_ = irisdata.data
 target_ = irisdata.target
 
 print(np.unique(target_))
+print(features_[:5, :])
 
 # Standardization
 stardard = StandardScaler()
 # Standardization with the original data
 stardard.fit(features_)
 features_std = stardard.transform(features_)
-
+print(features_std[:5, :])
 
 # Split features into test and training datasets
 X_train, X_test, y_train, y_test = splitit(features_std, target_, test_size=0.3,
