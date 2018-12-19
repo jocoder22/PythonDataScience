1b7f65b92387d988d61b15ef88390098660ba9b3
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-18 19:41:14 -0500

    Update

---
+++ datascience/sklearn/logReg.py
@@ -0,0 +1,8 @@
+import numpy as np 
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split as splitit
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
