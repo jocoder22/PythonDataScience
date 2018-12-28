6c231ee30b7159ce4279b70ac28916557e12167f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-27 21:40:01 -0500

    Update

--- datavisualization/visual2.py
+++ datavisualization/visual2.py
@@ -18,3 +18,27 @@ plt.title('My graphic display I')
 plt.legend()
 
 plt.show()
+plt.close()
+
+# Ploting multiple plots: using subplot
+groups = ['group1', 'group2', 'group3']
+sales = [90, 800, 5600]
+members = [13, 45, 10]
+totalHr = [214, 689, 280]
+
+plt.figure(1, figsize=(10.5, 4), facecolor='green',
+           edgecolor='r')
+plt.subplot(231)
+plt.bar(groups, sales)
+plt.subplot(232)
+plt.scatter(groups, members)
+plt.subplot(233)
+plt.plot(groups, totalHr)
+plt.subplot(234)
+plt.plot(x1, x1**3)
+plt.subplot(235)
+plt.plot(x1, x1**4)
+plt.subplot(236)
+plt.plot(x1, x1**5)
+
+plt.show()
