146dce0f60ca6301a97d66c733f4d47a766de4cd
Okigbo <okigbookey@gmail.com>
2018-12-28 12:13:41 -0500

    Update

---
+++ datavisualization/scatter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xplot = np.linspace(0, 10, 1000)
+yplot = np.sin(xplot)
+
+plt.scatter(xplot, yplot)
+plt.scatter(xplot+1, yplot, c='r')
+plt.scatter(xplot+2, yplot, c=np.cos(xplot))
+plt.colorbar()
+
+'''
+Colormap possible values are: 
+
+Accent, Accent_r, Blues, Blues_r, BrBG, BrBG_r, BuGn, BuGn_r, BuPu, BuPu_r, 
+CMRmap, CMRmap_r, Dark2, Dark2_r, GnBu, GnBu_r, Greens, Greens_r, Greys, Greys_r, OrRd, 
+OrRd_r, Oranges, Oranges_r, PRGn, PRGn_r, Paired, Paired_r, Pastel1, Pastel1_r, Pastel2, 
+Pastel2_r, PiYG, PiYG_r, PuBu, PuBuGn, PuBuGn_r, PuBu_r, PuOr, PuOr_r, PuRd, PuRd_r, 
+Purples, Purples_r, RdBu, RdBu_r, RdGy, RdGy_r, RdPu, RdPu_r, RdYlBu, RdYlBu_r, RdYlGn, 
+RdYlGn_r, Reds, Reds_r, Set1, Set1_r, Set2, Set2_r, Set3, Set3_r, Spectral, Spectral_r, 
+Wistia, Wistia_r, YlGn, YlGnBu, YlGnBu_r, YlGn_r, YlOrBr, YlOrBr_r, YlOrRd, YlOrRd_r, 
+
+afmhot, 
+afmhot_r, autumn, autumn_r, binary, binary_r, bone, bone_r, brg, brg_r, bwr, bwr_r, cividis, 
+cividis_r, cool, cool_r, coolwarm, coolwarm_r, copper, copper_r, cubehelix, cubehelix_r, flag, 
+flag_r, gist_earth, gist_earth_r, gist_gray, gist_gray_r, gist_heat, gist_heat_r, gist_ncar, 
+gist_ncar_r, gist_rainbow, gist_rainbow_r, gist_stern, gist_stern_r, gist_yarg, gist_yarg_r, 
+gnuplot, gnuplot2, gnuplot2_r, gnuplot_r, gray, gray_r, hot, hot_r, hsv, hsv_r, inferno, 
+inferno_r, jet, jet_r, magma, magma_r, nipy_spectral, nipy_spectral_r, ocean, ocean_r, pink, 
+pink_r, plasma, plasma_r, prism, prism_r, rainbow, rainbow_r, seismic, seismic_r, spring, 
+spring_r, summer, summer_r, tab10, tab10_r, tab20, tab20_r, tab20b, tab20b_r, tab20c, tab20c_r, 
+terrain, terrain_r, viridis, viridis_r, winter, winter_r
+
+'''
+
+plt.scatter(xplot+2, yplot, c=np.cos(xplot), cmap='inferno')
+plt.scatter(xplot+3, yplot, c=np.cos(xplot), cmap='inferno', edgecolors='none')
+
+plt.scatter(xplot+3, yplot, c=np.cos(xplot), cmap='magma', edgecolors='none',
+            s=np.power(xplot, 4))
+plt.colormaps('inferno')
