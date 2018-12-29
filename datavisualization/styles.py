c2dd35ed3655cf92ecb3a54bfa3cfb5a44fc4ffa
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-29 11:58:03 -0500

    Update

---
+++ datavisualization/styles.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from scipy.ndimage.filters import gaussian_filter as gf
+import matplotlib.pyplot as plt
+##import matplotlib.gridspec as gridspec
+##plt.style.use('seaborn-white')
+import numpy as np
+
+style_list = ['seaborn-dark', 'dark_background', 'seaborn-pastel', 'seaborn-colorblind',
+ 'tableau-colorblind10', 'seaborn-notebook', 'seaborn-dark-palette',
+ 'grayscale', 'seaborn-poster', 'seaborn', 'bmh', 'seaborn-talk',
+ 'seaborn-ticks', '_classic_test', 'ggplot', 'seaborn-white', 'classic',
+ 'Solarize_Light2', 'seaborn-paper', 'fast', 'fivethirtyeight',
+ 'seaborn-muted', 'seaborn-whitegrid', 'seaborn-darkgrid', 'seaborn-bright',
+ 'seaborn-deep']
