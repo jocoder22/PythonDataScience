813156fb07b7738fae753d2a283acd88a9e436d2
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-29 13:43:40 -0500

    Update

---
+++ datavisualization/subplots.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import matplotlib
+# matplotlib.matplotlib_fname()
+# display the location of matplotlibrc file
+
+######set subplot params
+# plt.rcParams['figure.subplot.wspace'] = 0.8
