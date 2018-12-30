06cf23221d2dcaa2ce71c5730f7131ebcdf7028a
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-29 22:14:21 -0500

    Update

---
+++ jupyter/nbconvert.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# jupyter nbconvert will convert jupyter .ipynb to html or pdf
+# from the console type
+
+jupyter nbconvert "C:\Users\Jose\Example 1.ipynb" --to pdf
+jupyter nbconvert "C:\Users\Jose\Example 1.ipynb" --to html
