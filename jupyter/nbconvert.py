ecf5333e64ba71eb70b8a6c014b6198a9a49d3e9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-29 22:40:47 -0500

    Update

--- jupyter/nbconvert.py
+++ jupyter/nbconvert.py
@@ -5,3 +5,27 @@
 
 jupyter nbconvert "C:\Users\Jose\Example 1.ipynb" --to pdf
 jupyter nbconvert "C:\Users\Jose\Example 1.ipynb" --to html
+
+
+
+# Example: creating slides w/ speaker notes
+# Let’s suppose you have a notebook your_talk.ipynb that you want to convert to slides. 
+# For this example, we’ll assume that you are working in the same directory as the notebook 
+# you want to convert (i.e., when you run ls ., your_talk.ipynb shows up amongst the list of files).
+# First, we need a copy of reveal.js in the same directory as your slides. One way to do this is 
+# to use the following commands in your terminal:
+
+git clone https://github.com/hakimel/reveal.js.git
+cd reveal.js
+git checkout 3.5.0
+cd ..
+
+
+# Then we need to tell nbconvert to point to this local copy. To do that 
+# we use the --reveal-prefix command line flag to point to the local copy.
+
+jupyter nbconvert your_talk.ipynb --to slides --reveal-prefix reveal.js
+
+# This will create file your_talk.slides.html, which you should be able to 
+# access with open your_talk.slides.html. To access the speaker notes, 
+# press s after the slides load and they should open in a new window.
