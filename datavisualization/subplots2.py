f8da658f6541f3397a9ae481a12ea7b690e6ebd9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-30 20:13:11 -0500

    Update

---
+++ datavisualization/subplots2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# generate some data
+x = np.arange(0, 100, 0.01)
+# indices = range(0,10000, 101)
+# x1 = np.take(x, indices)
+# x1 = x[::10].copy
+x1 = x[:1000]
+y = np.sin(x)
+cll = np.random.randn(len(x))
+
+# plot it
+fig = plt.figure(figsize=(20, 6))
+plt.tight_layout()
+plt.subplot2grid((2, 3), (0, 0), colspan=2,  xticklabels=[],
+                 xticks=[])
+plt.plot(x, y, "g-.")
+
+plt.subplot2grid((2, 3), (0, 2), rowspan=2)
+plt.scatter(x1, np.sin(x1), c=np.cos(x1), cmap='Paired',
+            edgecolors='none',
+            s=np.power(x1, 2))
+
+plt.subplot2grid((2, 3), (1, 0), colspan=2)
+plt.scatter(x, y, c=cll, cmap='Accent',
+            edgecolors='none',
+            s=np.power(x, 1.5))
+plt.show()
