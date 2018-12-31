a7ffc52ce8a1f23ce929fb2f2734b366bf1b8c4f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-12-31 05:03:18 -0500

    Add new file

---
+++ datavisualization/axis_ticks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt 
+from matplotlib import ticker
+import numpy as np 
+
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+plt.figure(figsize=(20, 60))
+plt.tight_layout()
+
+ax1 = plt.subplot(231)
+plt.setp(ax1.get_xticklabels(), fontsize=12)
+ax1.yaxis.set_ticks_position('right')
+plt.plot(x, y)
+plt.xlim(0.01, 8.0)
+
+# sharex and sharey means sharing axis limit
+ax2 = plt.subplot(232, yticks=[], sharex=ax1)
+plt.setp(ax2.get_yticklabels(), visible=False)
+plt.plot(x, y)
+
+
+
