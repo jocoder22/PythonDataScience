c234fecfed927dc468d8f1c6eba0c6a1905a0d66
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-02 04:30:45 -0500

    Update: add axis parameters

--- datavisualization/advancedplots/lines.py
+++ datavisualization/advancedplots/lines.py
@@ -11,14 +11,17 @@ plt.plot(x, y)
 
 # adding lines
 plt.axhline(0.5, color='r')
-plt.axhline(0.0, color='m', xmax=0.75, xmin=0.5)
+plt.axhline(0.0, color='m', xmin=0.5, xmax=0.75)
 plt.axvline(0.9, color='g', linestyle="--")
-plt.axvline(4.0, color='r', linestyle="--", ymax=0.75, ymin=0.3)
-plt.grid(color='c', linestyle='-.')
+plt.axvline(4.0, color='r', linestyle="--", ymin=0.3, ymax=0.75,)
+
+# different axis parameters, can use axis='both' for both axis of the grid
+plt.grid(color='c', linestyle='-.', axis='y')
+plt.grid(color='y', linestyle=':', axis='x')
 
 # Adding span
 plt.axhspan(-0.15,0.25, color='k', alpha=0.4)
-plt.axhspan(-0.75,-0.5, color='y', alpha=0.6, xmax=0.8, xmin=0.3)
+plt.axhspan(-0.75,-0.5, color='y', alpha=0.6, xmin=0.3, xmax=0.8)
 
 
 plt.show()
