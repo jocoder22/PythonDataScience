fc7185c334e72ed8be86570a409d836bb3d98c54
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-02 04:24:09 -0500

    Update

--- datavisualization/advancedplots/lines.py
+++ datavisualization/advancedplots/lines.py
@@ -8,20 +8,19 @@ x = np.arange(0, 10, 0.01)
 y = np.sin(x)
 
 plt.plot(x, y)
+
+# adding lines
 plt.axhline(0.5, color='r')
 plt.axhline(0.0, color='m', xmax=0.75, xmin=0.5)
 plt.axvline(0.9, color='g', linestyle="--")
 plt.axvline(4.0, color='r', linestyle="--", ymax=0.75, ymin=0.3)
-plt.grid()
+plt.grid(color='c', linestyle='-.')
 
 # Adding span
+plt.axhspan(-0.15,0.25, color='k', alpha=0.4)
+plt.axhspan(-0.75,-0.5, color='y', alpha=0.6, xmax=0.8, xmin=0.3)
 
-# adding lines
-np.arange(1,10,0.1)
 
-
-# plt.axhspan(-0.5,0.5, color='k', alpha=0.4)
-# xmin=0.2 , xmax=0.8
 
 
 # plt.grid()
