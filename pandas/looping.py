4def6397d59e9f54dba5349d2263a47c2c728072
Okigbo <okigbookey@gmail.com>
2019-01-04 12:08:43 -0500

    Update

--- pandas/looping.py
+++ pandas/looping.py
@@ -24,3 +24,35 @@ print(data)
 
 data['Total'] = data['height'].apply(lambda x: x + x)
 print(data)
+
+
+newdict = {}
+# Iterate over the columns in DataFrame
+for entry in data:
+    if entry in newdict.keys():
+        newdict[entry] += 1
+    else:
+        newdict[entry] = 1
+    print(entry)
+# Print the populated dictionary
+print(newdict)
+
+
+
+
+
+# Initialize an empty dictionary: counts
+counts = {}
+# Iterate over the file chunk by chunk
+for chunk in pd.read_csv('people.csv', chunksize=1):
+
+    # Iterate over the column 'name' in  the DataFrame
+    for entry in chunk['name']:
+        if entry in counts.keys():
+            counts[entry] += 1
+        else:
+            counts[entry] = 1
+
+# Print the populated dictionary
+print(counts)
+
