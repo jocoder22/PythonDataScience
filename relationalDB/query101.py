d2e3d9635c58e761caf75d54501ff7d1555e74ff
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-05 11:15:05 -0500

    Update: Using context manager

--- relationalDB/query101.py
+++ relationalDB/query101.py
@@ -16,9 +16,19 @@ rs = con.execute("SELECT * FROM Album")
 # Save results of the query to DataFrame: df
 df = pd.DataFrame(rs.fetchall())
 
-
 # Close connection
 con.close()
 
 # Print head of DataFrame df
 print(df.head())
+
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT LastName, Title FROM Employee")
+    df = pd.DataFrame(rs.fetchmany(size=5))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
