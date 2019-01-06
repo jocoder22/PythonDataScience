9876d7e149ce12e6b93a73de3d9f745218a9d1e8
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-06 06:54:28 -0500

    Add new file explore.py

--- importingData/localData/jsonfile.py
+++ importingData/localData/jsonfile.py
@@ -10,3 +10,13 @@ for key, value in jsonData.items():
     print(f'{key} : {value}')
 
 
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
+
+
+
