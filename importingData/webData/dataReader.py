6123f35d01824c90673d9331b3a7f5b62e81166b
Okigbo <okigbookey@gmail.com>
2019-01-07 14:07:26 -0500

    Update

--- importingData/webData/dataReader.py
+++ importingData/webData/dataReader.py
@@ -11,3 +11,9 @@ apple = pdr.get_data_yahoo(symbol, starttime, endtime)
 type(apple)
 # <class 'pandas.core.frame.DataFrame'>
 apple.to_csv('apple.csv')
+
+symbol2 = '^GSPC'
+starttime2 = datetime.datetime(2018, 1, 1)
+endtime2 = datetime.datetime(2019, 1, 7)
+sp500 = pdr.get_data_yahoo(symbol2, starttime2, endtime2)
+sp500.to_csv('sp500.csv')
