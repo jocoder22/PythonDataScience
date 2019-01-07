331b3ee9b8900b5f0b3150223bbd35f948bc24e9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-06 20:25:24 -0500

    Update

--- cleaningData/plotExplore.py
+++ cleaningData/plotExplore.py
@@ -45,3 +45,20 @@ plt.show()
 
 
 
+g1800s = pd.read_csv('g1800s.csv')
+# Create the scatter plot
+g1800s.plot(kind='scatter', x='1800', y='1899')
+
+# Specify axis labels
+plt.xlabel('Life Expectancy by Country in 1800')
+plt.ylabel('Life Expectancy by Country in 1899')
+
+# Specify axis limits
+plt.xlim(20, 55)
+plt.ylim(20, 55)
+
+# Display the plot
+plt.show()
+
+
+
