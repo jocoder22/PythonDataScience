6b8d5ccf555505b74206f91dd326c68bfc62d339
Okigbo <okigbookey@gmail.com>
2019-01-08 18:18:19 -0500

    Update

--- DataFrameManipulataion/groupby.py
+++ DataFrameManipulataion/groupby.py
@@ -48,3 +48,28 @@ print(aggregated.loc[:, ('age', 'max')])
 
 # Print the median fare in each class
 print(aggregated.loc[:, ('fare', 'median')])
+
+
+# Read the CSV file into a DataFrame and sort the index: gapminder
+gapminder = pd.read_csv('gapminder.csv', index_col=[
+                        'Year', 'region', 'Country']).sort_index()
+
+# Group gapminder by 'Year' and 'region': by_year_region
+by_year_region = gapminder.groupby(level=['Year', "region"])
+
+# Define the function to compute spread: spread
+
+
+def spread(series):
+    return series.max() - series.min()
+
+
+# Create the dictionary: aggregator
+aggregator = {'population': 'sum', 'child_mortality': 'mean', 'gdp': spread}
+
+# Aggregate by_year_region using the dictionary: aggregated
+aggregated = by_year_region.agg(aggregator)
+
+# Print the last 6 entries of aggregated
+aggregated.tail(6)
+print(aggregated.tail(6))
