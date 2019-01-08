46d5456fa6e25bea8d3e145de7cf17e42409134b
Okigbo <okigbookey@gmail.com>
2019-01-08 18:17:15 -0500

    Update

--- DataFrameManipulataion/groupby.py
+++ DataFrameManipulataion/groupby.py
@@ -20,3 +20,31 @@ count_mult = by_mult.survived.count()
 
 # Print count_mult
 print(count_mult)
+
+
+# Read life_fname into a DataFrame: life
+life = pd.read_csv(life_fname, index_col='Country')
+
+# Read regions_fname into a DataFrame: regions
+regions = pd.read_csv(regions_fname, index_col='Country')
+
+# Group life by regions['region']: life_by_region
+life_by_region = life.groupby(regions['region'])
+
+# Print the mean over the '2010' column of life_by_region
+print(life_by_region['2010'].mean())
+
+# Group titanic by 'pclass': by_class
+by_class = titanic.groupby('pclass')
+
+# Select 'age' and 'fare'
+by_class_sub = by_class[['age', 'fare']]
+
+# Aggregate by_class_sub by 'max' and 'median': aggregated
+aggregated = by_class_sub.agg(['max', 'median'])
+
+# Print the maximum age in each class
+print(aggregated.loc[:, ('age', 'max')])
+
+# Print the median fare in each class
+print(aggregated.loc[:, ('fare', 'median')])
