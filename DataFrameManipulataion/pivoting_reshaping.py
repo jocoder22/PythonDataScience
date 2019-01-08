5732e425cae3186f45d3f9a8b58110254f74c9e0
Okigbo <okigbookey@gmail.com>
2019-01-08 18:10:39 -0500

    Update

--- DataFrameManipulataion/pivoting_reshaping.py
+++ DataFrameManipulataion/pivoting_reshaping.py
@@ -27,3 +27,39 @@ pivot = users.pivot(index='weekday', columns='city')
 
 # Print the pivoted DataFrame
 print(pivot)
+
+
+######################### pivot table
+# Create the DataFrame with the appropriate pivot table: by_city_day
+by_city_day = users.pivot_table(index='weekday', columns='city')
+
+# Print by_city_day
+print(by_city_day)
+
+
+# Use a pivot table to display the count of each column: count_by_weekday1
+count_by_weekday1 = users.pivot_table(index='weekday', aggfunc='count')
+
+# Print count_by_weekday
+print(count_by_weekday1)
+
+# Replace 'aggfunc='count'' with 'aggfunc=len': count_by_weekday2
+count_by_weekday2 = users.pivot_table(index='weekday', aggfunc=len)
+
+# Verify that the same result is obtained
+print('==========================================')
+print(count_by_weekday1.equals(count_by_weekday2))
+
+
+# Create the DataFrame with the appropriate pivot table: signups_and_visitors
+signups_and_visitors = users.pivot_table(index='weekday', aggfunc=sum)
+
+# Print signups_and_visitors
+print(signups_and_visitors)
+
+# Add in the margins: signups_and_visitors_total
+signups_and_visitors_total = users.pivot_table(
+    index='weekday', aggfunc=sum, margins=True)
+
+# Print signups_and_visitors_total
+print(signups_and_visitors_total)
