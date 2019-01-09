1be9ab85d1411795a4fca9d16d4224b432c9686b
Okigbo <okigbookey@gmail.com>
2019-01-09 18:09:37 -0500

    Update

---
+++ DataFrameManipulation/indexing.py
@@ -0,0 +1,44 @@
+
+# Import pandas
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Reindex weather1 using the list year: weather2
+weather2 = weather1.reindex(year)
+
+# Print weather2
+print(weather2)
+
+# Reindex weather1 using the list year with forward-fill: weather3
+weather3 = weather1.reindex(year).ffill()
+
+# Print weather3
+print(weather3)
+
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
+
+
+# Import pandas
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
