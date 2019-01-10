5e61b2e7cd9c660c49582ed49091aa10f2ddf521
Okigbo <okigbookey@gmail.com>
2019-01-10 10:39:57 -0500

    update

---
+++ importingData/webData/importEngWords.py
@@ -0,0 +1,27 @@
+import requests
+from itertools import permutations
+
+url = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
+
+respond = requests.get(url)
+
+words = respond.text
+
+print(type(words))
+
+mylistword = words.split('\n')
+print(mylistword[:11])
+
+print('2' in mylistword)
+
+
+def formWord(word):
+    permutlist = []
+    for s in permutations(word):
+        new = ''.join(s)
+        if new in mylistword:
+            permutlist.append(new)
+
+    print(permutlist)
+
+formWord('sicd')
