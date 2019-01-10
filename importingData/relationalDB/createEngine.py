6769fa421ecd85c2c5e68a36cfb830e8a5841107
Okigbo <okigbookey@gmail.com>
2019-01-10 09:35:02 -0500

    Update

--- importingData/relationalDB/createEngine.py
+++ importingData/relationalDB/createEngine.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # Import necessary module
-from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_engine, MetaData, Table
 
+path = 'C:/Users/okigboo/Desktop/PythonDataScience/importingData/webData'
+os.chdir(path)
 # Create engine: engine
-engine = create_engine('sqlite:///Chinook.sqlite')
+engine = create_engine('sqlite:///survey.db')
 
 # Save the table names to a list: table_names
 table_names = engine.table_names()
 
 # Print the table names to the shell
-print(table_names)
+print(table_names)   # ['Person', 'Site', 'Survey', 'Visited']
 
+
+metadata = MetaData()
+person = Table('Person', metadata, autoload=True, autoload_with=engine)
+
+print(repr(person))
+# Table('Person', MetaData(bind=None), Column('id', TEXT(), table=<Person>), 
+#           Column('personal', TEXT(), table=<Person>), 
+#           Column('family', TEXT(), table=<Person>), schema=None)
 
 
 
