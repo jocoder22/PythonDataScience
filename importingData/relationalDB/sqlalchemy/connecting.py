6f75717663161943784d953b8ead963fabb50a67
Okigbo <okigbookey@gmail.com>
2019-01-10 20:14:50 -0500

    Update:

--- importingData/relationalDB/sqlalchemy/connecting.py
+++ importingData/relationalDB/sqlalchemy/connecting.py
@@ -13,9 +13,9 @@ connection = engine.connect()
 
 metadata = MetaData()
 
-print(engine.table_names())
+print(engine.table_names())  # ['Person', 'Site', 'Survey', 'Visited']
 
-survey = Table('Survey', metadata, autoload=True, autoload_with=engine)
+survey = Table('Visited', metadata, autoload=True, autoload_with=engine)
 
 ssmt = select([survey])
 
@@ -27,21 +27,21 @@ print(results[0])
 print(results[0].keys())
 
 
-# Get the first row of the results by using an index: first_row
-first_row = results[0]
+# # Get the first row of the results by using an index: first_row
+# first_row = results[0]
 
-# Print the first row of the results
-print(first_row)
+# # Print the first row of the results
+# print(first_row)
 
-# Print the first column of the first row by using an index
-print(first_row[0])
+# # Print the first column of the first row by using an index
+# print(first_row[0])
 
-# Print the 'family' column of the first row by using its name
-print(first_row['quant'])
+# # Print the 'family' column of the first row by using its name
+# print(first_row['quant'])
 
 
-# Add a where clause to filter the results to only those for lake
-ssmt = ssmt.where(survey.columns.person == 'lake')
+# # Add a where clause to filter the results to only those for lake
+# ssmt = ssmt.where(survey.columns.person == 'lake')
 
-# Execute the query to retrieve all the data returned: results
-results = connection.execute(ssmt).fetchall()
+# # Execute the query to retrieve all the data returned: results
+# results = connection.execute(ssmt).fetchall()
---
+++ importingData/relationalDB/sqlalchemy/joins.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Import necessary module
+import os
+from sqlalchemy import create_engine, MetaData, Table, select,  and_
+
+path = 'C:/Users/okigboo/Desktop/PythonDataScience/importingData/webData'
+os.chdir(path)
+# Create engine: engine
+engine = create_engine('sqlite:///survey.db')
+connection = engine.connect()
+
+metadata = MetaData()
+
+survey = Table('Survey', metadata, autoload=True, autoload_with=engine)
+
+ssmt = select([survey])
+
+############# join predefined join table
+# Build a statement to join survey and state_fact tables: stmt
+stmt = select([survey.columns.pop2000, state_fact.columns.abbreviation])
+
+# Execute the statement and get the first result: result
+result = connection.execute(stmt).first()
+
+# Loop over the keys in the result object and print the key and value
+for key in result.keys():
+    print(key, getattr(result, key))
+
+
+# Build a statement to select the survey and state_fact tables: stmt
+stmt = select([survey, state_fact])
+
+# Add a select_from clause that wraps a join for the survey and state_fact
+# tables where the survey state column and state_fact name column match
+stmt = stmt.select_from(
+    survey.join(state_fact, survey.columns.state == state_fact.columns.name))
+
+# Execute the statement and get the first result: result
+result = connection.execute(stmt).first()
+
+# Loop over the keys in the result object and print the key and value
+for key in result.keys():
+    print(key, getattr(result, key)
+
+
+          # Start a while loop checking for more results
+          while more_results:
+          # Fetch the first 50 results from the ResultProxy: partial_results
+          partial_results=results_proxy.fetchmany(50)
+
+          # if empty list, set more_results to False
+          if partial_results == []:
+          more_results=False
+
+          # Loop over the fetched records and increment the count for the state
+          for row in partial_results:
+          if row.state in state_count:
+              state_count[row.state] += 1
+          else:
+              state_count[row.state]=1
+
+          # Close the ResultProxy, and thus the connection
+          results_proxy.close()
+
+          # Print the count by state
+          print(state_count)
