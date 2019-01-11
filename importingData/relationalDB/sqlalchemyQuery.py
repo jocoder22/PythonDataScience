18515296aef699ee1e284415efe90ced49c82ff3
Okigbo <okigbookey@gmail.com>
2019-01-10 19:52:23 -0500

    Add new file joins.py

--- importingData/relationalDB/sqlalchemyQuery.py
+++ importingData/relationalDB/sqlalchemyQuery.py
@@ -2,55 +2,6 @@
 
 
 
-############# join predefined join table
-# Build a statement to join census and state_fact tables: stmt
-stmt = select([census.columns.pop2000, state_fact.columns.abbreviation])
-
-# Execute the statement and get the first result: result
-result = connection.execute(stmt).first()
-
-# Loop over the keys in the result object and print the key and value
-for key in result.keys():
-    print(key, getattr(result, key))
-
-
-# Build a statement to select the census and state_fact tables: stmt
-stmt = select([census, state_fact])
-
-# Add a select_from clause that wraps a join for the census and state_fact
-# tables where the census state column and state_fact name column match
-stmt = stmt.select_from(
-    census.join(state_fact, census.columns.state == state_fact.columns.name))
-
-# Execute the statement and get the first result: result
-result = connection.execute(stmt).first()
-
-# Loop over the keys in the result object and print the key and value
-for key in result.keys():
-    print(key, getattr(result, key)
-
-
-          # Start a while loop checking for more results
-          while more_results:
-          # Fetch the first 50 results from the ResultProxy: partial_results
-          partial_results=results_proxy.fetchmany(50)
-
-          # if empty list, set more_results to False
-          if partial_results == []:
-          more_results=False
-
-          # Loop over the fetched records and increment the count for the state
-          for row in partial_results:
-          if row.state in state_count:
-              state_count[row.state] += 1
-          else:
-              state_count[row.state]=1
-
-          # Close the ResultProxy, and thus the connection
-          results_proxy.close()
-
-          # Print the count by state
-          print(state_count)
 
 
 
