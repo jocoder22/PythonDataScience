2beb4d7bf732711d8485c41c6234fa109ce406c1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-12 08:10:42 -0500

    Update

--- images/pillow_1.py
+++ images/pillow_1.py
@@ -1,6 +1,13 @@
 import os
-from PIL import Image
+from PIL import Image as img
 
 path = 'C:\\Users\\Jose\\Pictures\\'
 os.chdir(path)
 print(os.listdir())
+'light.jpg' in os.listdir()
+myfiles = os.listdir()
+myfiles.index('light.jpg')
+
+image1 = img.open('light.jpg')
+image1.show()
+image1.save('light2.png')
