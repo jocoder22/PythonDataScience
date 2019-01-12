593d12dd00fd9a7cde9f82364054b736fce25629
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-12 08:25:27 -0500

    Update

--- images/pillow_1.py
+++ images/pillow_1.py
@@ -11,3 +11,9 @@ myfiles.index('light.jpg')
 image1 = img.open('light.jpg')
 image1.show()
 image1.save('light2.png')
+
+for file in os.listdir():
+    if file.endswith('.jpg'):
+        image = img.open(file)
+        fname, fext = os.path.splitext(file)
+        image.save(f'Newfile/{fname}.png')
