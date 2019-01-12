68a5bf4caedea08cca599404ce90f88a2088110c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-12 13:07:54 -0500

    Update

--- datavisualization/bokeh/simplyplot.py
+++ datavisualization/bokeh/simplyplot.py
@@ -26,3 +26,44 @@ colors = {'AF':'yellow','ASI':'magenta', 'EUR':'cyan',
 
 
 xl['ccmap'] = xl['Continent'].map(colors)
+
+
+fertility = xl['fertility']
+female_literacy = xl['female literacy']
+
+latinos = xl['Continent'] == 'LAT'
+fertility_latinamerica = xl.fertility[latinos]
+female_literacy_latinamerica = xl['female literacy'][latinos]
+
+africans = xl['Continent'] == 'AF'
+fertility_africa = xl.fertility[africans]
+female_literacy_africa = xl['female literacy'][africans]
+
+# Create the figure: p
+p = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to the figure p
+p.circle(fertility, female_literacy)
+
+# Call the output_file() function and specify the name of the file
+output_file('fert_lit.html')
+
+# Display the plot
+
+show(p)
+
+
+# Create the figure: p
+p = figure(x_axis_label='fertility', y_axis_label='female_literacy (% population)')
+
+# Add a circle glyph to the figure p
+p.circle(fertility_latinamerica,female_literacy_latinamerica)
+
+# Add an x glyph to the figure p
+p.x(fertility_africa,female_literacy_africa)
+
+# Specify the name of the file
+output_file('fert_lit_separate.html')
+
+# Display the plot
+show(p)
