295b5f3e6bfd04f2163ad67f42977ccc488c474d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-12 11:29:44 -0500

    Update

--- images/pillow_1.py
+++ images/pillow_1.py
@@ -1,5 +1,6 @@
 import os
-from PIL import Image as img
+from PIL import ImageFilter, Image as img
+
 
 path = 'C:\\Users\\Jose\\Pictures\\'
 os.chdir(path)
@@ -8,12 +9,17 @@ print(os.listdir())
 myfiles = os.listdir()
 myfiles.index('light.jpg')
 
-image1 = img.open('light.jpg')
-image1.show()
-image1.save('light2.png')
+images2 = img.open('light.jpg')
+images2.show()
+images2.save('light2.png')
 
+newsize = (450, 450)
 for file in os.listdir():
     if file.endswith('.jpg'):
         image = img.open(file)
         fname, fext = os.path.splitext(file)
-        image.save(f'Newfile/{fname}.png')
+        image.thumbnail(newsize)
+        image.save(f'Newfile/{fname}_resized.{fext}')
+
+images2.rotate(90).save('Newfile/light_r.jpg')
+images2.convert(mode='L').save('Newfile/light_l.jpg')
