f4a69cb5414a007fbecc220d7e8a706965348eb5
Okigbo <okigbookey@gmail.com>
2019-01-14 20:10:36 -0500

    Update

--- prob_stats/finalProject.py
+++ prob_stats/finalProject.py
@@ -151,3 +151,84 @@ for i in range(100):
 
 # Draw the plot again
 plt.show()
+
+
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+
+    return bs_replicates
+
+
+# Compute the Pearson correlation coefficients
+r_scandens = pearson_r(bd_parent_scandens, bd_offspring_scandens)
+r_fortis = pearson_r(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of Pearson r
+bs_replicates_scandens = draw_bs_pairs(
+    bd_parent_scandens, bd_offspring_scandens, pearson_r, size=1000)
+bs_replicates_fortis = draw_bs_pairs(
+    bd_parent_fortis, bd_offspring_fortis, pearson_r, size=1000)
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(bs_replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(bs_replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', r_scandens, conf_int_scandens)
+print('G. fortis:', r_fortis, conf_int_fortis)
+
+
+def heritability(parents, offspring):
+    """Compute the heritability from parent and offspring samples."""
+    covariance_matrix = np.cov(parents, offspring)
+    return covariance_matrix[0, 1] / covariance_matrix[0, 0]
+
+
+# Compute the heritability
+heritability_scandens = heritability(bd_parent_scandens,
+                                     bd_offspring_scandens)
+heritability_fortis = heritability(bd_parent_fortis,
+                                   bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of heritability
+replicates_scandens = draw_bs_pairs(
+    bd_parent_scandens, bd_offspring_scandens, heritability, size=1000)
+replicates_fortis = draw_bs_pairs(
+    bd_parent_fortis, bd_offspring_fortis, heritability, size=1000)
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(replicates_scandens, [2.5, 97.5])
+conf_int_fortis = np.percentile(replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', heritability_scandens, conf_int_scandens)
+print('G. fortis:', heritability_fortis, conf_int_fortis)
+
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted,
+                                      bd_offspring_scandens)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+# Print the p-value
+print('p-val =', p)
