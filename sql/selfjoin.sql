f426846656c6d80a09a2368efb04413024cbccdb
Okigbo <okigbookey@gmail.com>
2019-01-15 11:54:21 -0500

    Update

---
+++ sql/selfjoin.sql
@@ -0,0 +1,28 @@
+SELECT p1.country_code,
+       p1.size AS size2010,
+       p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON  p1.country_code = p2.country_code ;
+
+
+
+SELECT p1.country_code,
+       p1.size AS size2010,
+       p2.size AS size2015,
+       ((p2.size - p1.size)/p1.size * 100.0) AS growth_perc
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON  p1.country_code = p2.country_code 
+    AND p1.year = p2.year - 5;
+
+
+SELECT name, continent, code, surface_area,
+        -- first case
+    CASE WHEN surface_area > 2000000 THEN 'large'
+        -- second case
+        WHEN surface_area > 350000 THEN 'medium'
+        -- else clause + end
+        ELSE 'small' END
+        AS geosize_group
+FROM countries;
