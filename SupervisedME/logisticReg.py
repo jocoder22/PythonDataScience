6fdc2726413462cb82af668a21d1e2fc692dbf2c
Okigbo <okigbookey@gmail.com>
2019-01-18 17:31:29 -0500

    Update

--- SupervisedME/logisticReg.py
+++ SupervisedME/logisticReg.py
@@ -1,3 +1,4 @@
+from sklearn.preprocessing import Imputer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import roc_auc_score
@@ -125,3 +126,24 @@ pl.fit(X_train, y_train)
 accuracy = pl.score(X_test, y_test)
 print("\nAccuracy on sample data - numeric, no nans: ", accuracy)
 
+
+# Import the Imputer object
+
+# Create training and test sets using only numeric data
+X_train, X_test, y_train, y_test = train_test_split(sample_df[['numeric', 'with_missing']],
+                                                    pd.get_dummies(
+                                                        sample_df['label']),
+                                                    random_state=456)
+
+# Insantiate Pipeline object: pl
+pl = Pipeline([
+    ('imp', Imputer()),
+    ('clf', OneVsRestClassifier(LogisticRegression()))
+])
+
+# Fit the pipeline to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on sample data - all numeric, incl nans: ", accuracy)
