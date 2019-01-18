60726cdeea1f60104320bd2533eabd65de7ea005
Okigbo <okigbookey@gmail.com>
2019-01-18 18:09:01 -0500

    Update

--- SupervisedME/pipeline.py
+++ SupervisedME/pipeline.py
@@ -27,4 +27,26 @@ get_numeric_data = FunctionTransformer(
     lambda x: x[NUMERIC_COLUMNS], validate=False)
 
 
+# Complete the pipeline: pl
+pl = Pipeline([
+    ('union', FeatureUnion(
+        transformer_list=[
+            ('numeric_features', Pipeline([
+                ('selector', get_numeric_data),
+                ('imputer', Imputer())
+            ])),
+            ('text_features', Pipeline([
+                ('selector', get_text_data),
+                ('vectorizer', CountVectorizer())
+            ]))
+        ]
+    )),
+    ('clf', OneVsRestClassifier(LogisticRegression()))
+])
 
+# Fit to the training data
+pl.fit(X_train, y_train)
+
+# Compute and print accuracy
+accuracy = pl.score(X_test, y_test)
+print("\nAccuracy on budget dataset: ", accuracy)
