b00b58542d2b8691c8aee0724fb44b8a19faf8d1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-20 11:34:42 -0500

    Changes to be committed: 	new file:   keras_1.py 	modified:   ../../oneliner/oneline_1.py

--- oneliner/oneline_1.py
+++ oneliner/oneline_1.py
@@ -8,11 +8,12 @@ plt.style.use('ggplot')
 df = pd.read_csv('power.csv', sep=',')
 
 # this finds the columns that are binary
-[s for s in df if df[s].max == 1]
+[s for s in df if df[s].max() == 1]
 
 
 # thi finds the total number that is binary
 (df.max() == 1).sum()
+len([s for s in df if df[s].max() == 1])
 
 # this finds the columns that are of type float64
 [s for s in df if df[s].dtype == 'float64']
