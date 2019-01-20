ca2274c285df6f063663ae1dae32c1a2caab2572
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-01-20 11:29:21 -0500

    Update

---
+++ oneliner/oneline_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+plt.style.use('ggplot')
+
+
+df = pd.read_csv('power.csv', sep=',')
+
+# this finds the columns that are binary
+[s for s in df if df[s].max == 1]
+
+
+# thi finds the total number that is binary
+(df.max() == 1).sum()
+
+# this finds the columns that are of type float64
+[s for s in df if df[s].dtype == 'float64']
