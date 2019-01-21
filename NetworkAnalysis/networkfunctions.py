541d21ae409891a35c5d8d73b0149002445fe81a
Okigbo <okigbookey@gmail.com>
2019-01-21 17:50:38 -0500

    Update

--- NetworkAnalysis/networkfunctions.py
+++ NetworkAnalysis/networkfunctions.py
@@ -21,7 +21,6 @@ def nodes_with_m_nbrs(G, m):
 # six_nbrs = nodes_with_m_nbrs(T, 6)
 # print(six_nbrs)
 
-# Define path_exists()
 def path_exists(G, node1, node2):
     """
     This function checks whether a path exists between two nodes (node1, node2) in graph G.
@@ -42,3 +41,81 @@ def path_exists(G, node1, node2):
             print('Path exists between nodes {0} and {1}'.format(node1, node2))
             return True
             break
+
+        else:
+            visited_nodes.add(node)
+            queue.extend([n for n in neighbors if n not in visited_nodes])
+
+        # Check to see if the final element of the queue has been reached
+        if node == queue[-1]:
+            print('Path does not exist between nodes {0} and {1}'.format(
+                node1, node2))
+
+            # Place the appropriate return statement
+            return False
+
+
+
+# Define find_nodes_with_highest_deg_cent()
+def find_nodes_with_highest_deg_cent(G):
+
+    # Compute the degree centrality of G: deg_cent
+    deg_cent = nx.degree_centrality(G)
+
+    # Compute the maximum degree centrality: max_dc
+    max_dc = max(list(deg_cent.values()))
+
+    nodes = set()
+
+    # Iterate over the degree centrality dictionary
+    for k, v in deg_cent.items():
+
+        # Check if the current value has the maximum degree centrality
+        if v == max_dc:
+
+            # Add the current node to the set of nodes
+            nodes.add(k)
+
+    return nodes
+
+
+# Find the node(s) that has the highest degree centrality in T: top_dc
+top_dc = find_nodes_with_highest_deg_cent(T)
+print(top_dc)
+
+# Write the assertion statement
+for node in top_dc:
+    assert nx.degree_centrality(T)[node] == max(
+        nx.degree_centrality(T).values())
+
+
+# Define find_node_with_highest_bet_cent()
+def find_node_with_highest_bet_cent(G):
+
+    # Compute betweenness centrality: bet_cent
+    bet_cent = nx.betweenness_centrality(G)
+
+    # Compute maximum betweenness centrality: max_bc
+    max_bc = max(list(bet_cent.values()))
+
+    nodes = set()
+
+    # Iterate over the betweenness centrality dictionary
+    for k, v in bet_cent.items():
+
+        # Check if the current value has the maximum betweenness centrality
+        if v == max_bc:
+
+            # Add the current node to the set of nodes
+            nodes.add(k)
+
+    return nodes
+
+
+# Use that function to find the node(s) that has the highest betweenness centrality in the network: top_bc
+top_bc = find_node_with_highest_bet_cent(T)
+
+# Write an assertion statement that checks that the node(s) is/are correctly identified.
+for node in top_bc:
+    assert nx.betweenness_centrality(T)[node] == max(
+        nx.betweenness_centrality(T).values())
