709f4b1ec624e7ed5210f0d3016d62d4b8ba46a2
Okigbo <okigbookey@gmail.com>
2019-01-22 09:25:06 -0500

    Update

---
+++ NetworkAnalysis/subgraph2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+# plt.style.use('ggplot')
+import os
+import networkx as nx
+import nxviz as nz
+
+
+T = nx.erdos_renyi_graph(n=10, p=0.5, seed=123)
+
+nodes_of_interest1 = [2, 4]
+
+# Define get_nodes_and_nbrs()
+
+
+def get_nodes_and_nbrs(G, nodes_of_interest):
+    """
+    Returns a subgraph of the graph `G` with only the `nodes_of_interest` and their neighbors.
+    """
+    nodes_to_draw = []
+
+    # Iterate over the nodes of interest
+    for n in nodes_of_interest:
+
+        # Append the nodes of interest to nodes_to_draw
+        nodes_to_draw.append(n)
+
+        # Iterate over all the neighbors of node n
+        for nbr in G.neighbors(n):
+
+            # Append the neighbors of n to nodes_to_draw
+            if nbr not in nodes_to_draw:
+                nodes_to_draw.append(nbr)
+
+    return G.subgraph(nodes_to_draw)
+
+
+# Extract the subgraph with the nodes of interest: T_draw
+T_draw = get_nodes_and_nbrs(T, nodes_of_interest1)
+
+# Draw the subgraph to the screen
+nx.draw(T_draw, with_labels=True)
+plt.pause(2)
+plt.close()
