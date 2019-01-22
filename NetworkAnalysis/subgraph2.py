3ebaa88d5064e87b6514270da2ca463a261d70c8
Okigbo <okigbookey@gmail.com>
2019-01-22 11:23:41 -0500

    Update

--- NetworkAnalysis/subgraph2.py
+++ NetworkAnalysis/subgraph2.py
@@ -44,3 +44,26 @@ T_draw = get_nodes_and_nbrs(T, nodes_of_interest1)
 nx.draw(T_draw, with_labels=True)
 plt.pause(2)
 plt.close()
+
+
+# Extract the nodes of interest: nodes
+nodes = [n for n, d in T.nodes(data=True) if d['occupation'] == 'celebrity']
+
+# Create the set of nodes: nodeset
+nodeset = set(nodes)
+
+# Iterate over nodes
+for n in nodes:
+
+    # Compute the neighbors of n: nbrs
+    nbrs = T.neighbors(n)
+
+    # Compute the union of nodeset and nbrs: nodeset
+    nodeset = nodeset.union(nbrs)
+
+# Compute the subgraph using nodeset: T_sub
+T_sub = T.subgraph(nodeset)
+
+# Draw T_sub to the screen
+nx.draw(T_sub)
+plt.show()
