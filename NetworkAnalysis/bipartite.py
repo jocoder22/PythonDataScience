34e07f83291bbbfeea61b9117628639bed9c567f
Okigbo <okigbookey@gmail.com>
2019-01-22 19:18:45 -0500

    Update

--- NetworkAnalysis/bipartite.py
+++ NetworkAnalysis/bipartite.py
@@ -11,18 +11,19 @@ from networkx.algorithms import bipartite
 
 N = nx.Graph()
 N.add_nodes_from(['A', 'B', 'C', 'D', 'E', 'F', 'G'], bipartite=0)
-N.add_nodes_from(['Manu', 'Chelsea', 'Westham'], bipartite=1)
+N.add_nodes_from(['ManU', 'Chelsea', 'Westham'], bipartite=1)
 
-N.add_edges_from([('A', 'Manu'), ('C', 'Manu'),
-                  ('B', 'Manu'), ('E', 'Manu'), ('D', 'Manu'),
-                  ('B', 'Chelsea'), ('E', 'Chelsea'), ('F', 'Manu'),
+N.add_edges_from([('A', 'ManU'), ('C', 'ManU'),
+                  ('B', 'ManU'), ('E', 'ManU'), ('D', 'ManU'),
+                  ('B', 'Chelsea'), ('E', 'Chelsea'), ('F', 'ManU'),
                   ('G', 'Westham'), ('C', 'Chelsea'), ('A', 'Chelsea'),
                   ('D', 'Chelsea'), ('A', 'Westham'),
                   ('F', 'Westham'), ('C', 'Westham'), ('E', 'Westham')])
 
 nx.draw(N)
 plt.show()
-
+print(bipartite.is_bipartite(N))
+print(bipartite.sets(N))
 
 gg = nx.Graph()
 gg.add_edges_from([('C', 'F'), ('C', 'E'), ('B', 'F'), ('B', 'E'),
@@ -33,3 +34,4 @@ plt.show()
 
 
 print(bipartite.is_bipartite(gg))
+print(bipartite.sets(gg))
