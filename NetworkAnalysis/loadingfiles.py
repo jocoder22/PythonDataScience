69594311f23f0b0e396f364d43c8cd0a2d741c17
Okigbo <okigbookey@gmail.com>
2019-01-23 18:30:42 -0500

    Update

--- NetworkAnalysis/loadingfiles.py
+++ NetworkAnalysis/loadingfiles.py
@@ -47,3 +47,16 @@ df2.rename({"source": "a", "target": "b"}, axis='columns', inplace=True)
 print(df2.head())
 Gf2 = nx.from_pandas_edgelist(df2, 'a', 'b', edge_attr=['Weight', 'relation'])
 print(Gf2.edges(data=True))
+
+
+# G = nx.MultiGraph()
+# G.add_node('A', role='manager')
+# G.add_edge('A', 'B', relation='friend')
+# G.add_edge('A', 'C', relation='business partner')
+# G.add_edge('A', 'B', relation='classmate')
+# G.node['A']['role'] = 'team member'
+# G.node['B']['role'] = 'engineer'
+
+Gk = nx.karate_club_graph()
+nx.draw(Gk)
+plt.plot
