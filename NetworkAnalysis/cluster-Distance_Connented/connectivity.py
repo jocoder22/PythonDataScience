ea64956d79c18822eae710b77dff0f25a500d133
Okigbo <okigbookey@gmail.com>
2019-01-24 17:30:03 -0500

    Updated  Changes to be committed: 	new file:   connectivity.py

---
+++ NetworkAnalysis/cluster-Distance_Connented/connectivity.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+# plt.style.use('ggplot')
+import os
+import networkx as nx
+import nxviz as nv
+
+G = nx.karate_club_graph()
+
+# Undirected graph: 
+# Graph is connected is there is a path between pairs of nodes
+print(nx.is_connected(G)) # True
+
+
+#### connected components
+print(nx.number_connected_components(G))
+
+
+####### show the connected components
+print(sorted(nx.connected_components(G)))
+
+
+########### show the component which a node belongs to
+print(sorted(nx.node_connected_component(G, 4)))
+
+
+
+
+# Directed Graph: connectivity
+# Strongly connected, there is path from a -> b and path form b -> a
+# print(nx.is_strongly_connected(G))
+
+# Weakly connected the result if the graph becomes Undirected
+print(nx.is_weakly_connected(G))
+
+
+############ connected components
+print(sorted(nx.strongly_connected_components(G)))
