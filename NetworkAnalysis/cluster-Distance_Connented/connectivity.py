40aa29c6f66c1538bcfa185482d82ae4af439302
Okigbo <okigbookey@gmail.com>
2019-01-24 18:13:14 -0500

    Updatae  Changes to be committed: 	modified:   connectivity.py 	modified:   robustness.py

--- NetworkAnalysis/cluster-Distance_Connented/connectivity.py
+++ NetworkAnalysis/cluster-Distance_Connented/connectivity.py
@@ -26,11 +26,13 @@ print(sorted(nx.connected_components(G)))
 print(sorted(nx.node_connected_component(G, 4)))
 
 
-
+G = nx.DiGraph()
+G.add_edges_from([('A', 'B'), ('C', 'A'), ('A', 'E'), ('G', 'A'), ('A', 'N'), ('B', 'C'), ('D', 'B'), ('B', 'E'), ('C', 'D'), ('E', 'C'), ('D', 'E'), ('E', 'D'), ('F', 'G'), ('I', 'F'), ('J', 'F'),
+                  ('H', 'G'), ('I', 'G'), ('G', 'J'), ('I', 'H'), ('H', 'I'), ('I', 'J'), ('J', 'O'), ('O', 'J'), ('K', 'M'), ('K', 'L'), ('O', 'K'), ('O', 'L'), ('N', 'L'), ('L', 'M'), ('N', 'O')])
 
 #########################  Directed Graph: connectivity
 # Strongly connected, there is path from a -> b and path form b -> a
-# print(nx.is_strongly_connected(G))
+print(nx.is_strongly_connected(G))
 
 # Weakly connected the result if the graph becomes Undirected
 print(nx.is_weakly_connected(G))
--- NetworkAnalysis/cluster-Distance_Connented/robustness.py
+++ NetworkAnalysis/cluster-Distance_Connented/robustness.py
@@ -33,3 +33,43 @@ plt.show()
 nx.draw_spectral(G, with_labels=True)
 plt.show()
 
+
+# Robust networks have large minimum_edge_cut and mininum_node_cut numbers
+
+
+G = nx.DiGraph()
+G.add_edges_from([('A', 'B'), ('C', 'A'), ('A', 'E'), ('G', 'A'), ('A', 'N'), ('B', 'C'), ('D', 'B'), ('B', 'E'), ('C', 'D'), ('E', 'C'), ('D', 'E'), ('E', 'D'), ('F', 'G'), ('I', 'F'), ('J', 'F'),
+                  ('H', 'G'), ('I', 'G'), ('G', 'J'), ('I', 'H'), ('H', 'I'), ('I', 'J'), ('J', 'O'), ('O', 'J'), ('K', 'M'), ('K', 'L'), ('O', 'K'), ('O', 'L'), ('N', 'L'), ('L', 'M'), ('N', 'O')])
+
+######## Directed Graphs
+# To print all the path between two nodes
+print(sorted(nx.all_simple_paths(G, 'A', 'L')))
+
+# How many nodes to remove
+print(nx.node_connectivity(G, 'A', 'L'))
+
+
+# which nodes to remove
+print(nx.minimum_node_cut(G, 'A', 'L'))
+
+
+# How edges to remove
+print(nx.edge_connectivity(G, 'A', 'L'))
+
+
+# which edges to remove
+print(nx.minimum_edge_cut(G, 'A', 'L'))
+
+
+# Blocking from H to O
+# How many nodes to remove
+print('Nodes to remove from H to 0: ', nx.node_connectivity(G, 'H', 'O'))
+
+# which nodes to remove
+print('Which Nodes to remove from H to 0: ', nx.minimum_node_cut(G, 'H', 'O'))
+
+# How edges to remove
+print('Edges to remove from H to 0: ', nx.edge_connectivity(G, 'H', 'O'))
+
+# which edges to remove
+print('Which Edges to remove from H to 0: ', nx.minimum_edge_cut(G, 'H', 'O'))
