7a2f94d8cbf1bf2777fb635742a3fc62eba703be
Okigbo <okigbookey@gmail.com>
2019-01-25 17:40:50 -0500

    Update

--- NetworkAnalysis/NodeAnalysis/betweeness.py
+++ NetworkAnalysis/NodeAnalysis/betweeness.py
@@ -8,5 +8,12 @@ G = nx.karate_club_graph()
 # Betweeeness
 # Important nodes are once that connect other nodes
 bet_c = nx.betweenness_centrality(G, normalized=True, endpoints=False)
+bet_c10 = nx.betweenness_centrality(G, normalized=True, endpoints=False , k=10)
 print(sorted(((v, k) for k, v in bet_c.items()), reverse=True)[:5])
 print(sorted(bet_c.items(), reverse=True, key=operator.itemgetter(1))[:5])
+print(sorted(bet_c10.items(), reverse=True, key=operator.itemgetter(1))[:5])
+
+
+# important edge show often on nodes shortest path
+bet_ce = nx.edge_betweenness_centrality(G, normalized=True)
+print(sorted(bet_ce.items(), reverse=True, key=operator.itemgetter(1))[:5])
---
+++ NetworkAnalysis/NodeAnalysis/pagerank.py
@@ -0,0 +1,3 @@
+import os
+import networkx as nx
+import nxviz as nv
