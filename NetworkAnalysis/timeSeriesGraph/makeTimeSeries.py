ac0e5ea3d4d2096a824b27f1f7679f73eb41b9cc
Okigbo <okigbookey@gmail.com>
2019-01-30 15:02:56 -0500

    Update

--- NetworkAnalysis/timeSeriesGraph/makeTimeSeries.py
+++ NetworkAnalysis/timeSeriesGraph/makeTimeSeries.py
@@ -54,3 +54,57 @@ for month in months:
     Gs.append(G)
 
 print(len(Gs))
+
+
+# Instantiate a list of graphs that show edges added: added
+added = []
+# Instantiate a list of graphs that show edges removed: removed
+removed = []
+# Here's the fractional change over time
+fractional_changes = []
+window = 1
+i = 0
+
+for i in range(len(Gs) - window):
+    g1 = Gs[i]
+    g2 = Gs[i + window]
+
+    # Compute graph difference here
+    added.append(nx.difference(g2, g1))
+    removed.append(nx.difference(g1, g2))
+
+    # Compute change in graph size over time
+    fractional_changes.append(
+        (len(g2.edges()) - len(g1.edges())) / len(g1.edges()))
+
+# Print the fractional change
+print(fractional_changes)
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+# Plot the number of edges added over time
+edges_added = [len(g.edges()) for g in added]
+plot1 = ax1.plot(edges_added, label='added', color='orange')
+
+# Plot the number of edges removed over time
+edges_removed = [len(g.edges()) for g in removed]
+plot2 = ax1.plot(edges_removed, label='removed', color='purple')
+
+# Set yscale to logarithmic scale
+ax1.set_yscale('log')
+ax1.legend()
+
+# 2nd axes shares x-axis with 1st axes object
+ax2 = ax1.twinx()
+
+# Plot the fractional changes over time
+plot3 = ax2.plot(fractional_changes, label='fractional change', color='green')
+
+# Here, we create a single legend for both plots
+lines1, labels1 = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax2.legend(lines1 + lines2, labels1 + labels2, loc=0)
+plt.axhline(0, color='green', linestyle='--')
+plt.show()
