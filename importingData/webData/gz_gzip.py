997ceb1c8f6655744be3cefb62b7ae4af1ecb0d7
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-16 23:15:11 -0500

    Update

--- importingData/webData/gz_gzip.py
+++ importingData/webData/gz_gzip.py
@@ -14,16 +14,19 @@ os.chdir(path)
 url_1 = 'https://stacks.stanford.edu/file/druid:py883nd2578/NJ-clean.csv.gz'
 url = 'https://stacks.stanford.edu/file/druid:py883nd2578/WY-clean.csv.gz'
 
-r = requests.get(url)
+r = requests.get(url_1)
 
 rfile = io.BytesIO(r.content)
+tts = []
 
 with gzip.GzipFile(fileobj=rfile) as gzfile:
-    df = pd.read_csv(gzfile)
+    for chunk in pd.read_csv(gzfile, chunksize=100000):
+        tts.append(chunk)
+
+df = pd.concat(tts)
 
 print(df.shape, df.columns, sep='\n')
 
-del r; del rfile; del gzfile
-
 print(sys.getsizeof(r), sys.getsizeof(rfile), sys.getsizeof(gzfile), sys.getsizeof(df), sep='\n')
 
+del r; del rfile; del gzfile; del df
