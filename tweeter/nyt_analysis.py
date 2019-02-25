1bf634d55e73109f3dc4d976c1c57446b509d9c8
Okigbo <okigbookey@gmail.com>
2019-02-25 14:56:05 -0500

    Update

--- tweeter/nyt_analysis.py
+++ tweeter/nyt_analysis.py
@@ -17,7 +17,7 @@ excludePunt = set(string.punctuation)
 excludePunt.update(('"', "'"))
 stopword = set(stopwords.words("english"))
 stopword.update(("said", "to", "th", "e", "cc", "subject", "http", "from", "new", "time", "times", "york",
-             "sent", "ect", "u", "fwd", "w", "n", "s", "www", "com", "de", "one", "may", "home",
+             "sent", "ect", "u", "fwd", "w", "n", "s", "www", "com", "de", "one", "may", "home", "u", "la",
              "advertisement", "information", "service", "—", "year", "would"))
 wordlemm = WordNetLemmatizer()
 wordporter = SnowballStemmer("english")
@@ -42,8 +42,8 @@ data['scores'] = data.News_content.apply(sid.polarity_scores).apply(lambda x: x[
 def cleantext(text, stop):
     text = str(text).rstrip()
     stopfree = " ".join([word for word in text.lower().split() if (
-        (word not in stopword) and (not word.isdigit()))])
-
+        (word not in stop) and (not word.isdigit()))])
+        # if word.isalpha()
     puncfree = ''.join(word for word in stopfree if word not in excludePunt)
     lemmy = " ".join(wordlemm.lemmatize(word)
                           for word in puncfree.split())
@@ -51,10 +51,28 @@ def cleantext(text, stop):
     return lemmy
 
 
+def preprocessText(text, stop):
+    # form word tokens
+    text2 = word_tokenize(text)
+    # Retain alphabetic words: alpha_only
+    alpha_only = [t.lower() for t in text2 if t.isalpha()]
+
+    # Remove all stop words: no_stops
+    no_stops = [t for t in alpha_only if t not in stop]
+
+    # Lemmatize all tokens into a new list: lemmatized
+    lemmat = [wordlemm.lemmatize(t) for t in no_stops]
+    
+    return lemmat
+
 text_clean = []
 
+# for text in data['News_content']:
+#     text_clean.append(cleantext(text, stopword).split()) 
+
+
 for text in data['News_content']:
-    text_clean.append(cleantext(text, stopword).split()) 
+    text_clean.append(preprocessText(text, stopword))
 
 data['wordList'] = text_clean
 
@@ -92,7 +110,7 @@ plt.show()
 
 
 data['BagsWords'] = data.News_content.apply(word_tokenize)
-
+datawordbags = word_tokenize(data.News_content[2])
 allword = " "
 
 for idx, val in data['News_content'].iteritems():
@@ -100,8 +118,10 @@ for idx, val in data['News_content'].iteritems():
 
 # Get Counter object
 countdict = Counter(word_tokenize(allword))
-print(countdict.most_common(10))
-
+print(countdict.most_common(10), sep=sp)
+print(Counter(datawordbags).most_common(10))
 sortcount = sorted(countdict,  reverse=True)
 # print(sortcount, countdict, sep=sp)
 print(type(sortcount), type(countdict), sep=sp)
+
+
