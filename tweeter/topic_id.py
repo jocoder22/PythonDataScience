793f9ccbcf1ae0d20e7c68f8b124243fa2365521
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-02-27 21:28:27 -0500

    Update

--- tweeter/topic_id.py
+++ tweeter/topic_id.py
@@ -5,12 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import nltk
 import pickle
+import pyLDAvis.gensim
 
-from text_preprocessing import cleantext as pptext
-import pickle
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.tfidfmodel import TfidfModel
 from gensim.models.ldamodel import LdaModel
+from text_preprocessing import cleantext as pptext
 
 
 sp = '\n\n'
@@ -18,7 +18,7 @@ path = "C:\\Users\\Jose\\Desktop\\PythonDataScience\\tweeter\\"
 # path = 'C:\\Users\\okigboo\\Desktop\\PythonDataScience\\tweeter\\'
 os.chdir(path)
 
-data = pd.read_csv('nyt2.csv')
+data = pd.read_csv('nyt.csv')
 
 text_clean = []
 
@@ -32,11 +32,21 @@ print(text_clean[:3])
 
 dictionary = Dictionary(text_clean)
 corpus = [dictionary.doc2bow(text) for text in text_clean]
-pickle.dump(corpus, open('corpus.pkl', 'wb'))
-dictionary.save('dictionary.gensim')
+pickle.dump(corpus, open('topicModels//corpus2.pkl', 'wb'))
+dictionary.save('topicModels//dictionary2.gensim')
 
-ldamodel = LdaModel(corpus, num_topics = 10, id2word=dictionary, passes=15)
-ldamodel.save('model10.gensim')
+ldamodel = LdaModel(corpus, num_topics = 15, id2word=dictionary, passes=15)
+ldamodel.save('topicModels//model15.gensim')
 topics = ldamodel.print_topics(num_words=4)
 for topic in topics:
     print(topic)
+
+
+# Visaulization works only on Jupyter Notebook
+# type jupyter notebook or jupyter console
+dictionary = Dictionary.load('topicModels//dictionary2.gensim')
+corpus = pickle.load(open('topicModels//corpus2.pkl', 'rb'))
+ldamd = LdaModel.load('topicModels//model15.gensim')
+
+lda_display = pyLDAvis.gensim.prepare(ldamd, corpus, dictionary, sort_topics=False)
+pyLDAvis.display(lda_display)
