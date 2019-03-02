80e0e512ff59431ad2fd111eb08c0a0e9b41dd00
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-02 15:38:46 -0500

    Update

--- scrapy/intro_scapy.py
+++ scrapy/intro_scapy.py
@@ -1,7 +1,12 @@
 import os
 import requests
-from scrapy import selector
+from scrapy import Selector
 
+sp = '\n\n'
 url = 'https://www.cdc.gov/nchs/tutorials/NHANES/index_continuous.htm'
 res = requests.get(url)
 html = res.text 
+
+sel = Selector(text=html)
+sll = sel.xpath('//p')[2]
+print(sll)
