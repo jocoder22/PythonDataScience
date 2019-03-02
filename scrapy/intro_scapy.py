98bda03edf1eb4229de8bbf348eced2c47113eb9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-02 16:39:04 -0500

    Update

--- scrapy/intro_scapy.py
+++ scrapy/intro_scapy.py
@@ -19,10 +19,14 @@ def preview_result(Xpath, inputtext):
 
 sp = '\n\n'
 url = 'https://www.cdc.gov/nchs/tutorials/NHANES/index_continuous.htm'
-res = requests.get(url)
-html = res.text 
+# res = requests.get(url)
+# html = res.text 
+html = requests.get(url).content
+
+
 
 xpath = '//p'
+xpath2 = '//*'
 sel = Selector(text=html)
 sll = sel.xpath('//p')[2].extract()  # extract the 3rd element (here paragrph) of the selectorList
 sll_ = sel.xpath('//p')  # without extract(), the selectorList give a 36 line preview of the paragraph
@@ -33,4 +37,4 @@ sllf = sel.xpath('//p').extract_first()
 
 # print(sll, slla, sllf, sep=sp)
 
-print(number_of_element(xpath, html), preview_result(xpath, html), sep=sp)
+print(number_of_element(xpath, html), number_of_element(xpath2, html),preview_result(xpath, html), sep=sp)
