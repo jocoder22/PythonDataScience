943183ae82c245a2b0fd9797301be7caf589fb6b
Okigbo <okigbookey@gmail.com>
2019-03-05 10:54:03 -0500

    Update

---
+++ sql/building_R_databases.sql
@@ -0,0 +1,11 @@
+-- Query the right table in information_schema
+SELECT table_name 
+FROM information_schema.tables
+-- Specify the correct table_schema value
+WHERE table_schema = 'public';
+
+
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'unilever' AND table_schema = 'public';
