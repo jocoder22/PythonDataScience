a536564d6ea412ab97201f7fa6f9b4e4c180e466
Okigbo <okigbookey@gmail.com>
2019-03-07 13:55:59 -0500

    Update

--- tensorflow/linearModel1.py
+++ tensorflow/linearModel1.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn import datasets
+from datetime import datetime
 
 sp = '\n\n'
 # plt.style.use('ggplot')
@@ -14,24 +15,35 @@ sp = '\n\n'
 # 2: collect necessary data
 # Using stimulated dataset
 
-np.random.seed(299)
+np.random.seed(123)
 xx1 = np.random.normal(120, 10, 4000).astype('int')
-xx2 = np.random.normal(12, 2.5, 4000).astype('int')
-xx3 = np.random.choice(3, 4000)
-xx4 = np.random.normal(1,4000)
+xx2 = np.random.normal(12, 2.5, 4000)
+xx3 = np.random.choice(4, 4000)
+xx4 = np.random.choice(2,4000)
 xx5 = np.random.normal(40, 6, 4000).astype('int')
-DateB = np.datetime64('2018-01-30') - 365 * xx5
+DateB = np.datetime64('2018-01-30') - 365*xx5
 
 intercept_y = 3
 err = np.random.normal(0, 1.1, 4000)
 
-print(data.info(), data.shape, data.describe(), sep=sp)
 
-yy = np.array([0.6*x1 + 2.3*x2 + 0.83*x3 + 2.2*x4 + 
-                1.3*xx5 + intercept_y + err for x1,x2,x3,x4,x5 in
-                zip(xx1,xx2,xx3,xx4,xx5)])
+
+yy = np.array([1.6*x1 + 2.3*x2 + 0.83*x3 + 1.2*x4 + 
+                1.3*x5 + intercept_y + err for x1,x2,x3,x4,x5, err in
+               zip(xx1, xx2, xx3, xx4, xx5, err)])
 
 cols=['IQ', 'YearsExperience', 'levelEducation', 'Gender', 'DateBirth']
 
-data = pd.DataFrame(list(zip(xx1, xx2, xx3, xx4, DateB)))
+data = pd.DataFrame(list(zip(xx1, xx2, xx3, xx4, DateB)), columns=cols)
 data['Income'] = yy
+
+print(data.info(), data.head(), data.shape, data.describe(), sep=sp)
+
+
+# cleaning data
+# drop null values
+data.dropna(axis=0)
+data = data[data.YearsExperience > 0]
+
+print(data.info(), data.head(), data.shape, data.describe(), sep=sp, end=sp)
+print(data.describe(include=['datetime64']))
