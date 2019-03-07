f32e6915a508a14660f33943ea3ce5a6c1b7fb52
Okigbo <okigbookey@gmail.com>
2019-03-07 13:59:32 -0500

    Update

--- tensorflow/linearModel1.py
+++ tensorflow/linearModel1.py
@@ -22,6 +22,7 @@ xx3 = np.random.choice(4, 4000)
 xx4 = np.random.choice(2,4000)
 xx5 = np.random.normal(40, 6, 4000).astype('int')
 DateB = np.datetime64('2018-01-30') - 365*xx5
+race = np.random.choice(['Black', 'White', 'Asian', 'Chinese', 'Latino'], 4000)
 
 intercept_y = 3
 err = np.random.normal(0, 1.1, 4000)
@@ -32,9 +33,9 @@ yy = np.array([1.6*x1 + 2.3*x2 + 0.83*x3 + 1.2*x4 +
                 1.3*x5 + intercept_y + err for x1,x2,x3,x4,x5, err in
                zip(xx1, xx2, xx3, xx4, xx5, err)])
 
-cols=['IQ', 'YearsExperience', 'levelEducation', 'Gender', 'DateBirth']
+cols=['IQ', 'YearsExperience', 'levelEducation', 'Gender', 'DateBirth', 'Race']
 
-data = pd.DataFrame(list(zip(xx1, xx2, xx3, xx4, DateB)), columns=cols)
+data = pd.DataFrame(list(zip(xx1, xx2, xx3, xx4, DateB, race)), columns=cols)
 data['Income'] = yy
 
 print(data.info(), data.head(), data.shape, data.describe(), sep=sp)
