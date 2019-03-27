16cbffe535fbaa0b63b332ba3f89dd3f5f4196b2
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-26 22:19:41 -0400

    Update

--- datavisualization/mplfinance/intro102.py
+++ datavisualization/mplfinance/intro102.py
@@ -19,8 +19,8 @@ df_volume = df['Volume'].resample('10D').sum()
 df_ohlc.reset_index(inplace=True)
 df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
 
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+ax1 = plt.subplot2grid((7,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((7,1), (5,0), rowspan=2, colspan=1)
 # ax1.xaxis_date()
 ax2.xaxis_date()
 ax1.set_xticklabels([])
@@ -29,3 +29,5 @@ candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
 ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 # plt.tight_layout()
 plt.show()
+
+# https://pythonprogramming.net/sp500-company-price-data-python-programming-for-finance/?completed=/sp500-company-list-python-programming-for-finance/
