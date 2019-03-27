298aed6e6d3ab7cec3f15574d8e4e3d4fe5f1906
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-26 21:21:32 -0400

    Update

--- datavisualization/mplfinance/intro101.py
+++ datavisualization/mplfinance/intro101.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from datetime import datetime, date
 from mpl_finance import candlestick2_ohlc
-from matplotlib.dates import MonthLocator, date2num, DateFormatter
-
+from matplotlib.dates import MonthLocator, date2num, DateFormatter,  WeekdayLocator,\
+    DayLocator, MONDAY
+import matplotlib.ticker as mticker
 
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/
 # http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/
@@ -23,28 +24,63 @@ stock = stock['2017':]
 
 print(stock.head())
 
+mondays = WeekdayLocator(MONDAY)        
+alldays = DayLocator()              
+weekFormatter = DateFormatter('%b %d')  
+dayFormatter = DateFormatter('%d') 
+
 fig, ax = plt.subplots()
-fig.subplots_adjust(bottom=0.5)
+fig.subplots_adjust(bottom=0.2)
 
-loc = MonthLocator()
-fmt = DateFormatter('%b')
-
-ax.xaxis.set_major_locator(loc)
-ax.xaxis.set_major_formatter(fmt)
+ax.xaxis.set_major_locator(mondays)
+ax.xaxis.set_minor_locator(alldays)
+ax.xaxis.set_major_formatter(weekFormatter)
 
 candlestick2_ohlc(ax,
                   opens=stock.Open,
                   closes=stock.Close,
                   highs=stock.High,
                   lows=stock.Low,
-                  width=0.3,
+                  width=0.5,
                   colordown='red',
                   colorup='green')
 
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
 plt.tight_layout()
 plt.legend()
 plt.show()
 
 
 stock.Close.plot()
+plt.show()
+
+
+
+mondays = WeekdayLocator(MONDAY)        
+alldays = DayLocator()              
+weekFormatter = DateFormatter('%b %d')  
+dayFormatter = DateFormatter('%d')      
+
+ax.xaxis.set_major_locator(mticker.MaxNLocator(10))
+
+fig, ax = plt.subplots()
+fig.subplots_adjust(bottom=0.2)
+ax.xaxis.set_major_locator(mondays)
+ax.xaxis.set_minor_locator(alldays)
+ax.xaxis.set_major_formatter(weekFormatter)
+ax.xaxis.set_minor_formatter(dayFormatter)
+
+candlestick2_ohlc(ax,
+                  opens=stock.Open,
+                  closes=stock.Close,
+                  highs=stock.High,
+                  lows=stock.Low,
+                  width=0.5,
+                  colordown='red',
+                  colorup='green')
+
+ax.xaxis_date()
+ax.autoscale_view()
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
 plt.show()
