6a823b7758def0406c883993dabdc75b0eaad08c
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-26 20:06:45 -0400

    Update

--- datavisualization/mplfinance/intro101.py
+++ datavisualization/mplfinance/intro101.py
@@ -5,21 +5,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from datetime import datetime, date
-import seaborn as sns
-# Import the statsmodels.api library with the alias sm
-import statsmodels.api as sm
-from statsmodels.tsa.arima_process import ArmaProcess
-from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.arima_model import ARMA
-from statsmodels.graphics.tsaplots import plot_pacf
+from mpl_finance import candlestick2_ohlc
+
 
 # https://www.lfd.uci.edu/~gohlke/pythonlibs/
 # http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/
 # http://codetheory.in/how-to-convert-a-video-with-python-and-ffmpeg/
-import talib
+
 
 stocksname = ['LNG']
 startdate = datetime(2000, 4, 15)
 enddate = date.today()
 
 lng_df = pdr.get_data_yahoo(stocksname, startdate, enddate)
+
+
