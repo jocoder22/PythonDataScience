7dcdab28340cad87100ec02cded9782892edc0f9
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-03-28 01:50:13 -0400

    Update

--- datavisualization/mplfinance/intro102.py
+++ datavisualization/mplfinance/intro102.py
@@ -46,7 +46,9 @@ ax1.set_xticklabels([])
 candlestick_ohlc(ax1, df_ohlc.values, width=4, colorup='g')
 ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 plt.tight_layout()
-plt.show()
+plt.pause(2)
+plt.clf()
+# plt.show()
 
 # https://pythonprogramming.net/sp500-company-price-data-python-programming-for-finance/?completed=/sp500-company-list-python-programming-for-finance/
 
@@ -63,14 +65,21 @@ print(stock.head(), stock.shape, sep=sp)
 
 scaler = MinMaxScaler()
 voll = stock[['Volume']]
+# closeprice = stock[['Close']].values.reshape(-1, 1)
 closeprice = stock[['Close']]
 closeprice = scaler.fit_transform(closeprice)
-voll = scaler.fit_transform(voll)
-print(closeprice, voll, sep=sp)
+
+scaler2 = MinMaxScaler()
+voll = scaler2.fit_transform(voll)
+print(closeprice[:2], voll[:2], sep=sp, end=sp)
+print(closeprice.shape, voll.shape, sep=sp, end=sp)
 
 window = 14
 val = 0.1
-test = 0.1
+test = 0.2
+lrate = 0.001
+epoch = 100
+decay_rate = lrate / epoch
 
 def preprocess(data, data2, wdw):
     feature, target = [], []
@@ -86,8 +95,8 @@ def train_validate_test_split2(datatt, tx, vx, ww):
     test, validate, train = np.split(datatt, [int(tx*len(datatt)), int(vxx*len(datatt))])
     return np.expand_dims(train, axis=-1), np.expand_dims(validate, axis=-1), np.expand_dims(test, axis=-1)
 
-xdata, ydata = preprocess(closeprice, voll, window)
-
+xdata, ydata = preprocess(voll, closeprice, window)
+# xdata, ydata = preprocess(closeprice, closeprice, window)
 xtrain, xval, xtest = train_validate_test_split2(xdata, val, test, window)
 ytrain, yval, ytest = train_validate_test_split2(ydata, val, test, window)
 
@@ -95,12 +104,65 @@ print(xtrain.shape, xval.shape, xtest.shape, sep=sp)
 print(ytrain.shape, yval.shape, ytest.shape, sep=sp)
 
 
+# saving weights
+savedate = datetime.now().strftime("%Y_%m_%d %H_%M_%S")
+savedir = os.path.join(os.getcwd(), 'weights')
+modelname = 'Best_{0}.h5'.format(savedate)
+
+if not os.path.isdir(savedir):
+    os.makedirs(savedir)
+filepath = os.path.join(savedir, modelname)
+
+monitorbest = ModelCheckpoint(filepath=filepath, monitor='val_loss',
+                             verbose=1,
+                             save_best_only=True)
+
+callbacks=[monitorbest]
+
 model = Sequential()
-model.add(LSTM(256, input_shape=(window, 1)))
+model.add(LSTM(256,  return_sequences=True, input_shape=(window, 1)))
+model.add(Dropout(0.3))
+
+model.add(LSTM(190))
+model.add(Dropout(0.3))
 
 model.add(Dense(1))
+
+adam = Adam(lr=lrate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=decay_rate, amsgrad=False)
 model.compile(optimizer='adam', loss='mse')  
 
 
-history = model.fit(xtrain, ytrain, epochs=30, validation_data=(xval, yval), 
+history = model.fit(xtrain, ytrain, epochs=epoch, validation_data=(xval, yval), 
             callbacks=callbacks, shuffle=False)
+
+# to load only the weights you must define the model as above
+# model.load_weights('weights\Best_2019_03_28 00_48_24.h5') for both vol and closing price
+# model.fit with callbacks save only the weights
+
+
+# model = load_model(f"model\model55.h5")
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='Val_loss')
+plt.legend()
+plt.pause(2)
+plt.clf()
+# plt.show()
+
+print(model.summary())
+pred = model.predict(xtest)
+print(type(pred), ytest.shape, sep=sp, end=sp)
+# actual, prediction = [], []
+prediction = scaler.inverse_transform(pred).reshape(-1)
+actual= scaler.inverse_transform(ytest).reshape(-1)
+df = pd.DataFrame({'Actual': actual, 'Predictions': prediction})
+
+print(df.shape, df.head(), df.tail(), sep=sp, end=sp)
+plt.plot(df.Actual.values, label='Actual', color='red')
+plt.plot(df.Predictions.values, label='Prediction', color='yellow')
+plt.legend()
+plt.show()
+
+# now = datetime.now().strftime("%Y_%m_%d %H_%M_%S")
+# save model
+# model.save_weights('model_lstm.h5')
+# model.save(f"model\model55.h5")
