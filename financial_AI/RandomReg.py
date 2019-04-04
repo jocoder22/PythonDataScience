d184fd5cc8a568339c95d482fc319e7a836539f1
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-03 20:42:08 -0400

    Update

---
+++ financial_AI/RandomReg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from datetime import datetime, date
+
+from sklearn.ensemble import RandomForestRegressor as regg
+from sklearn.model_selection import train_test_split
+
+plt.style.use('ggplot')
