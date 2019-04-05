5091bfe474714cb7a594e0bba567d81203f24424
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-04 22:22:25 -0400

    Update

--- financial_AI/RandomReg.py
+++ financial_AI/RandomReg.py
@@ -24,8 +24,8 @@ datenow = dat1.strftime('%b_%d_%y')
 start = datetime(2000, 6, 29)
 # end = datetime(2018, 3, 27)
 end = date.today()
-symbol = 'AAPL'
-# symbol = 'TSLA'
+# symbol = 'AAPL'
+symbol = 'TSLA'
 
 stock = pdr.get_data_yahoo(symbol, start, end)[['Close']]
 
@@ -44,14 +44,21 @@ print(X.shape, y.shape, sep=sp, end=sp)
 
 X_train, X_test, y_train, y_test = tts(X, y, test_size=0.3)
 
-model = regg(bootstrap=True, criterion='mse', max_depth=None,
-           max_features='auto', max_leaf_nodes=None,
-           min_impurity_decrease=0.0, min_impurity_split=None,
-           min_samples_leaf=1, min_samples_split=2,
-           min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=16,
-           oob_score=True, random_state=None, verbose=1, warm_start=False)
+# loading the model
+symbol = 'AAPL'
+filename = f'model_{symbol}_{datenow}.sav'
+model = pkl.load(open(filename, 'rb'))
+result = model.score(X_test, y_test)
+print(result)
 
-model.fit(X_train, y_train)
+# model = regg(bootstrap=True, criterion='mse', max_depth=None,
+#            max_features='auto', max_leaf_nodes=None,
+#            min_impurity_decrease=0.0, min_impurity_split=None,
+#            min_samples_leaf=1, min_samples_split=2,
+#            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=16,
+#            oob_score=True, random_state=None, verbose=1, warm_start=False)
+
+# model.fit(X_train, y_train)
 score = model.score(X_test, y_test)
 print(score, sep=sp, end=sp)
 
@@ -83,14 +90,14 @@ plt.legend()
 plt.show()
 
 
-# Save the model
-filename = f'model_{symbol}_{datenow}.sav'
-pkl.dump(model, open(filename, 'wb'))
-dump(model, f'model2_{symbol}_{datenow}.joblib')
+# # Save the model
+# filename = f'model_{symbol}_{datenow}.sav'
+# pkl.dump(model, open(filename, 'wb'))
+# dump(model, f'model2_{symbol}_{datenow}.joblib')
 
 
-# loading the model
-model2 = pkl.load(open(filename, 'rb'))
-result = model2.score(X_test, y_test)
-print(result)
+# # loading the model
+# model2 = pkl.load(open(filename, 'rb'))
+# result = model2.score(X_test, y_test)
+# print(result)
 
