ed981e28cc843ef129441508bd3c65a190138abb
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-05 21:17:53 -0400

    Update

--- financial_AI/RandomReg.py
+++ financial_AI/RandomReg.py
@@ -56,7 +56,7 @@ model = regg(bootstrap=True, criterion='mse', max_depth=None,
            max_features='auto', max_leaf_nodes=None,
            min_impurity_decrease=0.0, min_impurity_split=None,
            min_samples_leaf=1, min_samples_split=2,
-           min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=16,
+           min_weight_fraction_leaf=0.0, n_estimators=130, n_jobs=16,
            oob_score=True, random_state=None, verbose=1, warm_start=False)
 
 model.fit(X_train, y_train)
