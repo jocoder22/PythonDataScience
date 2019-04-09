cd1a2445e0979ddcd45e661ca2d71a30366c1fee
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-08 22:43:06 -0400

    Update

--- financial_AI/RandomReg.py
+++ financial_AI/RandomReg.py
@@ -25,9 +25,9 @@ start = datetime(2000, 6, 29)
 # end = datetime(2018, 3, 27)
 end = date.today()
 # symbol = 'AAPL'
-symbol = 'AMZN'
+# symbol = 'AMZN'
 # symbol = 'NYT'
-# symbol = 'GOOG'
+symbol = 'GOOG'
 
 stock = pdr.get_data_yahoo(symbol, start, end)[['Close']]
 
@@ -44,7 +44,7 @@ y = stock.iloc[:, 33:]
 
 print(X.shape, y.shape, sep=sp, end=sp)
 
-X_train, X_test, y_train, y_test = tts(X, y, test_size=0.3, shuffle=False)
+X_train, X_test, y_train, y_test = tts(X, y, test_size=0.3)
 
 # loading the model
 # symbol = 'AAPL'
@@ -53,21 +53,21 @@ X_train, X_test, y_train, y_test = tts(X, y, test_size=0.3, shuffle=False)
 # result = model.score(X_test, y_test)
 # print(result)
 
-model = regg(bootstrap=True, criterion='mse', max_depth=None,
+model2 = regg(bootstrap=True, criterion='mse', max_depth=None,
            max_features='auto', max_leaf_nodes=None,
            min_impurity_decrease=0.0, min_impurity_split=None,
            min_samples_leaf=1, min_samples_split=2,
            min_weight_fraction_leaf=0.0, n_estimators=130, n_jobs=16,
            oob_score=True, random_state=None, verbose=1, warm_start=False)
 
-model.fit(X_train, y_train)
-score = model.score(X_test, y_test)
+model2.fit(X_train, y_train)
+score = model2.score(X_test, y_test)
 print(score, sep=sp, end=sp)
 
-impt_feature = model.feature_importances_
+impt_feature = model2.feature_importances_
 print(impt_feature)
 
-ypred = model.predict(X_test)
+ypred = model2.predict(X_test)
 
 fig, ax = plt.subplots()
 
@@ -96,8 +96,8 @@ print(y_test2.tail(10))
 
 # # Save the model
 filename = f'model_{symbol}_{datenow}.sav'
-pkl.dump(model, open(filename, 'wb'))
-dump(model, f'model2_{symbol}_{datenow}.joblib')
+pkl.dump(model2, open(filename, 'wb'))
+dump(model2, f'model2_{symbol}_{datenow}.joblib')
 
 
 # # loading the model
