8ebe429a1da7e3c53b3e4c398e41032bb87d9584
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-09 21:30:42 -0400

    Update

--- financial_AI/RandomReg.py
+++ financial_AI/RandomReg.py
@@ -25,8 +25,8 @@ start = datetime(2010, 6, 29)
 # end = datetime(2018, 3, 27)
 end = date.today()
 # symbol = 'AAPL'
-symbol = 'AMZN'
-# symbol = 'NYT'
+# symbol = 'AMZN'
+symbol = 'NYT'
 # symbol = 'GOOG'
 
 stock = pdr.get_data_yahoo(symbol, start, end)[['Close']]
