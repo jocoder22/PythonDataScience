1d4493710835fd6a7981feaa02a154da55f3080f
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-04-26 22:21:05 -0400

    Update

---
+++ datavisualization/subplots/subplots2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+
+y = np.arange(10)
+z = np.random.rand(len(y), 6)*1000
+
+fig, axes = plt.subplots(2,3, sharex=True, sharey=True)
+axesr = axes.ravel()
+print(axesr)
+
+for idx in range(len(axesr)):
+    axesr[idx].bar(y, z[:,idx], color=plt.cm.Paired(idx/6.))
+
+plt.show()
