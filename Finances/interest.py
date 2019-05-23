dc0eb0a1bca7df38cb7333b09f78d01fa19a5e2b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-22 20:26:36 -0400

    Update

--- Finances/interest.py
+++ Finances/interest.py
@@ -2,19 +2,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.pipeline import FeatureUnion
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.preprocessing import Imputer
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_curve
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 plt.style.use('ggplot')
 
 
@@ -22,4 +9,6 @@ def interest(amt, rate, timet):
     int = (1 + (rate/12)) ** timet
     total = amt * int 
     return total-amt, total 
-    
+
+a, b = interest(46000, 0.03, 48)
+print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
