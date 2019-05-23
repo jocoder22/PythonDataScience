6af7ad6e9d52a00996c2af58f8ded5498123bbfb
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-22 21:17:40 -0400

    Update

--- Finances/interest.py
+++ Finances/interest.py
@@ -5,20 +5,27 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 
-def interest(amt, rate, timet):
+def interest(amt, rate, timet, down=None):
     int = (1 + (rate/12)) ** timet
     total = amt * int 
     return total-amt, total 
 
-a, b = interest(46000, 0.03, 48)
-print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
 
+a, b = interest(320000, 0.04257, 360)
+print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
+print(f'Monthly payment is {b/360:.2f}')
 
 a, b = interest(320000, 0.04, 360)
 print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
 print(f'Monthly payment is {b/360:.2f}')
 
-
+mm = 360
 a, b = interest(320000, 0.03, 360)
 print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
 print(f'Monthly payment is {b/360:.2f}')
+print(3141 * mm)
+
+a, b = interest(550000, 0.0425, mm)
+print(f'Interest paid is ${a:.2f}, and total amount is ${b:.2f}')
+print(f'Monthly payment is {b/360:.2f}')
+print(3141 * mm)
