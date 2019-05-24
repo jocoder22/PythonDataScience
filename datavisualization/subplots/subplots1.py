2ddfc3d288ea1cfa5f1029455c3acf13ceb493dc
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-23 22:39:48 -0400

    Update

--- datavisualization/subplots/subplots1.py
+++ datavisualization/subplots/subplots1.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 
-y = np.arange(10)
-z = np.random.rand(len(y), 6)*1000
+x = np.arange(10)
+y = np.random.rand(len(x), 6)*400
 
 fig, axes = plt.subplots(2,3, sharex=True, sharey=True)
 
 for idx, ax in enumerate(axes.flatten()):
-    ax.bar(y, z[:,idx], color=plt.cm.Paired(idx/6.))
+    ax.bar(x, y[:,idx], color=plt.cm.Paired(idx/6.))
 
 plt.show()
