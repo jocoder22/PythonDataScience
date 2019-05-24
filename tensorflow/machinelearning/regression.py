191936abfe0d97c6c365b750aeeb52971560785a
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-23 23:35:31 -0400

    Update

---
+++ tensorflow/machinelearning/regression.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import Imputer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+plt.style.use('ggplot')
+
+
