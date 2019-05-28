f44b06bd495ff390449d2290a32b040fe33817d2
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-27 21:58:14 -0400

    Update

---
+++ PCA/pca101.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+
+
+sp = '\n\n'
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
