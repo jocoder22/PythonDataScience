e6241ad3ec7e53403443697de5144a15e93fa35a
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-05-30 20:39:29 -0400

    Update

--- MachineLearning/FeatureEngineering/feature102.py
+++ MachineLearning/FeatureEngineering/feature102.py
@@ -10,6 +10,7 @@ colname = 'Age Sex CP Trestbps Chol Fbs Restecg Thalach Exang Oldpeak Slope CA T
 df = pd.read_csv(url, names=colname, na_values=['na','Na', '?'])
 print(df.shape, df.info(), df.head(), sep=sp, end=sp)
 
+df2 = df.copy()
 
 # Drop based on named column, drop rows
 df.dropna(subset=['Chol'], inplace=True)
@@ -19,8 +20,13 @@ print([name for name in colname if name not in df.columns])
 
 # Drop all column with NA
 df.dropna(how='any', axis=1, inplace=True)
-print(df.shape, sep=sp)
+print(df.head(), df.shape, sep=sp, end=sp)
 print([name for name in colname if name not in df.columns])
+
+
+# fill in values
+df2['Chol'] = df2['Chol'].fillna(round(df2['Chol'].mean()))
+print(df2.info(), sep=sp, end=sp)
 
 
 
