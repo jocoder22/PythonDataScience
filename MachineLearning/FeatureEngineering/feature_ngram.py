83986f2841dd310a2e90ff02792019d174edb23e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-02 03:21:54 -0400

    Update

---
+++ MachineLearning/FeatureEngineering/feature_ngram.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from collections import defaultdict
+
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer, PorterStemmer
+import string 
+
+
+def xprint(d):
+    for arg in (d.head(), d.info(), d.shape, d. columns):
+        print(arg, end='\n\n')
+
+
+sp = '\n\n'
+path = r'C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\FeatureEngineering'
+os.chdir(path)
+data = pd.read_csv('textdata.csv', compression='gzip')
+
+
+# Instantiate the sklearn countvectorizer
+# and limit the number of features generated
+cvect = CountVectorizer(max_features=10, ngram_range=(2,2), stop_words='english')
+
+# fit the sklearn countvectorizer
+cvect.fit(data['cleanText'])
+
+# print feature names
+print(len(cvect.get_feature_names()), cvect.get_feature_names(), sep=sp, end=sp)
+
+# Transform the text content
+News_content_vectorized = cvect.transform(data['cleanText'])
+
+# convert to array
+News_content_Varray = News_content_vectorized.toarray()
+print(News_content_Varray)
