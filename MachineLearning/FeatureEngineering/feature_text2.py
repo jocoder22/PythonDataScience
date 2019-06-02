995b0b7bd3fd4ad424d62b68e7867ecb1eb2b9fb
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-02 00:56:59 -0400

    Update

--- MachineLearning/FeatureEngineering/feature_text2.py
+++ MachineLearning/FeatureEngineering/feature_text2.py
@@ -16,10 +16,37 @@ def xprint(d):
         print(arg, end='\n\n')
 
 
+def preprocessText(text):
+
+    excludePunt = set(string.punctuation)
+    excludePunt.update(('"', "'"))
+    stopword = set(stopwords.words("english"))
+    stopword.update(("said", "to", "th", "e", "cc", "subject", "http", "from", "new", "time", "times", "york",
+                    "sent", "ect", "u", "fwd", "w", "n", "s", "www", "com", "de", "one", "may", "home", "u", "la",
+                    "advertisement", "information", "service", "—", "year", "would"))
+    wordlemm = WordNetLemmatizer()
+    wordporter = SnowballStemmer("english")
+    # form word tokens
+    text2 = word_tokenize(text)
+    # Retain alphabetic words: alpha_only
+    alpha_only = [t.lower() for t in text2 if t.isalpha()]
+
+    # Remove all stop words: no_stops
+    no_stops = [t for t in alpha_only if t not in stopword]
+
+    # Lemmatize all tokens into a new list: lemmatized
+    lemmat = [wordlemm.lemmatize(t) for t in no_stops]
+    
+    return lemmat
+
+text_clean = []
+
+for text in data['News_content']:
+    text_clean.append(cleantext(text, stopword).split()) 
 
 # Instantiate the sklearn countvectorizer
 # and limit the number of features generated
-cvect = CountVectorizer()
+cvect = CountVectorizer(min_df=0.2 ,max_df=0.7)
 
 # fit the sklearn countvectorizer
 cvect.fit(data['News_content'])
