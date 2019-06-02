519259377b11fbd520f359c40207959263a15f2d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-02 02:13:49 -0400

    Update

---
+++ MachineLearning/FeatureEngineering/feature_tfidf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def xprint(d):
+    for arg in (d.head(), d.info(), d.shape, d. columns):
+        print(arg, end='\n\n')
+
+
+sp = '\n\n'
+path = r'C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\FeatureEngineering'
+os.chdir(path)
+data = pd.read_csv('textdata.csv', compression='gzip')
