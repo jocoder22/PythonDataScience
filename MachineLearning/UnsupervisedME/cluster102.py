b3b954cf32751c6462b6f6025ccefff9d4400386
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-05 20:03:56 -0400

    Update

--- MachineLearning/UnsupervisedME/cluster102.py
+++ MachineLearning/UnsupervisedME/cluster102.py
@@ -15,5 +15,21 @@ from scipy.cluster.vq import vq, kmeans, whiten
 
 path = r"C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\UnsupervisedME"
 os.chdir(path)
-
 sp = '\n\n'
+
+# Read in the dataset, 
+data = pd.read_csv('car.csv', compression='gzip')
+
+origin = data.pop('Origin')
+modelyear = data.pop('Model_year')
+
+# Normalize the dataset
+data2 = whiten(data)
+dataset2 = pd.DataFrame(data2, columns=data.columns)
+
+# Cluster using kmeans
+k_cluster, _ = kmeans(dataset2, 3)
+dataset2['k_labels'], _ = vq(dataset2, k_cluster)
+
+
+print(dataset2.head())
