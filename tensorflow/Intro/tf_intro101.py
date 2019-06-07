ad1c18fedda49e0acff13985595b618c302da051
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-06 20:14:09 -0400

    Update

---
+++ tensorflow/Intro/tf_intro101.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pandas as pd
+from tensorflow import Variable, float32
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+# plt.style.use('ggplot')
+
+
+path = r"C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\UnsupervisedME"
+os.chdir(path)
+sp = '\n\n'
+
+# Read in the dataset, 
+data = pd.read_csv('car.csv', compression='gzip')
+data = data.drop(columns=['Origin','Model_year'])
+
+# define the variables
+intercept = Variable(0.1, float32)
+slope = Variable(1.0, float32)
+
+# define loss function
+def lossfunc(intercept, slope, feature, target):
+    predictions = intercept + slope * feature
+    return tf.keras.losses.mse(target, predictions)
+
+
