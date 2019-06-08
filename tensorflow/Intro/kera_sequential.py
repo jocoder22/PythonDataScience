3857e52b1dde8399357a242f1dd75978bfa72ac5
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-08 06:12:23 -0400

    Add Sequential model

--- tensorflow/Intro/kera_sequential.py
+++ tensorflow/Intro/kera_sequential.py
@@ -18,3 +18,22 @@ data_reg = data.drop(columns=['Origin','Model_year'])
 data_class = data.drop(columns=['Model_year'])
 
 print(data_class.groupby('Origin')['MPG'].mean())
+
+# Define the sequential model
+model = keras.Sequential()
+
+# Define the dimensions of the data
+# Use the flatten() on the ndarray
+_nshape = data_reg.values.flatten().shape
+
+# Define the first dense hidden layer
+model.add(keras.layers.Dense(200, activation='relu', input_shape = _nshape))
+
+# Define the second dense hidden layer
+model.add(keras.layers.Dense(25, activation='relu'))
+
+# Define the output layer
+model.add(keras.layers.Dense(1))
+
+
+
