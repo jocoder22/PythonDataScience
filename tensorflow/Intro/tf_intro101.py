ea3c6cd67eea34c5eeee6a5d59e4f56c44ddfb31
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-08 03:58:02 -0400

    Upgrade numpy and Tensorflow

--- tensorflow/Intro/tf_intro101.py
+++ tensorflow/Intro/tf_intro101.py
@@ -4,11 +4,8 @@ import numpy as np
 import pandas as pd
 from tensorflow import Variable, float32, keras
 import tensorflow as tf
-# from tensorflow.keras.optimizers import Adam
-import keras
-from tensorflow.python.keras.optimizers import Adam
 import matplotlib.pyplot as plt
-from tensorflow import keras
+
 
 
 # plt.style.use('ggplot')
@@ -22,7 +19,7 @@ sp = '\n\n'
 data = pd.read_csv('car.csv', compression='gzip')
 data = data.drop(columns=['Origin','Model_year'])
 
-# define the trained parameters
+# define the trainable variables
 intercept = Variable(0.1, float32)
 slope = Variable(0.1, float32)
 
@@ -33,18 +30,21 @@ def lossfunc(intercept, slope, feature, target):
 
 
 # Initialize the Adam optimizer
-optim = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.84)
+optim = keras.optimizers.Adam()
 
 
 # Run the linear model
 for batch in pd.read_csv('car.csv', compression='gzip', chunksize=150):
     feature_batch = tf.cast(batch['Horsepower'], float32)
     target_batch = tf.cast(batch['MPG'], float32)
-    optim.minimize(lambda: lossfunc(intercept, slope, feature_batch, target_batch), 
-            var_list=[intercept, slope])
+    optim.minimize(lambda: lossfunc(intercept, slope, feature_batch, target_batch), var_list=[intercept, slope])
     print(intercept.numpy(), slope.numpy())
 
 
-# Print trained parameters
+
+# Print trainable variables
 print(intercept.numpy(), slope.numpy())
 print(data.head())
+
+
+
