e355a744df6e0afcd288d04e2c52b6e3b4065f42
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-13 01:23:26 -0400

    Add new style

--- datavisualization/seaborn/seaborn101.py
+++ datavisualization/seaborn/seaborn101.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
+plt.style.use('seaborn-whitegrid')
 
 
 path = r"C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\UnsupervisedME"
