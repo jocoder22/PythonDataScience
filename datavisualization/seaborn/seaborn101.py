e854729ac75f5e93b7e10a3e10d69b017ba53269
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-13 01:20:53 -0400

    Update

--- datavisualization/seaborn/seaborn101.py
+++ datavisualization/seaborn/seaborn101.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# plt.style.use('ggplot')
+plt.style.use('ggplot')
 
 
 path = r"C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\UnsupervisedME"
@@ -19,4 +19,9 @@ print(data.head())
 
 # Plot scatter plot
 sns.scatterplot(x=data['MPG'], y=data['Horsepower'])
+plt.show()
+
+# count plot
+sns.countplot(x=data['Origin'])
+plt.xticks(np.arange(3), 'USA Europe Asia'.split())
 plt.show()
