44c8a7b31843b16e7aa659f2e7357c026e6cde98
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-13 19:04:27 -0400

    Update

--- datavisualization/seaborn/seaborn101.py
+++ datavisualization/seaborn/seaborn101.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 # plt.style.use('ggplot')
-plt.style.use('seaborn-whitegrid')
+# plt.style.use('seaborn-whitegrid')
 
 
 path = r"C:\Users\Jose\Desktop\PythonDataScience\MachineLearning\UnsupervisedME"
@@ -26,4 +26,14 @@ plt.show()
 # count plot
 sns.countplot(x='Origin', data=cardata)
 plt.xticks(np.arange(3), 'USA Europe Japan'.split())
+plt.show()
+
+print(np.unique(cardata['Model_year']))
+
+# Relationship plot: continous features
+color_palette = 'Blue Green Red Yellow White'.split()
+size_l = [70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82]
+with plt.style.context(('dark_background')):
+    sns.relplot(x='MPG', y='Horsepower', data=cardata, kind='scatter', 
+            hue='Cylinders', col='Origin', palette=color_palette)
 plt.show()
