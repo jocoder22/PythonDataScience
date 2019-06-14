27aab3b6fdea2adccb7151477b9f678888ad5388
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-13 20:31:22 -0400

    Update

--- datavisualization/seaborn/seaborn101.py
+++ datavisualization/seaborn/seaborn101.py
@@ -28,27 +28,29 @@ sns.countplot(x='Origin', data=cardata)
 plt.xticks(np.arange(3), 'USA Europe Japan'.split())
 plt.show()
 
-print(np.unique(cardata['Model_year']))
+print(np.unique(cardata['Model_year']).sum)
 
 # Relationship plot: continous features
 color_palette = 'Blue Green Red Yellow White'.split()
+cardata['Origin2'] = cardata['Origin'].map({1: 'USA', 2: 'Europe', 3:'Japan'})
 with plt.style.context(('dark_background')):
     g = sns.relplot(x='MPG', y='Horsepower', data=cardata, kind='scatter', 
-            hue='Cylinders', col='Origin', palette=color_palette)
+            hue='Cylinders', col='Origin2', palette=color_palette)
     g.fig.suptitle('')
+    g.set_titles('{col_name} Origin')
 plt.show()
+
 
 
 # g.set_title() => for titles for AxesSubplot
 
 # category chat
-cardata['Origin2'] = cardata['Origin'].map({1: 'USA', 2: 'Europe', 3:'Japan'})
 with plt.style.context(('dark_background')):
     g = sns.catplot(x='Cylinders', y='Horsepower', data=cardata, kind='box', 
               palette=color_palette, col='Origin2')
     g.fig.suptitle('Cylinders vs. Horsepower', y=1.03)
     g.set_titles('This is {col_name}')
     g.set(xlabel='Number of Cyclinders')
-   
+plt.xticks(rotation=30)
 plt.show()
  
