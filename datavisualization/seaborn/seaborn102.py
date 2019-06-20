074d0d19c0cfabfc6a56c233d8f7a8bdb8770dca
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-19 22:00:47 -0400

    Update

--- datavisualization/seaborn/seaborn102.py
+++ datavisualization/seaborn/seaborn102.py
@@ -52,31 +52,30 @@ sns.jointplot(x='Displacement', y='MPG', data=car)
 plt.show()
 
 
-
 car.loc[car['Model_year'] >= 70, 'Year'] = 'Early 70s'
 car.loc[car['Model_year'] >= 75, 'Year'] = 'Late 70s'
 car.loc[car['Model_year'] >= 80, 'Year'] = 'Early 80s'
 
-# # boxplot
-# gg = sns.boxplot(x='Year', y='MPG', data=car)
-# plt.show()
+# boxplot
+gg = sns.boxplot(x='Year', y='MPG', data=car)
+plt.show()
 
 
-# # swarmplot
-# gs = sns.swarmplot(x='Year', y='MPG', data=car)
-# gs.set_xticklabels(gs.get_xticklabels(), rotation=45)
-# plt.show()
+# swarmplot
+gs = sns.swarmplot(x='Year', y='MPG', data=car)
+gs.set_xticklabels(gs.get_xticklabels(), rotation=45)
+plt.show()
 
-# # heatmap
-# df = car.pivot_table(index='Cylinders', columns='Displacement', values='MPG', aggfunc='mean')
-# print(df)
-# sns.heatmap(df)
-# plt.show()
+
+# heatmap
+df = car.pivot_table(index='Cylinders', columns='Displacement', values='MPG', aggfunc='mean')
+print(df)
+sns.heatmap(df)
+plt.show()
 
 
 # heatmap 2
 df2 = car.pivot_table('Cylinders', 'Year', 'Origin', 'median')
 sns.heatmap(df2, annot=True,  linewidths=.5, 
         cmap="YlGnBu", xticklabels='USA Europe Japan'.split())
-# plt.xticks(np.arange(3), 'USA Europe Japan'.split())
 plt.show()
