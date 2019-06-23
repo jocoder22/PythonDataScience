80aca01c7281e9c2caf30f22714322fbf4e0f8e5
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-23 18:38:26 -0400

    Update

--- images/edge_detection.py
+++ images/edge_detection.py
@@ -10,11 +10,40 @@ from skimage import io
 path = r'C:\Users\Jose\Desktop\PythonDataScience\images'
 os.chdir(path)
 
-# load image
-img2 = cv2.imread('car22.jpg', 0)  # load as gray directly
 
-# Sobel detection
-x_sobel = cv2.Sobel(img2, cv2.CV_64F, 0, 1, ksize=7)
-cv2.imshow('x_sobel', x_sobel)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# load image
+img = cv2.imread('color22.jpg')  
+
+# converting to gray scale
+Img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# remove noise
+img = cv2.GaussianBlur(Img_gray,(3,3),0)
+
+# convolute with proper kernels
+Laplacian_image = cv2.Laplacian(img,cv2.CV_64F)
+Sobel_X_image = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=9) 
+Sobel_Y_image = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=9)  
+
+
+# Plot the images
+plt.subplot(221)
+plt.imshow(img, cmap = 'gray')
+plt.title('Original_image')
+plt.axis('off')
+
+plt.subplot(222)
+plt.imshow(Laplacian_image, cmap = 'gray')
+plt.title('Laplacian_image')
+plt.axis('off')
+
+plt.subplot(223)
+plt.imshow(Sobel_X_image, cmap = 'gray')
+plt.title('Sobel_X_image')
+plt.axis('off')
+
+plt.subplot(224),
+plt.imshow(Sobel_Y_image, cmap = 'gray')
+plt.title('Sobel_Y_image')
+plt.axis('off')
+plt.show()
