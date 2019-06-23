7d43429ff95f6b94f775a450785a883bd3230f51
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-23 14:45:03 -0400

    Add erosion 1

--- images/image_transformation.py
+++ images/image_transformation.py
@@ -10,21 +10,13 @@ from skimage import io
 path = r'C:\Users\Jose\Desktop\PythonDataScience\images'
 os.chdir(path)
 
-url = 'https://timedotcom.files.wordpress.com/2015/01/the-mercedes-benz-f-015.jpg'
-
-# # read the image
-# mmm = io.imread(url)
-# plt.imshow(mmm)
-# # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-# plt.show()
-
+# load image
 meds = cv2.imread('car22.jpg')
 img = cv2.resize(meds,(1250,650))
 height, width = img.shape[:2]
 cv2.imshow('lamborghini', img)
 cv2.waitKey()
 cv2.destroyAllWindows()
-
 
 
 # perform rotation
@@ -83,56 +75,9 @@ cv2.waitKey()
 cv2.destroyAllWindows()
 
 
-# plt.imread(img)
-plt.imshow(img)
-plt.show()
-
-# x0, x1, x2, x3 = 100, 100, 450, 450
-# y0, y1, y2, y3 = 0, 400, 400, 0
-
-x0, x1, x2, x3 =  124, 80, 205, 230
-y0, y1, y2, y3 = 228, 306, 319, 246
-
-x0, x1, x2, x3 = 230, 308, 326, 265
-y0, y1, y2, y3 = 112, 59, 193, 239
-
-xx = 400
-yy = 600
-
-origin_matrix = np.float32([[x0, y0],[x1 ,y1],
-                          [x2, y2], [x3, y3]])
-
-dest_matrix = np.float32([[0, 0], [xx, 0],
-                          [xx, yy], [0, yy]])
-
-pers = cv2.getPerspectiveTransform(origin_matrix, dest_matrix)
-
-tires = cv2.warpPerspective(img, pers, (xx, yy))
-cv2.imshow('lamborghini_tire', tires)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-
-x0, x1, x2, x3 = 200, 450, 520, 170
-y0, y1, y2, y3 = 60, 150, 500, 470
-xx, yy = 800, 600
-
-origin_matrix = np.float32([[x0, y0],[x1 ,y1],
-                          [x2, y2], [x3, y3]])
-
-dest_matrix = np.float32([[0, 0], [xx, 0],
-                          [xx, yy], [0, yy]])
-
-pers = cv2.getPerspectiveTransform(origin_matrix, dest_matrix)
-
-tires = cv2.warpPerspective(img, pers, (xx, yy))
-cv2.imshow('lamborghini_tire', tires)
-cv2.waitKey()
-cv2.destroyAllWindows()
-
-
-url2 = 'https://www.here.com/sites/g/files/odxslz166/files/styles/hero_banner_secondary_xs/public/2019-03/HERE_Road_Signs-product_detail-hero_2880x1440.jpg?itok=oDgCaLLg'
-mmm = io.imread(url2)
+# Download image
+url = 'https://www.here.com/sites/g/files/odxslz166/files/styles/hero_banner_secondary_xs/public/2019-03/HERE_Road_Signs-product_detail-hero_2880x1440.jpg?itok=oDgCaLLg'
+mmm = io.imread(url)
 plt.imshow(mmm)
 # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
@@ -158,3 +103,25 @@ road_sign = cv2.warpPerspective(mmm, pers, (xx, yy))
 cv2.imshow('Speed Limit', road_sign)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+
+# Cropping
+height, width = mmm.shape[:2]
+w0, w1 = int(width * 0.5), int(width * 1.0)
+h0, h1 = int(height * 0.5), int(height * 1.0)
+crop_img = mmm[0: h0, w0:w1]
+cv2.imshow('crop_image', crop_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+# Erosion and dilatation
+kernel = np.ones((3, 3), np.uint8)
+erosion_img = cv2.erode(mmm, kernel, iterations=2)
+dilate_img = cv2.dilate(mmm, kernel, iterations=2)
+cv2.imshow('erosion_img', erosion_img)
+cv2.imshow('dilate_img', dilate_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
