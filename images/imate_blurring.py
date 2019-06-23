4051849f41efb4d524829890ab092b834f80d1ba
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-23 15:05:36 -0400

    Update

--- images/imate_blurring.py
+++ images/imate_blurring.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import matplotlib.image as mpimg
 import cv2
 from skimage import io
 
@@ -11,8 +11,18 @@ path = r'C:\Users\Jose\Desktop\PythonDataScience\images'
 os.chdir(path)
 
 # load image
+img4 = plt.imread('car22.jpg')
+plt.imshow(img4)
+plt.show()
+
+# load image
 img = cv2.imread('car22.jpg')
+img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img3 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 height, width = img.shape[:2]
 cv2.imshow('lamborghini', img)
+cv2.imshow('lamborghini_GRAY', img2)
+cv2.imshow('lamborghini_RGB', img3)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
