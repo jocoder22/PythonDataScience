8baddfeef7b353f31d6ba80c5b277b42fdf82648
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-23 14:50:45 -0400

    Update

--- images/imate_blurring.py
+++ images/imate_blurring.py
@@ -11,8 +11,7 @@ path = r'C:\Users\Jose\Desktop\PythonDataScience\images'
 os.chdir(path)
 
 # load image
-meds = cv2.imread('car22.jpg')
-img = cv2.resize(meds,(1250,650))
+img = cv2.imread('car22.jpg')
 height, width = img.shape[:2]
 cv2.imshow('lamborghini', img)
 cv2.waitKey()
