23691b09efc810f63b6928e34b1a4d0d79b83268
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-29 15:33:23 -0400

    Update

--- images/fast_orb_features.py
+++ images/fast_orb_features.py
@@ -18,25 +18,39 @@ imgg = io.imread(url)
 # converting to gray scale
 Img_gray = cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY)
 
-# remove noise
-img = cv2.GaussianBlur(Img_gray,(7,7),0)
 
 # cv2.imshow('self_drawing_car', img)
-cv2.imshow('Walgreens Truck', img)
+cv2.imshow('Walgreens Truck', Img_gray)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 
+# remove noise
+img = cv2.GaussianBlur(Img_gray,(7,7),0)
+
 # blur image
 # Blurring images with more powerful normalization
-blur_kernel = np.ones((4, 4)) * 1/16
+blur_kernel = np.ones((5, 5)) * 1/25
 blur_image = cv2.filter2D(img, -1, blur_kernel)
 
 
 # Perform canny's transformation
-lower_threshold, upper_threshold = (40, 200)
+lower_threshold, upper_threshold = (20, 250)
 Canny_image = cv2.Canny(blur_image, lower_threshold, upper_threshold)
 
 cv2.imshow('Walgreens Truck', Canny_image)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+# fast feature detection
+# create fast object
+_fast = cv2.FastFeatureDetector_create()
+keypoints = _fast.detect(Img_gray, None)
+print(len(keypoints))
+
+
+fast_image = cv2.drawKeypoints(imgg, keypoints, None, color=(255, 0, 0))
+cv2.imshow('Walgreens Truck_fast', fast_image)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
