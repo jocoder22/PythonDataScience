b2f4217fbf5c509603d5cd93ca64deeb3f47efbf
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-29 13:50:40 -0400

    Add new file fast_orb_features.py

---
+++ images/fast_orb_features.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+import cv2
+from skimage import io, exposure
+from skimage.feature import hog
+
+sp = '\n\n'
