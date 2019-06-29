4f4503c4d7c3642683d55b2c2343df225317539b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-06-29 14:36:33 -0400

    Download image

--- images/fast_orb_features.py
+++ images/fast_orb_features.py
@@ -9,3 +9,19 @@ from skimage import io, exposure
 from skimage.feature import hog
 
 sp = '\n\n'
+# url = 'https://live.staticflickr.com/7856/47133183871_52540009ac_b.jpg'
+url = 'https://live.staticflickr.com/3900/14288309597_d7bfd2bab6_b.jpg'
+
+# imgg = cv2.imread('car22.jpg') 
+imgg = io.imread(url)
+
+# converting to gray scale
+Img_gray = cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY)
+
+# remove noise
+img = cv2.GaussianBlur(Img_gray,(7,7),0)
+
+# cv2.imshow('self_drawing_car', img)
+cv2.imshow('Walgreens Truck', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
