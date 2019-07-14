d2d26ef40fe0242949fe9da6cf6dfcc7103d41dd
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-13 23:48:05 -0400

    Update

--- regex/regex101.py
+++ regex/regex101.py
@@ -3,9 +3,9 @@ import re
 
 
 pattern =  re.compile(r'w{3}.\w+.com')
-email = re.compile(r'\w+\W\w+.com')
+email = re.compile(r'\w+@\w+.com')
 
-noncharacter = re.compile(r'\W')
+nonchar = re.compile(r'\W\s')
 
 text = '''from: www.jugjss569.com, to www.maondgt.com
           cc: www.kildn.com, www.jdldaa.com, ladjda@yahoo.com, poty@hdd.com
@@ -17,3 +17,6 @@ print(result)
 
 emailaddress = email.findall(text)
 print(emailaddress)
+
+noncharacter = nonchar.findall(text)
+print(noncharacter)
