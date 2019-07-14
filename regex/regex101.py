ccb522057fc9d2f200236e4ee6cfe8007bf56395
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-14 00:02:12 -0400

    Update

--- regex/regex101.py
+++ regex/regex101.py
@@ -4,19 +4,20 @@ import re
 
 pattern =  re.compile(r'w{3}.\w+.com')
 email = re.compile(r'\w+@\w+.com')
-
-nonchar = re.compile(r'\W\s')
+nonchar = re.compile(r'\W?')
 
 text = '''from: www.jugjss569.com, to www.maondgt.com
           cc: www.kildn.com, www.jdldaa.com, ladjda@yahoo.com, poty@hdd.com
           header: check this website, www.viddaf.com, email: 7344dkd@jdas.com
-          body: This is to inform me ~  ` ! @ # $ % ^ & * ( ) _ + = / . , < > : ; ' " [ ] { } \ ; -
+          body: This is to inform me 
           '''
+text2 = """! ' ~  ` ! @ # $ % ^ & * ( ) _ + = / . , < > : ; ' " [ ] { } \ ; - '"""
+
 result = pattern.findall(text)
 print(result)
 
 emailaddress = email.findall(text)
 print(emailaddress)
 
-noncharacter = nonchar.findall(text)
+noncharacter = nonchar.findall(text2)
 print(noncharacter)
