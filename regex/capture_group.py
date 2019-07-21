d9394ba1dc4d14263df49e193dd0ac217baafdaa
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-07-20 20:59:44 -0400

    Update

--- regex/capture_group.py
+++ regex/capture_group.py
@@ -19,13 +19,18 @@ def readpdf(regex):
 
 
 friends = '''John has 4 sisters while Peter loaded 5 disc and Mary boiled 3 chickens
-            for Mummy 40 years celebrate working for Perking 20 months
-            we sang tetate t5g6 in the mama gaga pump hall in the afternoon'''
+            for Mummy 40 years celebrate working for Perking 20 months, 9987 66
+            we sang 4t4t8te 7t5g6 in the mama gaga pump hall in the 4u8k9b0 afternoon'''
+
+num = 'This is just repeated 2289 8798 and 667 '
 words = r'([A-Za-z]+)\s(\d+)\s(\w+)'
 numbers = r'\d+\s\d+\s'
 readpdf(words)
 print(re.findall(words, friends))
 
 # Repeatations
+
+print(re.findall(r"\s(\d[A-Za-z])\1", friends))
+print(re.findall(r'\s(\d{2,})\s', num))
 
 
