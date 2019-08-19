834c3db58705aa6c6753e267171de612768e2790
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-18 23:26:37 -0400

    Visualize the whole world

--- geospatialAnalysis/spatialVisualization.py
+++ geospatialAnalysis/spatialVisualization.py
@@ -6,3 +6,12 @@ import pandas as pd
 print(gpd.datasets.available)
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 print(world.head())
+
+
+world.plot()
+# plt.axis("off")
+plt.xticks([]),
+plt.yticks([])
+plt.show()
+
+
