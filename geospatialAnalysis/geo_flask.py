53429846496d97cb9f651329887e42f0a3fc9056
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-24 15:31:14 -0400

    Add new file geo_flask.py

---
+++ geospatialAnalysis/geo_flask.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import folium
+
+
+path = r'C:\Users\Jose\Desktop\PythonDataScience\geospatialAnalysis'
+os.chdir(path)
+sp = '\n\n'
+
+
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+worldcity = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+
+
+# Select the country Nigeria from the map
+nigeria = world[world['name'] == 'Nigeria']
+
+nigeria.plot()
+plt.show()
