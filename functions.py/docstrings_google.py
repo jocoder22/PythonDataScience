bd5309ba1ddae993799089384bd96ac8542e6c9e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-25 23:16:48 -0400

    Add new file docstrings_google.py

---
+++ functions.py/docstrings_google.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from random import randint, choices
+from secrets import choice
+import string
+
+
+def passwordgen(n):
+    """Generate password of length n
+
+    Args:
+        n (int) : The length of the password
+
+    Returns:
+        str
+        This is string combination of letters and numbers
+
+    Raises:
+        ValueError: if n is not an integer
+    """
