116a57f82f1384f65795ee773a698ec816693793
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-27 20:57:21 -0400

    Update

--- functions.py/decorators.py
+++ functions.py/decorators.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # import os
+
+def parentFunct(s):
+    """
+
+    """
+    # Nonlocal variables
+    bet = 90
+    bet_draw = bet * 1000 / bet ** 2
+    mydict = dict({'first': 5869, 'second':346})
+    mylist = list([4905, 9403, 560])
+
+
+    def childFunct():
+        print(bet)
+        print(mylist)
+        print(mydict)
+        return bet_draw
+    
+    return childFunct
+
+
+
+myfunc = parentFunct(34)
+
+print(myfunc())
+
+
