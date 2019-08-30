77e7b3f93e9e5e9853d8eea39c56509dc819d90b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-29 22:50:33 -0400

    Update

--- functions.py/decorator3.py
+++ functions.py/decorator3.py
@@ -10,10 +10,23 @@ def mycounter(func):
         result = func(*args, **kwargs)
 
         mywrapper.count += 1
-        
-        return result
+
+        print(f'The square of {args, kwargs} is {result}')
 
 
     mywrapper.count = 0
 
     return mywrapper
+
+@mycounter
+def squarefunc(n):
+    
+    print(f'Called {squarefunc.__name__} with {n} argument!')
+
+    return n ** 2
+
+
+squarefunc(5)
+squarefunc(4)
+
+print(f'Called {squarefunc.__name__} {squarefunc.count} times')
