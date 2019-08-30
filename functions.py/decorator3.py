3f068d95fd94237daae950cef49b6f6401be3b39
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-08-29 22:36:53 -0400

    Add mycounter decorator

--- functions.py/decorator3.py
+++ functions.py/decorator3.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # import os
+
+def mycounter(func):
+    """
+
+    """
+    def mywrapper(*args, **kwargs):
+
+        result = func(*args, **kwargs)
+
+        mywrapper.count += 1
+        
+        return result
+
+
+    mywrapper.count = 0
+
+    return mywrapper
