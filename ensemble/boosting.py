b1afe45ba3efba120b39348dfb75bdac69598a15
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-02 10:43:29 -0400

    Add linearRegression model

--- ensemble/boosting.py
+++ ensemble/boosting.py
@@ -5,7 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.metrics import confusion_matrix, classification_report, mean_squared_error
+
 # from sklearn.metrics import accuracy_score
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.svm import SVC
@@ -25,7 +27,16 @@ sp = {'sep':'\n\n', 'end':'\n\n'}
 print(dir(mgt), **sp)
 
 with mgt.changepath(path):
-    npzfile = np.load('mydata.npz')
+    npzfile = np.load('mydata_l.npz')
     X_train, X_test =  npzfile['X_train'], npzfile['X_test']
     y_train, y_test =npzfile['y_train'], npzfile['y_test']
 
+
+# Linear model to predict life expectancy
+# Initialize LinearRegression model
+lreg_model = LinearRegression(normalize=True)
+lreg_model.fit(X_train, y_train)
+pred = lreg_model.predict(X_test)
+score = lreg_model.score(X_test, y_test)
+rmse = np.sqrt(mean_squared_error(y_test, pred))
+print(f'RMSE: {rmse:.02f}\nAccuracy: {score:.02f}%', **sp)
