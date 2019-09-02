1d8c13bdf5b2e241fb33a6df57ebbd513ed35a8d
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-02 10:55:42 -0400

    Add AdaBoost Model

--- ensemble/boosting.py
+++ ensemble/boosting.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import confusion_matrix, classification_report, mean_squared_error
-
+from sklearn.ensemble import AdaBoostRegressor
 # from sklearn.metrics import accuracy_score
 # from sklearn.tree import DecisionTreeClassifier
 # from sklearn.svm import SVC
@@ -35,8 +35,27 @@ with mgt.changepath(path):
 # Linear model to predict life expectancy
 # Initialize LinearRegression model
 lreg_model = LinearRegression(normalize=True)
+
+# Fit the model
 lreg_model.fit(X_train, y_train)
+
+# Predict and score the model
 pred = lreg_model.predict(X_test)
 score = lreg_model.score(X_test, y_test)
 rmse = np.sqrt(mean_squared_error(y_test, pred))
+
+# print the rmse and accuracy score
 print(f'RMSE: {rmse:.02f}\nAccuracy: {score:.02f}%', **sp)
+
+
+
+lm_adaB = AdaBoostRegressor(lreg_model, n_estimators=12, random_state=500)
+lm_adaB.fit(X_train, y_train)
+
+# Calculate the predictions on the test set
+pred_adaB = lm_adaB.predict(X_test)
+
+# Evaluate the performance using the RMSE
+score_adaB = lm_adaB.score(X_test, y_test)
+rmsea = np.sqrt(mean_squared_error(y_test, pred_adaB))
+print(f'RMSE: {rmsea:.02f}\nAccuracy: {score_adaB:.02f}%', **sp)
