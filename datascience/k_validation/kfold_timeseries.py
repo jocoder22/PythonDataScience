729b7d9b40ec4b911b4db0d837edbbefef649de2
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-07 01:16:41 -0400

    Update

--- datascience/k_validation/kfold_timeseries.py
+++ datascience/k_validation/kfold_timeseries.py
@@ -16,3 +16,23 @@ df['Date'] = pd.to_datetime(df['Date'])
 df["year"] =  df.Date.dt.year
 
 df.sort_values('Date')
+
+
+
+print(df.head(), df.info(), **sp)
+
+
+kf_timeseries_object = TimeSeriesSplit(n_splits=5)
+
+k_fold = 0
+for train_idx, test_idx in kf_timeseries_object.split(df):
+    train_cv, test_cv = df.iloc[train_idx], df.iloc[test_idx]
+    print(f'Fold: {k_fold}')
+    print(f'Train fold shape: {train_cv.shape}')
+    print(f'Train fold range: {train_cv.Date.min()} - {train_cv.Date.max()}')
+    print(f'Test fold shape: {test_cv.shape}, Test fold range: {test_cv.Date.min()} - {test_cv.Date.max()}', **sp)
+  
+    k_fold += 1
+
+
+
