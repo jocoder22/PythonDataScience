8bfcb00005ccdd80d0e6f13dc3be78c103156d9b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-07 00:59:38 -0400

    Add nk fold stratified split

--- datascience/k_validation/kfold_stratified.py
+++ datascience/k_validation/kfold_stratified.py
@@ -18,3 +18,18 @@ df["year"] =  df.Date.dt.year
 print(df.head(), df.info(), **sp)
 print(df.year.value_counts(), **sp)
 
+
+kf_stratified_object = StratifiedKFold(n_splits=5, shuffle=True, random_state=1973)
+
+k_fold = 0
+for train_idx, test_idx in kf_stratified_object.split(df, df['year']):
+    train_cv, test_cv = df.iloc[train_idx], df.iloc[test_idx]
+    print(f'Fold: {k_fold}')
+    print(f'Train fold shape: {train_cv.shape}')
+    print(f'Train fold range: {train_cv.index.min()} - {train_cv.index.max()}')
+    print(f'Test fold shape: {test_cv.shape}, Test fold range: {test_cv.index.min()} - {test_cv.index.max()}', **sp)
+  
+    k_fold += 1
+
+
+
