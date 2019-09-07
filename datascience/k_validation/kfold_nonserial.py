cc579f611ce51aca2db132cc463b23235f47f8db
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-07 00:49:55 -0400

    Update

--- datascience/k_validation/kfold_nonserial.py
+++ datascience/k_validation/kfold_nonserial.py
@@ -27,16 +27,23 @@ sp = {'sep':'\n\n', 'end':'\n\n'}
 with changepath(path):
     df = pd.read_csv('AMZN.csv')
 
+df['Date'] = pd.to_datetime(df['Date'])
+df["year"] =  df.Date.dt.year
 
-kf_object = KFold(n_splits=5, shuffle=True, random_state=1973)
+print(df.head(), df.info(), **sp)
+print(df.year.value_counts(), **sp)
+
+
+kf_object = KFold(n_splits=5, shuffle=False, random_state=1973)
 
 k_fold = 0
 for train_idx, test_idx in kf_object.split(df):
     train_cv, test_cv = df.iloc[train_idx], df.iloc[test_idx]
     print(f'Fold: {k_fold}')
     print(f'Train fold shape: {train_cv.shape}')
-    print(f'Test fold shape: {test_cv.index(test_cv.iloc[-1])}', **sp)
+    print(f'Train fold range: {train_cv.index.min()} - {train_cv.index.max()}')
+    print(f'Test fold range: {test_cv.index.min()} - {test_cv.index.max()}', **sp)
+  
     k_fold += 1
 
 
-    
