ad6527d99a72d0636976bad93f8cc75865195f1e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-08 03:20:23 -0400

    Add function

--- MachineLearning/FeatureEngineering/new_numerical.py
+++ MachineLearning/FeatureEngineering/new_numerical.py
@@ -19,3 +19,28 @@ df = pd.read_csv(url)
 
 kf = KFold(n_splits=4, shuffle=True)
 
+
+def get_kfold_rmse(train):
+    mse_scores = []
+
+    for train_index, test_index in kf.split(train):
+        train = train.fillna(0)
+        feats = [x for x in train.columns if x not in ['Id', 'SalePrice', 'RoofStyle', 'CentralAir']]
+        
+        fold_train, fold_test = train.loc[train_index], train.loc[test_index]
+
+        # Fit the data and make predictions
+        # Create a Random Forest object
+        rf = RandomForestRegressor(n_estimators=10, min_samples_split=10, random_state=123)
+
+        # Train a model
+        rf.fit(X=fold_train[feats], y=fold_train['SalePrice'])
+
+        # Get predictions for the test set
+        pred = rf.predict(fold_test[feats])
+    
+        fold_score = mean_squared_error(fold_test['SalePrice'], pred)
+        mse_scores.append(np.sqrt(fold_score))
+        
+    return round(np.mean(mse_scores) + np.std(mse_scores), 2)
+
