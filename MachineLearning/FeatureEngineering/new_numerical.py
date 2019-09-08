235d7c3b25620b9b18b61d645f0fd2f16a6f99a5
Okechukwu J. Okigbo <okigbookey@gmail.com>
2019-09-08 03:32:29 -0400

    Update

--- MachineLearning/FeatureEngineering/new_numerical.py
+++ MachineLearning/FeatureEngineering/new_numerical.py
@@ -8,9 +8,8 @@ from contextlib import contextmanager
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LogisticRegression, LinearRegression
+
 
 sp = {'sep':'\n\n', 'end':'\n\n'}
 url = 'https://assets.datacamp.com/production/repositories/4443/datasets/40af41a3b8739d0ac4b3f9f85ee43630ecbe7f0c/house_prices_train.csv'
