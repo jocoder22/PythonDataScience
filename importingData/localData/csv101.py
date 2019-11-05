abb605c110173e0dc53dd7ea9e473e416f37458f
jocoder22 <okigbookey@gmail.com>
2019-11-05 01:23:06 -0500

    Update

--- importingData/localData/csv101.py
+++ importingData/localData/csv101.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 sp = '\n\n'
 
-params = {sp:"\n\n", end:"\n\n"}
+params = {"sp":"\n\n", "end":"\n\n"}
 # charge working directory
 path = f"D:\PythonDataScience\importingData\localData"
 os.chdir(path)
@@ -20,10 +20,14 @@ print(colnames, colnames2, sep=sp)
 limitcol = ['AMZN', 'AAPL', 'MSFT', 'AGG', 'JPM']
 limitcol = [0, 4, 3, 6]
 data = pd.read_csv("portfolios.csv", usecols=limitcol)
-# colnames = names(data)
 print(data.head(), data.shape, sep=sp)
 
 # import limited rows
 data = pd.read_csv("portfolios.csv", usecols=limitcol, nrows = 200)
-# colnames = names(data)
 print(data.head(), data.shape, sep=sp)
+
+
+# skip some rows, must use header=None, and specific column names using names
+data = pd.read_csv("portfolios.csv", skiprows=2000, 
+                    nrows=300, header=None, names=colnames)
+print(data.head(), data.shape, sep=sp, end=sp)
