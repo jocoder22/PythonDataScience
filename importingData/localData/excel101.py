20f8b89d07b2823f0a44bf0b6cc5d8d57506e123
jocoder22 <okigbookey@gmail.com>
2019-11-05 23:13:16 -0500

    Update

--- importingData/localData/excel101.py
+++ importingData/localData/excel101.py
@@ -21,3 +21,32 @@ os.chdir(path)
 # load the excel file
 data = pd.read_excel("portfolioxx.xlsx")
 print(data.head())
+
+# select rows and columns
+names = ["AMZN", "MSFT", "AGG", "VNQ"]
+data = pd.read_excel("portfolioxx.xlsx", nrows=1000, skiprows=100, usecols="A,C,E:F", names=names)
+print(data.head())
+print2(data)
+
+
+# import multiple sheets
+data = pd.read_excel("portfolioSheet.xlsx",  sheet_name=None)
+for sheetname, obj in data.items():
+    print(sheetname, type(obj), end=sp)
+
+
+# append multiple columns
+stocks = pd.DataFrame()
+name = ["Open", "High", "Low", "Close", "Volume", "Adjusted"]
+for shname, ddict in data.items():
+    ddict.columns = name
+    ddict.insert(0, "Stockname", shname)
+    ddict.insert(1, "OC_diff", ddict["Open"] - ddict["Close"])
+    stocks = stocks.append(ddict)
+
+# new_order = [-1,0,1,2,3,4,5]
+# stocks = stocks[stocks.columns[new_order]]
+# print2(stocks)
+
+# stocks.insert(0, "NewAdj", stocks.Adjusted)
+print2(stocks)
