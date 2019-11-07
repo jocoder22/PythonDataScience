86a6295b1bc2b47f92784b0436ad0e66e765e288
jocoder22 <okigbookey@gmail.com>
2019-11-06 19:05:20 -0500

    Update

---
+++ NewPackages/mypackage/trader.py
@@ -0,0 +1,11 @@
+
+class NewTrader:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    def __init_subclass__(cls):
+        return super().__init_subclass__()
+
+    
