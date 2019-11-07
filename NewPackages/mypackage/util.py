55293c4e41967cd93b1887b0e03deac181ebc60f
jocoder22 <okigbookey@gmail.com>
2019-11-06 19:49:26 -0500

    Update

--- NewPackages/mypackage/util.py
+++ NewPackages/mypackage/util.py
@@ -1,3 +1,3 @@
 def print2(*args):
     for memb in args:
-        print(memb.head(), memb.info, memb.describe(), sep = '\n\n', end = '\n\n')
+        print(memb.head(), memb.info(), memb.describe(), sep = '\n\n', end = '\n\n')
