5e92f1b9f298dea73b044965ce9c607d1b5428e7
jocoder22 <okigbookey@gmail.com>
2019-11-15 23:52:45 -0500

    Update

---
+++ AREPL/arepl101.py
@@ -0,0 +1,9 @@
+# use alt+shift+a to start areply
+
+# start typing
+v = 5785
+b = 234
+area = b * v
+
+# using howdoi to find answers to question
+howdoi('function to calcuate area of circle in python')
