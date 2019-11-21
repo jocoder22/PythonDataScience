0370e7d8c0b1a12074983404b1427a91ba472a99
jocoder22 <okigbookey@gmail.com>
2019-11-20 22:58:57 -0500

    Update

---
+++ aws_botos/s3_access_private.py
@@ -0,0 +1,19 @@
+import boto3
+
+AWS_KEY_ID = "keep off this"
+AWS_SECRET = "never used this"
+BucketName = "bucketStaging"
+
+# Create the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-2', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+
+# Download file to the bucket
+s3.download_file(
+    # Complete the filename where the file will be stored locally
+    Filename='./jonah.csv', 
+    # Set the key and bucket, default setting is private
+    Key='jonah_final.csv', 
+    Bucket=BucketName )
