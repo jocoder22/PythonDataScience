80d69ae44eca74c2ab1ab6fc29b962b6a1e1b0f0
jocoder22 <okigbookey@gmail.com>
2019-11-20 21:38:32 -0500

    Update

--- aws_botos/s3_access.py
+++ aws_botos/s3_access.py
@@ -7,3 +7,31 @@ AWS_SECRET = "never used this"
 s3 = boto3.client('s3', region_name='us-east-2', 
                          aws_access_key_id=AWS_KEY_ID, 
                          aws_secret_access_key=AWS_SECRET)
+
+
+# Create the buckets
+rstaging = s3.create_bucket(Bucket='bucketStaging')
+
+# Upload file to the bucket
+s3.upload_file(
+    # Complete the filename where the file is located locally
+    Filename='./jonah.csv', 
+    # Set the key and bucket, default setting is private
+    Key='jonah_final.csv', 
+    Bucket='bucketStaging' )
+
+
+
+
+
+# Upload file to the bucket
+s3.upload_file(
+    # Complete the filename where the file is located locally
+    Filename='./jonah.csv', 
+    # Set the key and bucket
+    Key='jonah_final.csv', 
+    Bucket='bucketStaging',
+    # During upload, set ACL to public-read
+    ExtraArgs = {
+    'ACL': 'public-read'})
+
