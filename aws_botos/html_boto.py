b157eee8951e77e117c4349360366d33a1da88d2
jocoder22 <okigbookey@gmail.com>
2019-11-21 21:22:22 -0500

    Update

--- aws_botos/html_boto.py
+++ aws_botos/html_boto.py
@@ -1,12 +1,19 @@
 import pandas as pd 
 import boto3
-import os
 
-path = "D:\PythonDataScience\pandas"
 
-os.chdir(path)
-
-df = pd.read_csv("people.csv")
+with open("D:\PythonDataScience\pandas\people.csv") as file:
+    df = pd.read_csv(file)
 
 print(df.head())
 
+# Generate an HTML table with no border and selected columns
+df.to_html('./people_No_border.html', render_links=True,
+           # Keep specific columns only
+           columns= "name  age gender".split(), 
+           # Set border
+           border=0)
+
+# Generate an html table with border and all columns.
+df.to_html('./people_border.html',render_links=True,
+           border=1)
