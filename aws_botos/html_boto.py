a085fca21ea14e2060a12b9ec3fb2e69eee567f9
jocoder22 <okigbookey@gmail.com>
2019-11-21 22:23:29 -0500

    Update

--- aws_botos/html_boto.py
+++ aws_botos/html_boto.py
@@ -4,9 +4,7 @@ import boto3
 import os
 
 print(os.getcwd())
-
 path = r"D:\PythonDataScience\aws_botos\html"
-
 os.chdir(path)
 
 with open("D:\TimerSeriesAnalysis\AMZN.csv") as file:
@@ -24,3 +22,52 @@ df.to_html('./people_No_border.html', render_links=True,
 # Generate an html table with border and all columns.
 df.to_html('./people_border.html',render_links=True,
            border=1)
+
+
+##################################################################################
+############################### Create bucket ####################################
+###################################################################################
+
+AWS_KEY_ID = "keep off this"
+AWS_SECRET = "never used this"
+BucketName = "htmlObjects"
+
+# Create the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-2', 
+                        aws_access_key_id=AWS_KEY_ID, 
+                        aws_secret_access_key=AWS_SECRET)
+
+
+# Upload the lines.html file to S3
+filelist = ["people_border.html", "people_No_border.html.html"]
+keylist = ["s3people_border.html", "s3people_No_border.html.html"]
+publicUrlList = []
+
+for i, file in enumerate(filelist):
+    s3.upload_file(Filename=file, 
+               # Set the bucket name
+               Bucket='htmlObjects', Key=keylist[i],
+               # Configure uploaded file
+               ExtraArgs = {
+                 # Set proper content type
+                 'ContentType':'text/html',
+                 # Set proper ACL
+                 'ACL': 'public-read'})
+    
+    # generate public url
+    publicUrl = f"http://htmlObjects.s3.amazonaws.com/{keylist[i]}"
+    publicUrlList.append(publicUrl)
+    print(publicUrl)
+    
+    # add dataset to df
+    if i > 0:
+        df.loc[i, ["links"]] = publicUrl
+    else:
+        df["links"] = publicUrl
+
+# Generate an html table with border and all columns.
+df.to_html('./people_border.html',render_links=True,
+           border=1)
+
+
+print(df.loc[2,["Low"]])
