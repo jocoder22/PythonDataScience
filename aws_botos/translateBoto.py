da5c691242cae7735de8f20eb139a635f86ead28
jocoder22 <okigbookey@gmail.com>
2019-11-27 20:53:52 -0500

    Update

--- aws_botos/translateBoto.py
+++ aws_botos/translateBoto.py
@@ -6,6 +6,6 @@ BucketName = "htmlObjects"
 
 
 # Create the boto3 client for interacting SNS
-recog = boto3.client('translate', region_name='us-east-2', 
+transL = boto3.client('translate', region_name='us-east-2', 
                         aws_access_key_id=AWS_KEY_ID, 
                         aws_secret_access_key=AWS_SECRET)
