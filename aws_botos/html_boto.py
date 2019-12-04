9e601154f2c7c7e8d281331c4982bbdab62aed32
jocoder22 <okigbookey@gmail.com>
2019-12-03 20:08:03 -0500

    Update

--- aws_botos/html_boto.py
+++ aws_botos/html_boto.py
@@ -7,6 +7,7 @@ def print2(*args):
     for arg in args:
         print(arg, end='\n\n')
         
+sp = {"sep":"\n\n", "end":"\n\n"} 
 
 print(os.getcwd())
 path = r"D:\PythonDataScience\aws_botos\html"
