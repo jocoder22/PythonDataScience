f591b04ee7edc65d7847e9b377ede77dd51cc39e
jocoder22 <okigbookey@gmail.com>
2019-12-03 19:58:36 -0500

    Update

--- aws_botos/boto102.py
+++ aws_botos/boto102.py
@@ -8,3 +8,12 @@ def print2(*args):
     for arg in args:
         print(arg, end='\n\n')
         
+             
+# Create the boto3 client for interacting with S3 and SNS
+s3 = boto3.client('s3', region_name='us-east-2', 
+                         aws_access_key_id=AWS_KEY_ID, 
+                         aws_secret_access_key=AWS_SECRET)
+
+
+
+    
