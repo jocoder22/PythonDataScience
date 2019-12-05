59f69ef74b29f298e6899305e8fb7fba3a34409b
jocoder22 <okigbookey@gmail.com>
2019-12-04 20:29:57 -0500

    Update

--- aws_botos/s3_access_private.py
+++ aws_botos/s3_access_private.py
@@ -5,6 +5,11 @@ AWS_KEY_ID = "keep off this"
 AWS_SECRET = "never used this"
 BucketName = "bucketStaging"
 
+def print2(*args):
+    for arg in args:
+        print(arg, end='\n\n')
+
+
 # Create the boto3 client for interacting with S3 and SNS
 s3 = boto3.client('s3', region_name='us-east-2', 
                         aws_access_key_id=AWS_KEY_ID, 
