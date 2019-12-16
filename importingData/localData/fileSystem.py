2fbbb72b7283b78e7c4640de5e17da9142bf5edc
jocoder22 <okigbookey@gmail.com>
2019-12-15 21:42:20 -0500

    Update

--- importingData/localData/fileSystem.py
+++ importingData/localData/fileSystem.py
@@ -5,3 +5,11 @@ def print2(*args):
         print(arg, end='\n\n')
   
 sp = {"sep":"\n\n", "end":"\n\n"}
+
+
+print(os.getcwd(), os.listdir(), **sp)
+
+path = r"D:\PythonDataScience"
+os.chdir(path)
+
+print(os.getcwd(), os.listdir(), **sp)
