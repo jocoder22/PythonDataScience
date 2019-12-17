923403f9109f16e30f084cbc307dd6e16ffe4094
jocoder22 <okigbookey@gmail.com>
2019-12-16 21:03:41 -0500

    Update

--- pandas/pandas3.py
+++ pandas/pandas3.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 import numpy as np
 import pandas as pd
+from datetime import date
+from datetime import timedelta
+from string import ascii_letters, digits, ascii_lowercase
+
+users = 'Bob John Peter James Anna Mary'.split()
+password = [''.join(np.random.choice(list(ascii_letters + digits), 12)) for x in range(len(users))]
+listDate = [date.today() - timedelta(days=int(x)) for x in np.random.randint(2, 100, len(users))]
+FirstName = ["".join(np.random.choice(list(ascii_lowercase), 6)).capitalize() for x in range(len(users))]
+LastName = ["".join(np.random.choice(list(ascii_lowercase), 8)).capitalize() for x in range(len(users))]
+print(password, listDate, FirstName, LastName)
