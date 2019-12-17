6d003b57e479d76cb4c8055b8d41ab05a49da757
jocoder22 <okigbookey@gmail.com>
2019-12-16 21:46:15 -0500

    Update

--- pandas/pandas3.py
+++ pandas/pandas3.py
@@ -3,7 +3,13 @@ import numpy as np
 import pandas as pd
 from datetime import date
 from datetime import timedelta
-from string import ascii_letters, digits, ascii_lowercase
+from string import ascii_letters, digits, ascii_lowercase, ascii_uppercase
+
+def print2(*args):
+    for arg in args:
+        print(arg, end='\n\n')
+  
+sp = {"sep":"\n\n", "end":"\n\n"} 
 
 users = 'Bob John Peter James Anna Mary'.split()
 password = [''.join(np.random.choice(list(ascii_letters + digits), 12)) for x in range(len(users))]
@@ -18,4 +24,15 @@ df1.loc[6] = [pd.to_datetime(date(2019,11,22)), "Jane", "Vaictme", "Moandad", "j
 
 df1.drop(2, inplace=True)
 df1.drop('Password', axis=1, inplace=True)
-print(df1)
+
+myindex = ["".join(np.random.choice(list(ascii_uppercase), 3)) + "00" + str(x) for x in range(len(users))]
+df1.index = myindex
+df1.index.name = "RegNum"
+
+print2(df1, df1.columns, df1.index)
+
+df1.reset_index(inplace=True)
+print2(df1, df1.columns, df1.index)
+
+df1.set_index(['RegNum', 'User'], inplace=True)
+print2(df1, df1.columns, df1.index)
