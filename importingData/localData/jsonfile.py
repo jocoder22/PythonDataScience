cdd678ba6ea5ed103103b9913c569bf2ed7a0d02
jocoder22 <okigbookey@gmail.com>
2019-12-18 21:53:40 -0500

    Update

--- importingData/localData/jsonfile.py
+++ importingData/localData/jsonfile.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import json
 
+def print2(*args):
+    for arg in args:
+        print(arg, end='\n\n')
+        
+       
+params = {"sp":"\n\n", "end":"\n\n"}
+
 with open('myfile.json', 'r') as json_file:
     jsonData = json.load(json_file)
 
 type(jsonData) ## dict
 
 for key, value in jsonData.items():
-    print(f'{key} : {value}')
+    print(f'{key} : {value}', **params)
 
 
 # Load JSON: json_data
@@ -16,7 +23,7 @@ with open("a_movie.json") as json_file:
 
 # Print each key-value pair in json_data
 for k in json_data.keys():
-    print(k + ': ', json_data[k])
+    print(k + ': ', json_data[k], **params)
 
 
 
