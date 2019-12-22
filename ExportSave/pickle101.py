365902fd141b2c910227fa44ef60887b926ffce7
jocoder22 <okigbookey@gmail.com>
2019-12-22 13:40:25 -0500

    Save pickle file

--- ExportSave/pickle101.py
+++ ExportSave/pickle101.py
@@ -5,3 +5,9 @@ import pandas as pd
 with open(r'D:\PythonDataScience\ensemble\lifeExp.csv') as f:
     mydata = pd.read_csv(f)
     
+# save pickle file
+# method 1
+with open(r'D:\PythonDataScience\ExportSave\lifeExp.pkl', 'wb') as ppk:
+    pickle.dump(mydata, ppk)
+    
+# method 2
