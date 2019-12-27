32d06f5d1debb88a04ded353f04123894214099a
jocoder22 <okigbookey@gmail.com>
2019-12-26 23:18:53 -0500

    Update

--- cleaningData/concatMerge.py
+++ cleaningData/concatMerge.py
@@ -7,7 +7,7 @@ def print2(*args):
     for arg in args:
         print(arg, end='\n\n')
   
-
+sp = {"sep":"\n\n", "end":"\n\n"}
 
 uber1, uber2, uber3 = pd.read_csv(['uber1.csv', 'uber2.csv', 'uber3.csv'])
 
