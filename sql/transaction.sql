c795efc2e2bca17d1be2f7102f6091b96d6e6882
jocoder22 <okigbookey@gmail.com>
2019-12-28 14:10:21 -0500

    Update

--- sql/transaction.sql
+++ sql/transaction.sql
@@ -1,12 +1,14 @@
+
 BEGIN TRY 
     BEGIN TRANSACTION;
         UPDATE account SET current_balance = current_balance - 50000 WHERE accountID = 98709;
         INSERT INTO transRecord VALUES (98709, -50000, GETDATE());
 
         UPDATE account SET current_balance = current_balance + 50000 WHERE accountID = 66709;
-        INSERT INTO transRecord VALUES (66709, 50000, GETDATE())
+        INSERT INTO transRecord VALUES (66709, 50000, GETDATE());
     COMMIT TRANSACTION;
 END TRY
-BEGIN CATCH  
-	 ROLLBACK TRANSACTION;
+BEGIN CATCH
+    SELECT 'Rolling back the transaction';
+	ROLLBACK TRANSACTION;
 END CATCH
