6c9fde42362bb2e11407f884d927107244dd196a
jocoder22 <okigbookey@gmail.com>
2019-12-28 13:47:45 -0500

    Update

---
+++ sql/transaction.sql
@@ -0,0 +1,12 @@
+BEGIN TRY 
+    BEGIN TRANSACTION;
+        UPDATE account SET current_balance = current_balance - 50000 WHERE accountID = 98709;
+        INSERT INTO transRecord VALUES (98709, -50000, GETDATE());
+
+        UPDATE account SET current_balance = current_balance + 50000 WHERE accountID = 66709;
+        INSERT INTO transRecord VALUES (66709, 50000, GETDATE())
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH  
+	 ROLLBACK TRANSACTION;
+END CATCH
