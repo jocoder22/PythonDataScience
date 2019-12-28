263e5e666f659ade28ddaa37dc347ffc4c8287c1
jocoder22 <okigbookey@gmail.com>
2019-12-27 22:36:41 -0500

    Update

--- FinanacialPython/returns.py
+++ FinanacialPython/returns.py
@@ -9,7 +9,7 @@ import sklearn
 from datetime import datetime, date
 import tensorflow as tf
 
-import talib as tb
+# import talib as tb
 from tensorflow.python.keras.datasets import imdb
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.preprocessing import sequence
@@ -32,13 +32,29 @@ symbol = 'AAL'
 # symbol = 'RELIANCE.NS'
 starttime = datetime(1996, 1, 1)
 endtime = date.today()
+
 ALL = pdr.get_data_yahoo(symbol, starttime, endtime)[['Close']]
 ALL['maxClose'] = ALL.Close.max()
 ALL['minClose'] = ALL.Close.min()
 ALL['%Change'] = ALL.Close.pct_change()
 print(ALL.head())
 
+import pip
+# sorted(["%s==%s" % (i.key, i.version) for i in pip.get_installed_distributions()])
+
 # https://cv-tricks.com/tensorflow-tutorial/save-restore-tensorflow-models-quick-complete-tutorial/
 
 
+# import pkg_resources
+# installed_packages = pkg_resources.working_set
+# in_list = sorted(["%s==%s" % (i.key, i.version)
+#      for i in installed_packages])
+# print(in_list, len(in_list))
 
+
+df = pd.read_csv("D:\PythonDataScience\listt.txt", delim_whitespace=True, 
+                 skiprows=2, names=['Modules', 'Version'])
+print(df.head())
+
+mylist = list(df['Modules'])
+print(mylist)
