1fde6f8f754fbd4387e33bcedcf2106462a6ada1
jocoder22 <okigbookey@gmail.com>
2019-12-28 13:36:44 -0500

    Update

--- sql/throwError.sql
+++ sql/throwError.sql
@@ -1,14 +1,27 @@
-DECLARE @productID INT = 9854443
+
+
+DECLARE @productID INT = 9854443;
+DECLARE @error_message1 NVARCHAR(300) =
+    CONCAT('Product with ', @productID, ' does not exit in this store');
+
+DECLARE @error_message2 NVARCHAR(300) =
+    FORMATMESSAGE('Product with %d %s ', @productID, ' does not exit in this store');
+
+EXEC sp_addmessage
+    @msgnum = 56000, @severity = 18, @msgtext = 'Product with %d %s', @lang = N'us_english';
+
+DECLARE @error_message3 NVARCHAR(600) =
+    FORMATMESSAGE(56000, @productID, ' does not exit in this store');
 
 BEGIN TRY 
     SELECT * FROM sells WHERE productID = @productID
 END TRY 
 BEGIN CATCH
-    THROW 50034, 'No such product in store', 1;
+    THROW 50000, 'No such product in store', 1;
 END CATCH
 
 IF NOT EXISTS (SELECT * FROM sells WHERE productID = @productID)
-    THROW 50034, 'No such product in store', 1,
+    THROW 50000, @error_message1, 1,
 ELSE
     SELECT * FROM sells WHERE productID = @productID;
 
