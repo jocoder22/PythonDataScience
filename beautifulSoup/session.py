43220e40879aa2f8e1297c3a775b8509a859d1d6
jocoder22 <okigbookey@gmail.com>
2019-12-31 21:10:16 -0500

    Update

--- beautifulSoup/session.py
+++ beautifulSoup/session.py
@@ -5,7 +5,7 @@ def print2(*args):
     for arg in args:
         print(arg, end='\n\n')
   
-
+sp = {"sep":"\n\n", "end":"\n\n"}  
 
 session = requests.Session()
 # to see more https://www.whatismybrowser.com/
