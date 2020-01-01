5f79997f6e36c9ccca3dd83f35e9bba0acced486
jocoder22 <okigbookey@gmail.com>
2019-12-31 21:24:00 -0500

    Update

--- beautifulSoup/session2.py
+++ beautifulSoup/session2.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as Bs
 
+def print2(*args):
+    for arg in args:
+        print(arg, end='\n\n')
+        
 sp = {"sep":"\n\n", "end":"\n\n"}  
 
 # Begin a session
