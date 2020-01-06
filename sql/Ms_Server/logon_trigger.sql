05147eae8b3296fb81aa6fe6933db09f58c04c92
jocoder22 <okigbookey@gmail.com>
2020-01-05 21:51:19 -0500

    Update

---
+++ sql/Ms_Server/logon_trigger.sql
@@ -0,0 +1,17 @@
+
+-- Create trigger to log logins
+IF NOT EXISTS ( SELECT  name
+            FROM    sys.serve_triggers
+            WHERE   type = 'TR'
+                    AND name = 'logonTrigger' ) 
+BEGIN
+    EXEC('CREATE TRIGGER logonTrigger
+ON ALL SERVER WITH EXECUTE AS ''sa''
+FOR LOGON
+As 
+    INSERT INTO LogonLog(LogonName, LogonDate, SessionID, SourceIPAddress)
+    SELECT ORIGINAL_LOGIN(), GETDATE(), @@spid, client_net_address
+    FROM sys.dm_exec_connections WHERE session_id = @@SPID');
+END
+GO
+
