451eee416dfd7ed3527b3f3ce1f765011f641bd3
jocoder22 <okigbookey@gmail.com>
2020-01-06 19:27:01 -0500

    Add new file download_data.py

--- sql/Ms_Server/logon_trigger.sql
+++ sql/Ms_Server/logon_trigger.sql
@@ -14,3 +14,32 @@ As
 END
 GO
 
+EXEC msdb.dbo.sp_send_dbmail  
+    @profile_name = 'Adventure Works Administrator',  
+    @recipients = 'okigbookey@gmail.com',  
+    @body = 'The stored procedure finished successfully.',  
+    @subject = 'Automated Success Message' ; 
+
+-- DESKTOP-STF6SAO
+
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_set_sqlagent_properties @email_save_in_sent_folder=1, 
+		@databasemail_profile=N''
+GO
+
+
+Configuring...
+
+- Create new account 'SQLAlerts' for SMTP server 'DESKTOP-STF6SAO' (Success)
+
+- Create New profile 'okeyokigbo' (Success)
+
+- Add account 'SQLAlerts' to profile 'okeyokigbo' with priority '1' (Success)
+
+- Grant 'guest' access to 'okeyokigbo' (Success)
+
+
+
+-- https://www.sqlshack.com/email-sql-query-results-smartphone-using-sp_send_dbmail-stored-procedure/
+-- https://www.mssqltips.com/sqlservertip/2922/sql-server-alerts-with-text-messaging-from-sql-server-database-mail/
