02e7cbe22784be96ca07e6624be23b17a7b79f90
jocoder22 <okigbookey@gmail.com>
2020-01-08 03:41:44 -0500

    Update

--- MongoDB/query101.py
+++ MongoDB/query101.py
@@ -1,10 +1,12 @@
 import requests
 from pymongo import MongoClient
 
+sp = {"sep":"\n\n", "end":"\n\n"}
+
 client = MongoClient()
 
 db = client["nobelprizes"]
 
 # Save a list of names of the collections managed by the "nobelprizes" database
 nobel_coll_names = client.nobelprizes.list_collection_names()
-print(nobel_coll_names)
+print(nobel_coll_names, **sp)
