77f78ffa6356cb9612c123329b48c0f02b5ef8ba
jocoder22 <okigbookey@gmail.com>
2020-01-08 04:00:51 -0500

    Update

--- MongoDB/query101.py
+++ MongoDB/query101.py
@@ -12,11 +12,15 @@ nobel_coll_names = client.nobelprizes.list_collection_names()
 print(nobel_coll_names, **sp)
 
 
-# Retrieve sample prize, countries, and laureate documents
+# Retrieve sample prize, country, and laureate documents
 prize_doc = db.prizes.find_one()
 country_doc = db.country.find_one()
 laureate_doc = db.laureates.find_one()
 
-# Print the sample prize, countries, and laureate documents
+# Print the sample prize, country, and laureate documents
 print(prize_doc, country_doc, laureate_doc, **sp)
 print(type(prize_doc), type(country_doc), type(laureate_doc), **sp)
+
+# Print the fields of each document
+for doc in [prize_doc, country_doc, laureate_doc]:
+    print(list(doc.keys()), **sp)
