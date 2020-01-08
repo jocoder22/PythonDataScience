acd727a6eb152cc07b4be55dfbc28d64edd17cb2
jocoder22 <okigbookey@gmail.com>
2020-01-08 03:34:37 -0500

    Update

--- MongoDB/download_data.py
+++ MongoDB/download_data.py
@@ -1,12 +1,14 @@
 import requests
 from pymongo import MongoClient
 
+sp = {"sep":"\n\n", "end":"\n\n"}
+
 # https://www.tutorialspoint.com/mongodb/mongodb_drop_database.htm
 # first on commmand prompt type : mongod, then open another command prompt type: mongo
 client = MongoClient()
 
 db = client["nobelprizes"]
-print(db.name)
+print(db.name, **sp)
 
 for collections_m in ["prizes", "laureates", "countries"]:
     db[collections_m].delete_many({})
@@ -29,19 +31,29 @@ for collections_m in ["prizes" , "countrys" , "laureates"]:
 
 # Save a list of names of the databases managed by client
 db_names = client.list_database_names()
-print(db_names)
+print(db_names, **sp)
 
 # Save a list of names of the collections managed by the "nobel" database
 nobel_coll_names = client.nobelprizes.list_collection_names()
-print(nobel_coll_names)
+print(nobel_coll_names, **sp)
 
 filter = {}
 
 for collections_m in ["prizes", "laureates", "countries"]:
     bb = db[collections_m].count_documents({})
-    print(f'The {collections_m} has {bb} rows', end="\n\n")
+    print(f'The {collections_m} has {bb} rows', **sp)
 
 doc = db.prizes.find_one(filter)
-print(doc)
+print(doc, **sp)
+
+
+# Retrieve sample prize and laureate documents
+prize_one = db.prizes.find_one()
+laureate_one = db.laureates.find_one()
+
+# Print the sample prize and laureate documents
+print(prize_one, **sp)
+print(laureate_one, **sp)
+print(type(laureate_one), type(laureate_one), **sp)
 
 
