be864e0219db5a1c9905dc54054251b74c9c5ee9
jocoder22 <okigbookey@gmail.com>
2020-01-10 19:19:17 -0500

    Update

--- MachineLearning/SupervisedME/MarketingME/decisiontree.py
+++ MachineLearning/SupervisedME/MarketingME/decisiontree.py
@@ -2,7 +2,11 @@ import pandas as pd
 import os 
 from glob import glob
 
-
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
+        
+        
 # load pickle file
 mydir = "D:\PythonDataScience\MachineLearning\SupervisedME\MarketingME"
 features = pd.read_pickle(os.path.join(mydir, "features.pkl"))
@@ -11,4 +15,5 @@ target= pd.read_pickle(os.path.join(mydir, "target.pkl"))
 
 print(features.head(), target.head(), end="\n\n")
 ddd = features.loc[:,["tenure",  "MonthlyCharges",  "TotalCharges"]].agg(["mean", "std"]).round()
+
 
