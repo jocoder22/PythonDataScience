bffb870a1a8f3ecc5592bfb87bfb19baa768c454
jocoder22 <okigbookey@gmail.com>
2020-01-12 07:09:17 -0500

    Update

--- MachineLearning/SupervisedME/MarketingME/logisticReg.py
+++ MachineLearning/SupervisedME/MarketingME/logisticReg.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.linear_model import LogisticRegression, export_graphviz
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from pydotplus.graphviz import graph_from_dot_data
@@ -22,10 +22,23 @@ features = pd.read_pickle(os.path.join(mydir, "features.pkl"))
 target= pd.read_pickle(os.path.join(mydir, "target.pkl"))
 
 features.fillna(features.mean(),inplace=True)
+target = target.loc[:, "Churn"]
 
 X_train, X_test, Y_train, Y_test = train_test_split(features, target, test_size=0.2, stratify=target)
 
 print2(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
 
 # Initialize the model
-logreg = LogisticRegression(random_state=1973)
+logreg = LogisticRegression(penalty = "l1", C = 0.1, solver = "liblinear", random_state=1973)
+
+
+model = logreg.fit(X_train, Y_train)
+
+Y_predict_test = model.predict(X_test)
+Y_predict_train = model.predict(X_train)
+
+accuracyscoreTest = accuracy_score(Y_test, Y_predict_test)
+accuracyscoreTrain = accuracy_score(Y_train, Y_predict_train)
+
+print2("{} accuracy score : {:.2f}".format("Test", accuracyscoreTest),
+       "{} accuracy score : {:.2f}".format("Train", accuracyscoreTrain))
