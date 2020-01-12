bd5eb90f359cf7a72a3e1b27270e4056fdf19f71
jocoder22 <okigbookey@gmail.com>
2020-01-12 06:33:34 -0500

    Update

--- MachineLearning/SupervisedME/MarketingME/decisiontree.py
+++ MachineLearning/SupervisedME/MarketingME/decisiontree.py
@@ -1,7 +1,8 @@
 import os 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score
