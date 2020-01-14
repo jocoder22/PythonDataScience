73eb9ead64bb3b6465698ca36cbea75adef4d351
jocoder22 <okigbookey@gmail.com>
2020-01-13 22:45:09 -0500

    Update

--- MachineLearning/SupervisedME/MarketingME/onlinesale.py
+++ MachineLearning/SupervisedME/MarketingME/onlinesale.py
@@ -4,7 +4,11 @@ import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 import datetime as dt
-from datetime import timedelta
+# from datetime import date as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+# plt.style.use('ggplot')
+# plt.style.use('seaborn-whitegrid')
 
 def print2(*args):
     for arg in args:
@@ -33,7 +37,7 @@ onlinedata = pd.read_pickle(os.path.join(mydir, "onlinedata.pkl"))
 # 'InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate',
 #        'UnitPrice', 'CustomerID', 'Country'],
 #       dtype='object')
-onlinedata['InvoiceMonth'] = onlinedata['InvoiceDate'].apply(lambda x: dt.datetime(x.year, x.month, x.day))
+onlinedata['InvoiceMonth'] = onlinedata['InvoiceDate'].apply(lambda x: dt.datetime(x.year, x.month, 1))
 customergroups = onlinedata.groupby('CustomerID')['InvoiceMonth']
 onlinedata['CohortMonth'] = customergroups.transform('min')
 onlinedata['CohortMonth2'] = onlinedata.groupby('CustomerID')['InvoiceDate'].transform('min')
@@ -50,4 +54,30 @@ onlinedata['cohortIndex'] = yeardiff * 12 + monthdiff + 1
 onlinedata['cohortIndex2'] = onlinedata["InvoiceDate"] - onlinedata['CohortMonth2'] 
 onlinedata['cohortIndex2'] = round(onlinedata['cohortIndex2']/np.timedelta64(1, "M") + 1)
 
-print2(onlinedata.loc[:, "InvoiceDate": ].tail(), onlinedata.shape, onlinedata.columns)
+cohortgroups = onlinedata.groupby(["CohortMonth", "cohortIndex"])
+cohortNumber = cohortgroups["CustomerID"].apply(pd.Series.nunique).reset_index()
+
+
+cohortTable = cohortNumber.pivot(index = "CohortMonth",
+                                 columns = "cohortIndex",
+                                 values = "CustomerID")
+
+Retention = cohortTable.divide(cohortTable.iloc[:, 0], axis=0).round(3) 
+
+print2(onlinedata.loc[:, "InvoiceDate": ].tail(), onlinedata.info(), onlinedata.columns, cohortNumber, cohortTable, Retention)
+Retention.index = Retention.index.strftime("%Y-%m-%d")
+
+
+plt.figure(figsize=(12, 10))
+sns.heatmap(data = Retention,
+            annot = True,
+            fmt = '.0%',
+            vmin = 0.0, vmax = 0.5,
+            # cmap = "BuGn")
+            )
+
+# plt.yticks(rotation=0)
+plt.title("Retention Ratio")
+plt.show()
+
+print2(Retention.info(), Retention.index)
