5d6258b19d23e369f50bb8f5bc33520fe4bbaef6
jocoder22 <okigbookey@gmail.com>
2020-01-13 21:23:12 -0500

    Update

--- MachineLearning/SupervisedME/MarketingME/onlinesale.py
+++ MachineLearning/SupervisedME/MarketingME/onlinesale.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+import datetime as dt
+from datetime import timedelta
 
 def print2(*args):
     for arg in args:
@@ -27,6 +29,25 @@ mydir = "D:\PythonDataScience\MachineLearning\SupervisedME\MarketingME"
 
 # load pickle file
 onlinedata = pd.read_pickle(os.path.join(mydir, "onlinedata.pkl"))
-print2(onlinedata.head(), onlinedata.shape)
 
+# 'InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate',
+#        'UnitPrice', 'CustomerID', 'Country'],
+#       dtype='object')
+onlinedata['InvoiceMonth'] = onlinedata['InvoiceDate'].apply(lambda x: dt.datetime(x.year, x.month, x.day))
+customergroups = onlinedata.groupby('CustomerID')['InvoiceMonth']
+onlinedata['CohortMonth'] = customergroups.transform('min')
+onlinedata['CohortMonth2'] = onlinedata.groupby('CustomerID')['InvoiceDate'].transform('min')
 
+# Extract year and month for each invoiceDate and cohort group
+customerYear, customerMonth = onlinedata['InvoiceMonth'].dt.year, onlinedata['InvoiceMonth'].dt.month
+cohortYear, cohortMonth =  onlinedata['CohortMonth'].dt.year, onlinedata['CohortMonth'].dt.month
+
+yeardiff = customerYear - cohortYear
+monthdiff = customerMonth - cohortMonth
+onlinedata['cohortIndex'] = yeardiff * 12 + monthdiff + 1
+
+# Anothe method to extract cohortIndex
+onlinedata['cohortIndex2'] = onlinedata["InvoiceDate"] - onlinedata['CohortMonth2'] 
+onlinedata['cohortIndex2'] = round(onlinedata['cohortIndex2']/np.timedelta64(1, "M") + 1)
+
+print2(onlinedata.loc[:, "InvoiceDate": ].tail(), onlinedata.shape, onlinedata.columns)
