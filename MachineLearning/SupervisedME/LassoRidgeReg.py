362d5f00ddd8d81b27afb68e36f8c99082df52f2
jocoder22 <okigbookey@gmail.com>
2020-01-20 18:51:02 -0500

    Update

--- MachineLearning/SupervisedME/LassoRidgeReg.py
+++ MachineLearning/SupervisedME/LassoRidgeReg.py
@@ -99,7 +99,7 @@ display_plot(ridge_scores, ridge_scores_std)
 
 # Create training and test set
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.4, random_state=42)
+    X, y2, test_size=0.2, random_state=42)
 
 # Instantiate a k-NN classifier: knn
 knn = KNeighborsClassifier(n_neighbors=6)
@@ -118,7 +118,7 @@ print(classification_report(y_test, y_pred))
 
 # Create train and test sets
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.4, random_state=42)
+    X, y, test_size=0.2, random_state=42)
 
 # Create the hyperparameter grid
 l1_space = np.linspace(0, 1, 30)
