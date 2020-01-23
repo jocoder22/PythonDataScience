f11460b7d789677855c3ca80817ed977884136cc
jocoder22 <okigbookey@gmail.com>
2020-01-23 18:55:18 -0500

    Update

--- MachineLearning/SupervisedME/xgboost_API/xgb_regres.py
+++ MachineLearning/SupervisedME/xgboost_API/xgb_regres.py
@@ -9,9 +9,21 @@ from sklearn import datasets
 def print2(*args):
     for arg in  args:
         print(arg, end="\n\n")
-        
-cal_housing = datasets.fetch_california_housing()
-X = pd.DataFrame(cal_housing.data, columns=cal_housing.feature_names)
+
+# Load the California Housing Data     
+calhousing = datasets.fetch_california_housing()
+X = pd.DataFrame(calhousing.data, columns=calhousing.feature_names)
 y = cal_housing.target
 
 print2(X.head(), X.shape, y[:5])
+
+
+# Create training and test set
+X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+
+
+parameters = {'max_depth': 4, 'n_estimators':10, 'objective':'binary:logistic'}
+
+# Instantiate the XGBClassifier: xgbooster
+xgbooster = xgb.XGBClassifier(params=parameters)
