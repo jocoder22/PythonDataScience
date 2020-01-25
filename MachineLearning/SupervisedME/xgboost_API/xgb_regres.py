24b698c220383fe6c1e50879133574537028ac40
jocoder22 <okigbookey@gmail.com>
2020-01-24 20:00:15 -0500

    Update

--- MachineLearning/SupervisedME/xgboost_API/xgb_regres.py
+++ MachineLearning/SupervisedME/xgboost_API/xgb_regres.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, roc_auc_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn import datasets
 
 def print2(*args):
@@ -24,10 +24,10 @@ X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.2, random_state=42)
 
 
-parameters = {'max_depth': 4, 'n_estimators':10, "booster":"gblinear", "objective":"reg:linear"}
+parameters = {'max_depth': 4, 'n_estimators':10, 'booster':'gblinear'}
 
 # Instantiate the XGBClassifier: xgbRegress
-xgbRegress = xgb.XGBRegressor(params=parameters)
+xgbRegress = xgb.XGBRegressor(objective='reg:squarederror', params=parameters)
 
 # Convert the training and testing sets into DMatrixes: _train, _test
 _train = xgb.DMatrix(data=X_train, label=y_train)
@@ -42,4 +42,51 @@ yhat = xgbRegress.predict(X_test)
 
 # Compute and print the RMSE
 rmse = np.sqrt(mean_squared_error(y_test, yhat))
-print("RMSE: %f" % (rmse))
+mae = mean_absolute_error(y_test, yhat)
+print2(f"RMSE: {rmse}", f"MAE Score: {mae}")
+
+
+
+# Create the DMatrix: California Housing Data 
+_dmatrix = xgb.DMatrix(data=X, label=y)
+
+parameters = {'objective':'reg:squarederror', 'max_depth': 4}
+
+# Perform cross-validation: _results
+_results = xgb.cv(dtrain=_dmatrix,  params=parameters, nfold=4,
+                  num_boost_round=8, metrics=["error", "mae", "rmse"], as_pandas=True)
+
+# Print _results
+print(_results)
+
+# Extract and print final boosting round metric
+print((_results["test-rmse-mean"]).tail(1))
+
+
+# Create the DMatrix: reg_params
+regparams = [1, 10, 100]
+
+# Create the initial parameter dictionary for varying l2 strength: params
+params = {"objective":"reg:squarederror", "max_depth":3}
+
+# Create an empty list for storing rmses as a function of l2 complexity
+rmses = []
+
+# Iterate over reg_params
+for reg in regparams:
+
+    # Update l2 strength
+    params["lambda"] = reg
+    
+    # Pass this updated param dictionary into cv
+    _results_rmse = xgb.cv(dtrain=_dmatrix, params=params, nfold=4, 
+                             num_boost_round=5, metrics="rmse", as_pandas=True)
+    
+    # Append best rmse (final round) to rmses_l2
+    rmses.append(_results_rmse["test-rmse-mean"].tail(1).values[0])
+
+
+# Look at best rmse 
+df = pd.DataFrame(list(zip(regparams, rmses)), columns=["l2","rmse"])
+print2(df)
+
