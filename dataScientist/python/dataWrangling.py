fabc92fbaa187c95d50f9cb5dc89ff5b1bd12408
jocoder22 <okigbookey@gmail.com>
2020-02-08 11:00:52 -0500

    Update

--- dataScientist/python/dataWrangling.py
+++ dataScientist/python/dataWrangling.py
@@ -6,6 +6,7 @@ def print2(*args):
         print(arg, end="\n\n")
 
 path = r"C:\Users\okigb\Downloads\survey-results-public.csv"
+path2 = r"C:\Users\okigb\Downloads\survey-results-schema.csv"
 
 df = pd.read_csv(path)
 
@@ -13,4 +14,15 @@ df2 = df.CousinEducation.value_counts().reset_index()
 df2.rename(columns={"index":"Method", "CousinEducation":"Count"}, inplace=True)
 
 print2(df.CousinEducation, df.columns, df.shape, df2.head())
+
+schema = pd.read_csv(path2)
+print2(schema, list(schema[schema.Column == "CousinEducation"]["Question"]))
+
+answerlist = []
+for i in range(df2.shape[0]):
+    for k in  df2['Method'][i].split("; "):
+        if k not in answerlist:
+            answerlist.append(k)
+            
+print2(answerlist)
 
