6d742ec5d1753f92cf1b8310670abfdba055c299
jocoder22 <okigbookey@gmail.com>
2020-02-08 10:23:54 -0500

    Update

--- dataScientist/python/dataWrangling.py
+++ dataScientist/python/dataWrangling.py
@@ -1,3 +1,16 @@
 import numpy as np
 import pandas as pd
 
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
+
+path = r"C:\Users\okigb\Downloads\survey-results-public.csv"
+
+df = pd.read_csv(path)
+
+df2 = df.CousinEducation.value_counts().reset_index()
+df2.rename(columns={"index":"Method", "CousinEducation":"Count"}, inplace=True)
+
+print2(df.CousinEducation, df.columns, df.shape, df2.head())
+
