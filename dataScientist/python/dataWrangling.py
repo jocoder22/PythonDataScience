226272a29cbdb9ecac63a58309f742989cd1b5bf
jocoder22 <okigbookey@gmail.com>
2020-02-08 11:15:16 -0500

    Update

--- dataScientist/python/dataWrangling.py
+++ dataScientist/python/dataWrangling.py
@@ -4,6 +4,21 @@ import pandas as pd
 def print2(*args):
     for arg in args:
         print(arg, end="\n\n")
+        
+def countInline(dataframe, c1, c3, lookuplist):
+    from collections import defaultdict
+    mydict = defaultdict(int)
+    
+    for i in range(dataframe.shape[0]):
+        for answer in lookuplist:
+            if dataframe[c1][i].split("; "):
+                mydict[answer] += int(dataframe[c2][i])
+    
+    mydata = pd.DataFrame(pd.Series(mydict)).reset_index()
+    mydata.columns = [c1, c2]
+    mydata.sort_values(c2, ascending=False, inplace=True)
+    return mydata
+    
 
 path = r"C:\Users\okigb\Downloads\survey-results-public.csv"
 path2 = r"C:\Users\okigb\Downloads\survey-results-schema.csv"
