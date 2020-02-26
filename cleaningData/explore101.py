b6abf0f0e7759e3351af4668257de9bebb6aa497
jocoder22 <okigbookey@gmail.com>
2020-02-25 20:51:10 -0500

    Update

--- cleaningData/explore101.py
+++ cleaningData/explore101.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd 
 
 
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
+        
+        
 # download World Bank population data
 worldpop = pd.read_excel("http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=excel", 
                        index_col=None, skiprows=3)
