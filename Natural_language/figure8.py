eda836a4999289deee193d7f6fcdc8b7c34955f4
Okechukwu Okigbo <okigbookey@gmail.com>
2020-03-08 10:22:16 -0400

    Update figure8.py

--- Natural_language/figure8.py
+++ Natural_language/figure8.py
@@ -5,3 +5,8 @@ comeg = "https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/Corpor
 
 message = pd.read_csv(comeg, encoding='latin-1')
 print(message.columns)
+
+message = pd.read_csv(comeg, encoding='latin-1')
+mess = message[(message['category'] != "Exclude") & (message[ 'category:confidence'] == 1)]
+
+print(mess.category.value_counts())
