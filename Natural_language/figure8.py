97e1268e0f1056e30ded4777b0c114898fb7f82d
Okechukwu Okigbo <okigbookey@gmail.com>
2020-03-08 10:59:32 -0400

    Update figure8.py

--- Natural_language/figure8.py
+++ Natural_language/figure8.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
 
-comeg = "https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/Corporate-messaging-DFE.csv"
 
-message = pd.read_csv(comeg, encoding='latin-1')
-print(message.columns)
+def load_data():
+  comeg = "https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/Corporate-messaging-DFE.csv"
 
-message = pd.read_csv(comeg, encoding='latin-1')
-mess = message[(message['category'] != "Exclude") & (message[ 'category:confidence'] == 1)]
+  message = pd.read_csv(comeg, encoding='latin-1')
+#   print(message.columns)
 
-print(mess.category.value_counts())
+  mess = message[(message['category'] != "Exclude") & (message[ 'category:confidence'] == 1)]
+  
+  x = message.text.values()
+  y = message.category.values()
+  
+  return x, y
+
+
