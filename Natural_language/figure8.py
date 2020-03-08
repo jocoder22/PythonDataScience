5b04f6c6b575ac3d59db946c0014c5bb26d4ea67
Okechukwu Okigbo <okigbookey@gmail.com>
2020-03-08 10:22:01 -0400

    Update figure8.py

--- Natural_language/figure8.py
+++ Natural_language/figure8.py
@@ -1,1 +1,7 @@
+import pandas as pd
 
+
+comeg = "https://d1p17r2m4rzlbo.cloudfront.net/wp-content/uploads/2016/03/Corporate-messaging-DFE.csv"
+
+message = pd.read_csv(comeg, encoding='latin-1')
+print(message.columns)
