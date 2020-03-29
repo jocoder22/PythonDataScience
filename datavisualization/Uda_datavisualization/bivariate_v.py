68cedb0532506a953706a87cacf3e78512647c18
jocoder22 <okigbookey@gmail.com>
2020-03-28 21:55:33 -0400

    Update

--- datavisualization/Uda_datavisualization/bivariate_v.py
+++ datavisualization/Uda_datavisualization/bivariate_v.py
@@ -8,9 +8,26 @@ def print2(*args):
     for arg in args:
         print(arg, sep="\n\n", end="\n\n")
 
+path = "D:\PythonDataScience\datavisualization\Uda_datavisualization"
+os.chdir(path)
+
+fcon = pd.read_csv('fuel-econ.csv')
 
 url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2020-03-13/visualisations/listings.csv"
 
 airbnb = pd.read_csv(url)
 
-print2(airbnb.head(), airbnb.columns)
+print2(airbnb.head(), airbnb.columns, airbnb.info())
+
+# bivariate both numeric
+plt.scatter(data=airbnb, x="latitude", y="longitude")
+plt.show()
+
+plt.scatter(data=airbnb, x="price", y="longitude")
+plt.show()
+
+
+airbnb2 = airbnb.loc[airbnb.price < 200]
+airbnb2['logprice'] = np.log10(airbnb2['price'])
+sns.regplot(data=airbnb2, x="logprice", y="number_of_reviews")
+plt.show()
