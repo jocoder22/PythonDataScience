ab087d707e2464111abdb8c97581b9233eab5e51
jocoder22 <okigbookey@gmail.com>
2020-04-06 22:48:59 -0400

    Update

--- computational_finance/option_pricing.py
+++ computational_finance/option_pricing.py
@@ -7,43 +7,69 @@ import matplotlib.pyplot as plt
 import pandas_datareader as pdr
 from datetime import datetime, date, timedelta
 
-from iexfinance.refdata import get_symbols
-from iexfinance.stocks import Stock, get_historical_intraday, get_historical_data
+# from iexfinance.refdata import get_symbols
+# from iexfinance.stocks import Stock, get_historical_intraday, get_historical_data
 
-pathtk = r"D:\PPP"
-sys.path.insert(0, pathtk)
+# pathtk = r"D:\PPP"
+# sys.path.insert(0, pathtk)
 
-import wewebs
-
-
-def print2(*args):
-    for arg in args:
-        print(arg, sep="\n\n", end="\n\n")
+# import wewebs
 
 
-sp = {'sep': '\n\n', 'end': '\n\n'}
-
-path = r"D:\Intradays"
-
-ttt = wewebs.token
-
-stock = "NFLX"
-
-startdate = datetime(2016, 2, 2)
-enddate = datetime(2018, 5, 30)
-stdate = date.today() - timedelta(days=456)
+# def print2(*args):
+#     for arg in args:
+#         print(arg, sep="\n\n", end="\n\n")
 
 
-# allstocks = pdr.get_data_yahoo(stock, startdate)['Adj Close']
-# print(allstocks.head())
-get_symbols(output_format='pandas', token=ttt)
+# sp = {'sep': '\n\n', 'end': '\n\n'}
 
-neflex = Stock(stock, token=ttt)
-print2(neflex.get_quote()['close'])
+# path = r"D:\Intradays"
 
-start = datetime(2017, 1, 1)
-end = datetime(2018, 1, 1)
+# ttt = wewebs.token
 
-df = get_historical_data("TSLA", start, end, token=ttt, output_format='pandas')
+# stock = "NFLX"
 
-print2(df.close.var())
+# startdate = datetime(2016, 2, 2)
+# enddate = datetime(2018, 5, 30)
+# stdate = date.today() - timedelta(days=456)
+
+
+# # allstocks = pdr.get_data_yahoo(stock, startdate)['Adj Close']
+# # print(allstocks.head())
+# get_symbols(output_format='pandas', token=ttt)
+
+# neflex = Stock(stock, token=ttt)
+# print2(neflex.get_quote()['close'])
+
+# start = datetime(2017, 1, 1)
+# end = datetime(2018, 1, 1)
+
+# df = get_historical_data("TSLA", start, end, token=ttt, output_format='pandas')
+
+# print2(df.close.var())
+
+
+startdate = datetime(2013, 2, 2)
+# enddate = datetime(2018, 5, 30)
+
+stock = 'NFLX'
+
+allstocks = pdr.get_data_yahoo(stock, startdate)
+# Compute the logarithmic returns using the Closing price 
+allstocks['Log_Ret'] = np.log(allstocks['Close'] / allstocks['Close'].shift(1))
+
+# Compute Volatility using the pandas rolling standard deviation function
+allstocks['Volatility'] = allstocks['Log_Ret'].rolling(window=252).std() * np.sqrt(252)
+
+# Plot the NIFTY Price series and the Volatility
+allstocks[['Close', 'Volatility']].plot(subplots=True, color='blue',figsize=(8, 6))
+plt.show()
+
+print(allstocks.head())
+
+
+
+def sharpe(returns, rf, days=252):
+    volatility = returns.std() * np.sqrt(days) 
+    sharpe_ratio = (returns.mean() - rf) / volatility
+    return sharpe_ratio
