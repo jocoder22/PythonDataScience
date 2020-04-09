432309c6616b9526fa7d3d11e73c3fa4eccc755f
jocoder22 <okigbookey@gmail.com>
2020-04-08 21:03:26 -0400

    Update

--- Natural_language/scapy101.py
+++ Natural_language/scapy101.py
@@ -3,3 +3,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import spacy
+
+
+sp = '\n\n'
+largge = 'en_core_web_lg'
+path = r"D:\PythonDataScience\tweeter"
+os.chdir(path)
+
+data = pd.read_csv('nyt2.csv')
+
+text_clean = []
+
+mytext = str()
+
+for text in data['News_content']:
+    mytext += text + " "
+
+print(data.head())
