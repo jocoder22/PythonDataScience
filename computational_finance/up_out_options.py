61d0ae4a98cccae69867dbbe32a73f39f5140cf1
jocoder22 <okigbookey@gmail.com>
2020-04-11 08:20:01 -0400

    Update

---
+++ computational_finance/up_out_options.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as pdr
+from datetime import datetime, date, timedelta
+from scipy.integrate import quad
+from scipy.stats import norm
+from scipy.stats import uniform
+import math
