3ce996e96dab4f93c6b6e019624b8497ec3f3864
jocoder22 <okigbookey@gmail.com>
2020-04-14 21:07:34 -0400

    Add terminal value function

--- computational_finance/cva.py
+++ computational_finance/cva.py
@@ -26,3 +26,28 @@ v0 = 200                # firm current value
 sigma_firm = 0.25       # firm volatility 
 debt = 180              # firm debt
 recovery_rate = 0.2     # recovery rate
+
+
+
+
+# 1. Write a function which takes a risk-free rate, the initial share price, the share volatility, 
+# and term as inputs, and determines the terminal value of a share price, 
+# assuming geometric Brownian Motion. Note, you should vectorize this function where possible. 
+
+def terminal_shareprice(present_price, risk_free, sigma, Z, T):
+    """ terminal_shareprice function gives the terminal value of a share price,
+        assuming geometric Brownian Motion and vectorization where possible.
+        
+    Inputs: 
+        present_price(float/int): initial share price
+        riskfree(float/int): risk free rate
+        sigma: share volatility
+        Z: normal random variables
+        T(float/int): term of share price
+        
+    Output:
+        terminal value of a share price
+    
+    """
+    
+    return present_price*np.exp((risk_free - sigma**2/2)*T + sigma*np.sqrt(T)*Z)
