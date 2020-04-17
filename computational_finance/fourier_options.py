ce0b9db5622c074a977208cb8f046f7b92e2ec66
Okechukwu Okigbo <okigbookey@gmail.com>
2020-04-17 09:51:54 -0400

    Update fourier_options.py

--- computational_finance/fourier_options.py
+++ computational_finance/fourier_options.py
@@ -1,1 +1,8 @@
 
+# We will be pricing a vanilla European call option on a single stock under the following conditions:
+# o Continuously-compounded interest rate, 𝑟, of 6%
+# o Initial stock price, 𝑆0, of $100
+# o Stock volatility, 𝜎, 30%
+# o Strike price, 𝐾, of $110
+# o Maturity time, 𝑇, of one year
+# As per usual, we make all the assumptions of the Black-Scholes model.
