be5c24ce4fd3f38ffb60062415012b82509539db
Okechukwu Okigbo <okigbookey@gmail.com>
2020-04-30 14:56:07 -0400

    Update short_rates.py

--- computational_finance/short_rates.py
+++ computational_finance/short_rates.py
@@ -10,5 +10,13 @@ b = 0.08
 sigma = 0.025
 
 
-#  define useful function
+#  define useful functions
+def vasicef_mean(r, t1, t2):
+  """
+  
+  """
+  r0_discounted = r * np.exp(-alpha*(t2-t1))
+  b_discounted = b * (1 - np.exp(-alpha*(t2-t1)))
+  
+  return r0_discounted + b_discounted
 
