50178a14732425b15fc7c3e466d841d1a60e7e1a
Okechukwu Okigbo <okigbookey@gmail.com>
2020-04-30 14:49:48 -0400

    Update short_rates.py

--- computational_finance/short_rates.py
+++ computational_finance/short_rates.py
@@ -1,1 +1,14 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import random
+
+# set market parameters
+r0 =  0.05
+alpha = 0.2
+b = 0.08
+sigma = 0.025
+
+
+#  define useful function
 
