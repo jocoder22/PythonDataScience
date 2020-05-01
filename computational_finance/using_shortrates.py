262319b5121c88bb2c0de83c441908ef8a65bc9a
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-01 07:58:00 -0400

    Update using_shortrates.py

--- computational_finance/using_shortrates.py
+++ computational_finance/using_shortrates.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
+from short_rate import vasicek_mean, vasicek_var
