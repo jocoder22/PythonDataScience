9cdb3679dcbc8a5ccb100754ec7e7d1c5ce4f96f
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-01 08:12:07 -0400

    Update using_shortrates.py

--- computational_finance/using_shortrates.py
+++ computational_finance/using_shortrates.py
@@ -8,4 +8,30 @@ from scipy.stats import norm
 
 from short_rates import vasicek_mean, vasicek_var
 
+# Analytical bond price (ZCB)
+def a_part(t1,t2):
+  """
+  
+  """
+  numer_ = 1 - np.exp(-alpha * (t2-t1))
+  
+  result = numer_/alpha
+  
+  return result
 
+
+def d_part(t1, t2):
+  """
+  
+  
+  
+  """
+  
+  val1 = b - (sigma**2/(2*alpha**2))
+  val2 = a_part(t1,t2) - (t2 - t1)
+  val3 = sigma**2 * a_part(t1,t2)**2 / (4*alpha)
+  
+  result = val1 * val2 - val3
+  
+  return result
+  
