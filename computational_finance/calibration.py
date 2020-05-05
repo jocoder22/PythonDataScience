6a8360b3ee91d2f0f0016bc6a0242b87481cf8b0
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-05 11:07:01 -0400

    Update calibration.py

--- computational_finance/calibration.py
+++ computational_finance/calibration.py
@@ -1,4 +1,21 @@
 import numpy as np
 import pandas as pd
-from scipy.stat import norm
+from scipy.stats import norm
+import scipy.optimize
+
+
+# Set general parameters
+s0 = 100
+r = 0.1  # from govt bond prices and their yields
+
+# set put parameters
+# sigma and price are unknown
+Kp = 100
+Tp = 2
+
+# set call parameters
+Kc = 95
+Tc = 2
+price = 15
+
 
