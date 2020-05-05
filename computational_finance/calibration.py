982ecb443f209ea9b40804976ac19701a97d7ae1
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-05 12:10:29 -0400

    Update calibration.py

--- computational_finance/calibration.py
+++ computational_finance/calibration.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 import scipy.optimize as opt
-
+from functools import partial
 
 # Set general parameters
 s0 = 100
@@ -40,6 +40,34 @@ sigma_val = opt.broyden1(F, 0.2, args=("call")) # 0.2 is a random initializaton 
 print(sigma_val)
 
 
+def optionprice(sigma, type="call"):
+  if type == "call":
+    return norm.cdf(d1(sigma))*s0 - norm.cdf(d2(sigma))*Kc*np.exp(-r * Tc)
+  else:
+    return -norm.cdf(-d1(sigma))*s0 + norm.cdf(-d2(sigma))*Kc*np.exp(-r * Tc)
+
+
+
+def F(sigma, type="call"): # this gives F(x) = 0
+  return optionprice(sigma, type="call") - price
+
+
+G_partial = partial(F, type="call")
+
+# finding sigma
+sigma_val2 = opt.broyden1(G_partial, 0.2) # 0.2 is a random initializaton value
+
+print(sigma_val2)
 # Find the put price
-def putprice(sigma):
+
+def F3(sigma, typef="call"): # this gives F(x) = 0
+  return optionprice(sigma, type=typef) - price
+
+sigma_val3 = opt.broyden1(F3, xin = 0.1) 
+print(sigma_val3)
+
+
+# calculate the put price
+put_price = optionprice(sigma_val3, type="put")
+print(put_price)
   
