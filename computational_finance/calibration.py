9b120840fb47e173225a0a4ba50dc4323242f552
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-05 11:18:29 -0400

    Update calibration.py

--- computational_finance/calibration.py
+++ computational_finance/calibration.py
@@ -18,4 +18,15 @@ Kc = 95
 Tc = 2
 price = 15
 
+# define functions
+def d1(sigma):
+  return 1/(sigma * np.sqrt(Tc)) * (np.log(s0/Kc) + (r+ sigma**2/2)*Tc)
 
+def d2(sigma):
+  return d1(sigma) - sigma*np.sqrt(Tc)
+
+def callprice(sigma):
+  return norm.cdf(d1(sigma))*s0 - norm.cdf(d2(sigma))*Kc*np.exp(-r * Tc)
+
+def F(sigma):
+  return Callprice(sigma) - price
