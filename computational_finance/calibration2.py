b83130c71d4988ca4234c55d948a5ec6099f6764
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-07 09:12:19 -0400

    Update calibration2.py

--- computational_finance/calibration2.py
+++ computational_finance/calibration2.py
@@ -5,6 +5,7 @@ from scipy.stats import norm
 import scipy.optimize as opt
 
 
+r0 = 0.05
 # create yield curve for integer maturities
 years = np.linspace(1,10,10)
 
@@ -17,4 +18,21 @@ bond_prices = np.exp(-yield_curve*year)
 # plot yield curve
 plt.plot(years, yield_curve*100)
 plt.xlabel("Maturity")
+plt.ylabel("Yield")
+plt.title("Yield Curve")
 plt.show
+
+
+# calibrating Vasicek model
+# Analytical bond price
+def A_part(t1, t2, alpha):
+  return (1- np.exp(-alpha*(t2-t1)))/alpha
+
+def D_part(t1, t2, alpha, b , sigma):
+  val1 = (t2-t1 -A_part(ti, t2, alpha))*(sigma**2/2 / (2*alpha**2)-b)
+  val2 = sigma**2/2*A_part(t1,t2,alpha)**2/(4*alpha)
+  return val1 - val2
+
+def bondprice(r, t, T, alpha,b,sigma):
+  return np.exp(-A_part(t,T,alpha)*r + D_part(t,T,alpha, b,sigma))
+
