468a5b9fb8d342c8528d9e7af22732672d4a519c
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-08 08:20:10 -0400

    Update calibration3.py

--- computational_finance/calibration3.py
+++ computational_finance/calibration3.py
@@ -3,3 +3,18 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 import scipy.optimize as opt
+
+# parametes for option
+r = 0.06
+s0 = 100
+v0 = 0.06
+k = np.array([110,100,90])
+price = [8.02,12.63,18.72]
+T = 1
+k_log = np.log(k)
+k_log.shape = (3,1)
+rho = -0.4
+
+# parameter for Gil-Paelez
+t_max = 30
+N = 100
