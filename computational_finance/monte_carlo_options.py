9ac48e67b07cb1d6691061cf85602fae4cddb73c
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-15 09:59:33 -0400

    Update monte_carlo_options.py

--- computational_finance/monte_carlo_options.py
+++ computational_finance/monte_carlo_options.py
@@ -9,6 +9,12 @@ strikeprice = 100
 annualized_vol = 0.15   # implied annualised volatility
 ndays = 252
 
+# set monte carlo parameters
 np.random.seed(0)
 nsimulations = 500000
+
+# estimate the returns
+returns_ = np.random.randn(nsimulations, ndays)*annualized_vol/np.sqrt(ndays)
+
+
 
