0e0d1dbf0061ab65b6b89e2813b21a512e573d43
Okechukwu Okigbo <okigbookey@gmail.com>
2020-05-15 09:56:40 -0400

    Update monte_carlo_options.py

--- computational_finance/monte_carlo_options.py
+++ computational_finance/monte_carlo_options.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+
+# set stock parameters
+stockprice = 100
+strikeprice = 100
+annualized_vol = 0.15   # implied annualised volatility
+ndays = 252
+
+np.random.seed(0)
+nsimulations = 500000
 
