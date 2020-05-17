caf102e6d938dd4aecc0c5c06775bd0644bd8889
jocoder22 <okigbookey@gmail.com>
2020-05-17 03:29:14 -0400

    Update

--- datascience/neuron_network/simple_nn/simple101.py
+++ datascience/neuron_network/simple_nn/simple101.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Flatten, Dropout
+from keras.callbacks import ModelCheckpoint
+import tensorflow as tf
+
+
 # # from tensorflow.keras.datasets import mnist
 # import tensorflow as tf
 # # # load mnist datasets
@@ -13,22 +22,88 @@ from keras.datasets import mnist
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-fig = plt.figure(figsize=[20,20])
-for i in range(6):
-    ax = fig.add_subplot(1,6, i+1, xticks=[], yticks=[])
-    ax.imshow(X_train[i], cmap='gray')
-    ax.set_title(str(y_train[i]))
-plt.show()
+# fig = plt.figure(figsize=[20,20])
+# for i in range(6):
+#     ax = fig.add_subplot(1,6, i+1, xticks=[], yticks=[])
+#     ax.imshow(X_train[i], cmap='gray')
+#     ax.set_title(str(y_train[i]))
+# plt.show()
 
-# show the first digit
-plt.imshow(X_train[0], cmap='gray')
-plt.xticks([])
-plt.yticks([])
-plt.title(str(y_train[0]))
-plt.show()
+# # show the first digit
+# plt.imshow(X_train[0], cmap='gray')
+# plt.xticks([])
+# plt.yticks([])
+# plt.title(str(y_train[0]))
+# plt.show()
 
 # rescale [0,255] 
 X_train = X_train.astype('float32')/255
-X_train = X_test.astype("float32")/255
+X_test = X_test.astype("float32")/255
+
+# one hot encode the labels
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+
+# build our model
+model = Sequential()
+model.add(Flatten(input_shape=X_train.shape[1:]))
+model.add(Dense(512, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(250, activation="relu"))
+model.add(Dropout(0.1))
+model.add(Dense(10, activation="softmax"))
+
+# summary of our model
+model.summary()
+
+# compile our model
+model.compile(loss="categorical_crossentropy", 
+             optimizer='rmsprop',
+             metrics=['accuracy'])
+
+# evaluate test accuracy
+score = model.evaluate(X_test, y_test, verbose = 0)
+accuracy = score[1] * 100
+
+print(f'Test accuracy : {accuracy:.2f}')
+
+# only comment the lines below after training our model
+# checkpoint = ModelCheckpoint(filepath=filepath, verbose=1,
+#                             save_best_only=True)
+
+# hist = model.fit(X_train, y_train, batch_size=128,
+#                 epochs=10, validation_split=0.2,
+#                 callbacks=[checkpoint],
+#                 verbose=1, shuffle=True)
+
+
+# to load only the weights you must define the model as above
+# model.load_weights('weights\Best.h5')
+# model.fit with callbacks save only the weights
+
+# save model
+# modelname = 'BestModel.h5'
+# filepath2 = os.path.join(os.getcwd(), modelname)
+# model.save(filepath2)
+
+# model = load_model("BestModel.h5")
+model.load_weights(r'weights\Best.h5')
+# plt.figure(figsize=[10,8])
+# plt.plot(hist.history['loss'], label="Train Loss")
+# plt.plot(hist.history['val_loss'], label="Validation Loss")
+# plt.legend()
+# plt.show()
+
+pred = model.predict(X_test)
+print(type(pred), y_test.shape, **sp)
+
+print(pred.shape)
+
+# # evaluate test accuracy
+score = model.evaluate(X_test, y_test, verbose = 0)
+accuracy = score[1] * 100
+
+print(f'Test accuracy : {accuracy:.2f}')
+
 
 
