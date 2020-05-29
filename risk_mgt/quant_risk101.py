04f5a81dcd8277e46add7a9d1534dea6603b7555
jocoder22 <okigbookey@gmail.com>
2020-05-28 21:55:28 -0400

    Update

--- risk_mgt/quant_risk101.py
+++ risk_mgt/quant_risk101.py
@@ -5,6 +5,9 @@ import datetime
 import pandas_datareader as pdr
 from pandas.util.testing import assert_frame_equal
 
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
 
 stocklist = ["C","JPM","MS", "GS"]
 
@@ -12,10 +15,24 @@ starttime = datetime.datetime(2000, 1, 1)
 portfolio = pdr.get_data_yahoo(stocklist, starttime)
 
 # multiindex dataframe
-print(portfolio.head(), portfolio.columns, portfolio.info())
+print2(portfolio.head(), portfolio.columns, portfolio.info())
 
 # get only the closing prices
 # portfolio_close = pdr.get_data_yahoo(stocklist, starttime)['Close']
 portfolio_close = portfolio['Close']
-
+p_labels = ["Citibank", "J.P. Morgan", "Morgan Stanley", "Goldman Sachs"]
+portfolio_close.columns = p_labels
 print2(portfolio_close.head())
+
+
+# Plot portfolio's asset prices
+portfolio_close.plot().set_ylabel("Closing Prices, USD")
+plt.show()
+
+
+# Select portfolio asset prices for the middle of the crisis, 2008-2009
+asset_prices = portfolio_close.loc['2008':'2009']
+
+# Plot portfolio's asset prices during this time
+asset_prices.plot().set_ylabel("Closing Prices, USD")
+plt.show()
