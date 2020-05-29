8f50ba289ad93997652f6cdf6436ac5196d30818
jocoder22 <okigbookey@gmail.com>
2020-05-28 22:06:12 -0400

    Update

--- risk_mgt/quant_risk101.py
+++ risk_mgt/quant_risk101.py
@@ -29,9 +29,19 @@ portfolio_close.plot().set_ylabel("Closing Prices, USD")
 plt.show()
 
 
+# set the weights
+weights = [0.20, 0.30, 0.30, 0.20]
 # Select portfolio asset prices for the middle of the crisis, 2008-2009
 asset_prices = portfolio_close.loc['2008':'2009']
 
 # Plot portfolio's asset prices during this time
 asset_prices.plot().set_ylabel("Closing Prices, USD")
+plt.show()
+
+# calculate percentage return and portfolio return
+asset_returns = portfolio_close.pct_change()
+portfolio_returns = asset_returns.dot(weights)
+
+# Plot portfolio returns
+portfolio_returns.plot().set_ylabel("Daily Return, %")
 plt.show()
