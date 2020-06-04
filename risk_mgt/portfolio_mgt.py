9a4334127ff6d34a6700cb893540404b77f08089
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-04 09:10:57 -0400

    Update portfolio_mgt.py

--- risk_mgt/portfolio_mgt.py
+++ risk_mgt/portfolio_mgt.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import pandas_datareader as pdr
+import statsmodels.api as sm
+from pandas.util.testing import assert_frame_equal
 
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
+
+stocklist = ["C","JPM","MS", "GS"]
+p_labels = ["Citibank", "J.P. Morgan", "Morgan Stanley", "Goldman Sachs"]
+
+starttime = datetime.datetime(2000, 1, 1)
+endtime = datetime.datetime(2019, 10, 1)
+
+# get only the closing prices
+portfolio = pdr.get_data_yahoo(stocklist, starttime, endtime)['Close']
