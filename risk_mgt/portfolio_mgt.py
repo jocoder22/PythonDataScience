fc5abb6afe90ef7b753a4dbd52f3d235ed920b17
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-04 10:01:12 -0400

    Update portfolio_mgt.py

--- risk_mgt/portfolio_mgt.py
+++ risk_mgt/portfolio_mgt.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import pandas_datareader as pdr
-import statsmodels.api as sm
 from pandas.util.testing import assert_frame_equal
 
 def print2(*args):
