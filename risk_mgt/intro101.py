16d949e51dc745b0e29e5b1343489e64277e94df
jocoder22 <okigbookey@gmail.com>
2020-06-06 05:45:15 -0400

    Update

--- risk_mgt/intro101.py
+++ risk_mgt/intro101.py
@@ -4,3 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import pandas_datareader as pdr
+
+def print2(*args):
+    for arg in args:
+        print(arg, end="\n\n")
+
+
+stocklist = ["JPM", "GS", "BAC", "MS", "C","CS"]             
+             
+pp_labels = ["JPMorgan Chase", "Goldman Sachs", "BofA Securities", "Morgan Stanley", "Citigroup", "Credit Suisse"] 
