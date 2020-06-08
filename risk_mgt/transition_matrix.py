e0c269838c9f0d5d3096bd6ee0287686c40ac948
jocoder22 <okigbookey@gmail.com>
2020-06-08 02:47:48 -0400

    Update

---
+++ risk_mgt/transition_matrix.py
@@ -0,0 +1,15 @@
+# My first stock is Neflix
+# I'm using daily close prices
+stock_1 = "NFLX"
+
+# using 2 years of data from January 01, 2018 to December 31, 2019
+starttime = datetime.datetime(2018, 1, 1)
+endtime = datetime.datetime(2019, 12, 31)
+
+# get only the closing prices
+neflix = pdr.get_data_yahoo(stock_1, starttime, endtime)['Close']
+
+
+# Calculate log return
+logReturn_neflix = np.log(neflix).diff().dropna()
+logReturn_neflix.head()
