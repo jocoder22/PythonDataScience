54824cf431d00ccad645a0d6cb861521f2059869
jocoder22 <okigbookey@gmail.com>
2020-06-08 02:49:20 -0400

    Update

--- risk_mgt/transition_matrix.py
+++ risk_mgt/transition_matrix.py
@@ -24,3 +24,43 @@ neflix = pdr.get_data_yahoo(stock_1, starttime, endtime)['Close']
 # Calculate log return
 logReturn_neflix = np.log(neflix).diff().dropna()
 logReturn_neflix.head()
+
+# Calulate their correlation
+# Create combine data using pandas dataframe
+data = pd.DataFrame({"Neflix" : logReturn_neflix, "Tesla" :logReturn_tesla})
+data.corr()
+
+# define a function to show stocks movements
+def stock_movt(data_input):
+    """The stock_movt function will add a column to the dataset
+        showing the movement of the stocks either 
+        uu = both stocks moved upwards,
+        ud = the first moved upwards and the second moved downwards,
+        ud = the first moved downwards and the second moved upwards, or 
+        dd = both stocks moved downwards
+
+    Args: 
+        data_input (DataFrame): the DataFrame with the stocks log return
+        
+    Returns: 
+        DataFrame: The DataFrame with the movement column
+
+    """
+    
+    # Make a copy of the input dataset
+    data_out = data_input.copy()
+
+    # Make categorise, u for up and d for down movements for each stock
+    data_out['_movt0'] = np.where(data_input.iloc[:,0] > 0,"u","d")
+    data_out['_movt1'] = np.where(data_input.iloc[:,1] > 0,"u","d")
+    
+    # Create direction to represent the combined movements of the stocks
+    data_out['direction'] = data_out['_movt0'] .str.cat(data_out['_movt1'])
+    
+    # Remove the columns for each stock movement
+    data_out.drop(columns=['_movt0', '_movt1'], inplace=True)
+    
+    
+    # return the final dataset
+    return data_out
+    
