e108b05f4e091247d29e6da8fc64c97792648c41
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-12 16:51:58 -0400

    Update Multi_Period_GGM.py

--- risk_mgt/Multi_Period_GGM.py
+++ risk_mgt/Multi_Period_GGM.py
@@ -6,6 +6,7 @@ from decimal import *
 Year = ["2008", "2009", "2010", "2011", "2012", "2013"]
 FCFE_growth = [0.18, 0.18, 0.16, 0.12, 0.11, 0.06]
 equity_discount_rate = 0.125
+g = 0.06
 
 
 def ggm(dividend, dividend_growth_rate, equity_discount_r):
@@ -94,40 +95,43 @@ def multi_ggm(fcfe_list, dividend, g, r):
   return round(value, 3)
 
 
+
+
 def multi_ggm2(fcfe_list_, dividend_, g_, r_):
-  """
-  
-  
-  
-  """
-  from decimal import Decimal
-  
-  fcfe_list = list(map(Decimal, fcfe_list))
-  dividend = Decimal(dividend_)
-  g = Decimal(g_)
-  r = Decimal(r_)
-  n = Decimal(len(fcfe_list))
-  
-  fcfe_list = 
-  FCFE = []
-  FCFE.append(dividend * (1+fcfe_list[0]))
-
-  for i in range(n-1):
-    FCFE.append(FCFE[i] * (1+ fcfe_list[i+1]))
-
-  # data = list(round(map(Decimal,FCFE),2))
-  data= [round(Decimal(v), 2) for v in FCFE]
-
-  pv_fcfe = [round(Decimal(presentvalue(v, r, 1+i)),2) for i,v in enumerate(FCFE)]
-  
-  
-  comp1 = sum(pv_fcfe)
-  
-  # comp2_ = Decimal(comp2(FCFE[-1],g ,r, n))
-  comp2_ = Decimal(comp2(data[-1],g ,r, n))
-  value = comp1 + comp2_
-  # value =  comp2_
-  return round(value, 2), pv_fcfe,FCFE, data[-1]
+    """
 
 
-print(multi_ggm(FCFE_growth,fcfe2007,g, equity_discount_rate))
+
+    """
+    from decimal import Decimal
+
+    fcfe_list = list(map(Decimal, fcfe_list_)) 
+    dividend = Decimal(dividend_)
+    g = Decimal(g_)
+    r = Decimal(r_)
+    n = Decimal(len(fcfe_list))
+    nn = len(fcfe_list)
+    one = Decimal(1)
+
+
+
+    FCFE = []
+    FCFE.append(dividend * (one+fcfe_list[0]))
+
+    for i in range(nn-1):
+        FCFE.append(FCFE[i] * (one+ fcfe_list[i+1]))
+
+    data= [round(Decimal(v), 2) for v in FCFE]
+
+    pv_fcfe = [round(Decimal(presentvalue(v, r, 1+i)),2) for i,v in enumerate(FCFE)]
+
+
+    comp1 = sum(pv_fcfe)
+
+    comp2_ = Decimal(comp2(data[-1],g ,r, n))
+    value = comp1 + comp2_
+
+    return round(value, 2)
+
+
+print(multi_ggm2(FCFE_growth,fcfe2007,g, equity_discount_rate))
