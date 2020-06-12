46ff3f782bc9c118a44d155493bd79fa5ab6ab47
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-12 15:00:03 -0400

    Update Multi_Period_GGM.py

--- risk_mgt/Multi_Period_GGM.py
+++ risk_mgt/Multi_Period_GGM.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 Year = ["2008", "2009", "2010", "2011", "2012", "2013"]
 FCFE_growth = [0.18, 0.18, 0.16, 0.12, 0.11, 0.06]
+equity_discount_rate = 0.125
 
 def presentvalue(amt, rate, time):
   """
@@ -20,9 +21,14 @@ def presentvalue(amt, rate, time):
 fcfe2007 = 2.0
 
 fcfe = []
-fcfe[0] = fcfe2007 * (1+FCFE_growth[0])
-for i in range(1,len(FCFE_growth)):
-  fcfe[i] = fcfe[i-1] * FCFE_growth[i-1]
+fcfe.append(fcfe2007 * (1+FCFE_growth[0]))
+
+for i in range(len(FCFE_growth)-1):
+  fcfe.append(round((fcfe[i] * (1+ FCFE_growth[i+1])),3))
+  
+pv_fcfe = [round(presentvalue(v, equity_discount_rate, 1+i),3) for i,v in enumerate(fcfe)]
   
 print(fcfe)
+print(pv_fcfe)
+
 
