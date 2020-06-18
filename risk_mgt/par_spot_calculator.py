1b485927155d84acf973899aec0f1f02b06e33c8
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-18 08:23:15 -0400

    Update par_spot_calculator.py

--- risk_mgt/par_spot_calculator.py
+++ risk_mgt/par_spot_calculator.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python
 
 def print2(*args):
   for arg in args:
     print(arg, end="\n\n")
 
 def spot_from_par(parList, year):
-  price, n = 100, year
-  total = 0
+  price, n , total = 100, year, 0
+
   for ind, val in enumerate(parList, start=1):
     total += (parList[n-1]*price)/pow((1 + val),ind)
     if ind == n-1:
       rate = pow((parList[n-1]*price + price)/(price - total), 1/n) - 1
-      # print(round(rate*100,2), price - total)
       return round(rate*100, 2)
 
     
