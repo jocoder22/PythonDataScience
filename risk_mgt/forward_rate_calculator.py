bf2fa03033f46764ed36e73c241c8cd97e6b6d19
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-19 08:28:50 -0400

    Update forward_rate_calculator.py

--- risk_mgt/forward_rate_calculator.py
+++ risk_mgt/forward_rate_calculator.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
-import pandas as pd
+
+nlist = [0.06,0.07, 0.08, 0.09, 0.10 ]
+def forward_rate(spotList, tstar, t):
+  nume = pow(1 + spotList[tstar -1], tstar)
+  denom = pow(1 + spotList[t], t+1)
+  result =  np.sqrt(denom / nume) - 1
+  return round(result * 100, 2) 
+
+print(forward_rate(nlist,1,2))
 
