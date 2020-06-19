46760b2de348e3fbc530598c8230c0986c5c546b
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-19 08:42:36 -0400

    Update forward_rate_calculator.py

--- risk_mgt/forward_rate_calculator.py
+++ risk_mgt/forward_rate_calculator.py
@@ -8,18 +8,17 @@ def forward_rate(spotList, tstar, t):
       
       Inputs:
           spotList (list) : yearly spot rates
-          tstar (int): time to start calculating forward rate
-          t (int): the future time for the forward rate
+          tstar (int): time until initiation of the rate
+          t (int): the tiemto maturity from tstar
           
        Output:
             result (float): forward rate
   
-  
   """
   
   nume = pow(1 + spotList[tstar -1], tstar)
-  denom = pow(1 + spotList[t], t+1)
-  result =  np.sqrt(denom / nume) - 1
+  denom = pow(1 + spotList[t+tstar - 1], t+tstar)
+  result =  pow((denom / nume), 1/tstar) - 1
   return round(result * 100, 2) 
 
 print(forward_rate(nlist,1,2))
