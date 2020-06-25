1c9d7c28195a92fd61f304d74a2cfb5b12928ba0
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-25 08:04:59 -0400

    Update indexation.py

--- risk_mgt/indexation.py
+++ risk_mgt/indexation.py
@@ -4,7 +4,20 @@ import pandas as pd
 PriceReturn = [5.2, 4.1, -2.6]
 IncomeReturn = [1.5, 1.5, 1.5]
 
-data = pd.DataFrame({"PriceIncome":PriceReturn, "IncomeReturn":IncomeReturn})
+
+def print2(*args):
+  for arg in args:
+    print(arg, end="\n\n")
+
+PriceReturn = [5.2, 4.1, -2.6]
+IncomeReturn = [1.5, 1.5, 1.5]
+
+data = pd.DataFrame({"PriceIncome":PriceReturn, "IncomeReturn":IncomeReturn})*0.01
 
 data["TotalReturn"] = data.sum(axis = 1)
+
+v0 = 100
+ivalue = v0 * (1 + data["PriceIncome"]).cumprod()
+
+print2(data, round(ivalue.iloc[-1], 4))
 
