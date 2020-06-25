978f01416d4eb610eb89314e75766404fce9958e
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-25 10:40:22 -0400

    Update indexation.py

--- risk_mgt/indexation.py
+++ risk_mgt/indexation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tabulate
 
 PriceReturn = [5.2, 4.1, -2.6]
 IncomeReturn = [1.5, 1.5, 1.5]
@@ -24,8 +25,11 @@ print2(data, round(ivalue.iloc[-1], 4), round(itotal, 4))
 
 
 indexes = pd.DataFrame(index="A B C D E".split())
-indexes["PricePerShare_0"], indexes["Return"] = [60, 30, 15, 12, 4.80], [0.095, -0.1140, -0.342, 0.38, 0.475]
-indexes["PricePerShare_1"],indexes["DividendPerShare"] = [65.7, 26.58, 9.87, 16.56, 7.08], [1.05, 0.14, 0.00, 0.07, 0.00]
+indexes["PricePerShare_0"], indexes["Return"] = [60, 30, 15, 12, 4.80], [0.095, -0.1140, -0.342, 0.38, 0.475], 
+indexes["PricePerShare_1"] = indexes["PricePerShare_0"] *  (1 + indexes["Return"])
+indexes["DividendPerShare"] = [1.05, 0.14, 0.00, 0.07, 0.00]
 indexes["SharesOutstanding"] = [3300, 11000, 5500, 8800, 7700]
 indexes["FloatAdjFactor"] = [1.0, 0.7, 0.9, 0.25, 0.8]
 indexes["MarketFloat"] = indexes["FloatAdjFactor"].mul(indexes["SharesOutstanding"])
+
+print(tabulate.tabulate(indexes, headers=indexes.columns, tablefmt="fancy_grid"))
