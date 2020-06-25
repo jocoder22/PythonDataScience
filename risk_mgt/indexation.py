2bc4e56fc7ab39f0e58e47be18f8df4cbfeabd49
Okechukwu Okigbo <okigbookey@gmail.com>
2020-06-25 10:58:26 -0400

    Update indexation.py

--- risk_mgt/indexation.py
+++ risk_mgt/indexation.py
@@ -25,6 +25,8 @@ print2(data, round(ivalue.iloc[-1], 4), round(itotal, 4))
 
 
 indexes = pd.DataFrame(index="A B C D E".split())
+indexes.index.name = "Security"
+
 indexes["PricePerShare_0"], indexes["Return"] = [60, 30, 15, 12, 4.80], [0.095, -0.1140, -0.342, 0.38, 0.475], 
 indexes["PricePerShare_1"] = indexes["PricePerShare_0"] *  (1 + indexes["Return"])
 indexes["DividendPerShare"] = [1.05, 0.14, 0.00, 0.07, 0.00]
@@ -32,4 +34,11 @@ indexes["SharesOutstanding"] = [3300, 11000, 5500, 8800, 7700]
 indexes["FloatAdjFactor"] = [1.0, 0.7, 0.9, 0.25, 0.8]
 indexes["MarketFloat"] = indexes["FloatAdjFactor"].mul(indexes["SharesOutstanding"])
 
-print(tabulate.tabulate(indexes, headers=indexes.columns, tablefmt="fancy_grid"))
+
+indexes.reset_index(inplace=True)
+print(tabulate.tabulate(indexes, headers=indexes.columns, tablefmt="fancy_grid", showindex="never"))
+
+
+
+## Price Weighted index
+pwi = pd.DataFrame(index=indexes.Security)
