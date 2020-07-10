e3519b5ef2df6b36a6fde8e3152917676e87d8d2
jocoder22 <okigbookey@gmail.com>
2020-07-10 03:53:27 -0400

    Update

--- risk_mgt/group_assignment3.py
+++ risk_mgt/group_assignment3.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 from printdescribe import print2, describe2, changepath
 
@@ -24,3 +26,16 @@ print2(df_return)
 # df_activeReturn = df_return.sub(df_return.iloc[:,-1], axis=0).drop(['SP_500'], axis=1)
 df_activeReturn = df_return.sub(df_return['S&P 500'], axis=0).drop(['S&P 500'], axis=1)
 print2(df_activeReturn)
+
+
+
+for col in df_return.columns[:-1]:
+    plt.figure(figsize=[10, 8])
+    plt.plot(df_return[[col, "S&P 500"]])
+    plt.legend([col, "S&P500"])
+    plt.show()
+
+plt.figure(figsize=[10, 8])
+plt.plot(df_return)
+plt.legend(df_return.columns.tolist())
+plt.show()
