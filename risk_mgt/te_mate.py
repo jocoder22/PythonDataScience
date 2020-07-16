84da5e89ee6587271e87946fd4e16f5d3e1afb57
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-16 10:04:20 -0400

    Update te_mate.py

--- risk_mgt/te_mate.py
+++ risk_mgt/te_mate.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import datetime
-import pandas_datareader as pdr
+from printdescribe import print2
 
 
 bmask1 = [100, 90, 104, 124, 161, 186, 204, 235, 258, 271, 339, 254, 216, 216, 238, 262, 275]
@@ -13,9 +10,8 @@ fund = [100,  91, 104, 127, 167, 190, 206, 234, 260, 271, 346, 256, 221, 223, 24
 
 data = pd.DataFrame({"bmask1":bmask1, "bmask2":bmask2, "fund":fund})
 data2 = data.pct_change().dropna()
-returns = pd.DataFrame()
-returns["Ret1"] = data2.fund - data2.bmask1
-returns["Ret2"] = data2.fund - data2.bmask2
-mate = np.sqrt(pow(returns, 2).sum()/returns.shape[0])
-print(data, data2, returns, round(mate*100, 2), sep="\n\n")
+data2["Ret1"] = data2.fund - data2.bmask1
+data2["Ret2"] = data2.fund - data2.bmask2
+mate = np.sqrt(pow(returns, 2).sum()/data2.shape[0])
+print2(data, data2, returns, round(mate*100, 2), sep="\n\n")
 
