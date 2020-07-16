c1436352315586167c301d1e6d53926e22563aea
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-16 11:14:56 -0400

    Update cum_returns.py

--- risk_mgt/cum_returns.py
+++ risk_mgt/cum_returns.py
@@ -1,1 +1,22 @@
+# import required modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import pandas_datareader as pdr
+from printdescribe import print2, describe2
 
+# initialize stock ticker
+stock = "JPM"                      
+
+# initialize the duration
+starttime = datetime.datetime(2000, 1, 1)
+endtime = datetime.datetime(2019, 10, 1)
+
+# # get only the closing prices
+pp = pdr.get_data_yahoo(stock, starttime, endtime)['Close']
+pp.columns = ["price"]
+
+# compute simple and log returns
+pp["simple"] = pp.pct_change()*100
+pp["log"] = np.log(pp["price"]).diff()*100
