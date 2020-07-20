e8cad1b8710495574d04f5f9bd5521892cfdeec5
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-20 16:25:25 -0400

    Update worker.py

--- OOP/worker.py
+++ OOP/worker.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python
+import pandas as pd
 
+class Worker:
+  
+  def __init__(self, name, wage = 15.53, hour = 8):
+    self.name = name
+    self.wage = wage
+    self.hour = hour
+    self.salary = Worker.salary()
+
+  def salary(self):
+    amount = self.wage * self.hour
+    return amount
+  
+  
+class Staff(Worker):
+  
+  def __init__(self, name, wage = 22.29, hour = 7.5, bonus = 1.05):
+    Worker.__init__(self, name, wage, hour)
+    self.bonus = bonus
+    
+    
+   def get_reward(self, bonus):
+      self.salary +* bonus
+    
