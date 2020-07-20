34f4926a16d348fd3aa1d5d11579b0b2d77f7fb6
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-20 16:44:51 -0400

    Update worker.py

--- OOP/worker.py
+++ OOP/worker.py
@@ -7,23 +7,26 @@ class Worker:
     self.name = name
     self.wage = wage
     self.hour = hour
-    self.salary = Worker.salary()
+#     self.salary = Worker.salary()
+    self.salary = self.wage * self.hour
 
-  def salary(self):
-    amount = self.wage * self.hour
-    return amount
+  def newsalary(self, bonus = 1):
+    amount = self.wage * self.hour  * bonus
+    self.salary = amount
   
   
 class Staff(Worker):
   
-  def __init__(self, name, wage = 22.29, hour = 7.5, reward = 0.05):
+  def __init__(self, name, wage = 22.29, hour = 7.5, compensation = 0.15):
     Worker.__init__(self, name, wage, hour)
-    self.bonus = bonus
+    self.compensation = compensation
     
     
    def give_reward(self):
-      self.salary +* self.bonus
-      Worker.salary()
+      Worker.newsalary(bonus = self.compensation)
+      
+   def give_raise(self):
+      Worker.newsalary(bonus = 1.057)
       
      
     
