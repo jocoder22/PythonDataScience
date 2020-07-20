7f5811970dacafca202cc4ab23aea97ee9a32cf5
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-20 16:55:25 -0400

    Update worker.py

--- OOP/worker.py
+++ OOP/worker.py
@@ -10,7 +10,7 @@ class Worker:
 #     self.salary = Worker.salary()
     self.salary = self.wage * self.hour
 
-  def newsalary(self, bonus = 1):
+  def _salary(self, bonus = 1):
     amount = self.wage * self.hour  * bonus
     self.salary = amount
   
@@ -22,11 +22,40 @@ class Staff(Worker):
     self.compensation = compensation
     
     
-   def give_reward(self):
-      Worker.newsalary(bonus = self.compensation)
+   def _salary(self, bonus):
+      totalbenefits = bonus * self.compensation
+      Worker._salary(bonus = totalbenefits)
       
-   def give_raise(self):
-      Worker.newsalary(bonus = 1.057)
+
       
+      
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+        
+class Manager(Employee):
+    def display(self):
+        print("Manager ", self.name)
+
+    def __init__(self, name, salary=50000, project=None):
+        Employee.__init__(self, name, salary)
+        self.project = project
+
+    # Add a give_raise method
+    def give_raise(self, amount, bonus=1.05):
+        new_amount = amount * bonus
+        Employee.give_raise(self, new_amount)
+    
+    
+mngr = Manager("Ashta Dunbar", 78500)
+mngr.give_raise(1000)
+print(mngr.salary)
+mngr.give_raise(2000, bonus=1.03)
+print(mngr.salary)
      
     
