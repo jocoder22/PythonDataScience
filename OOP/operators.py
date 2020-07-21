784e66e5bc6ed4fe10caa12fe54c0f98a76126da
jocoder22 <okigbookey@gmail.com>
2020-07-20 22:36:09 -0400

    Update

--- OOP/operators.py
+++ OOP/operators.py
@@ -9,23 +9,31 @@ class Patients:
         self.gender = gender
 
     def __eq__(self, other):
+        # return self.name == other.name and self.id == other.id\
+        #     and  type(self) == type(other)
         return self.name == other.name and self.id == other.id\
-            and  type(self) == type(other)
+            and isinstance(other, Patients)
+
 
 class Staff:
 
-    
     def __init__(self, name, id, gender):
         self.name = name
         self.id = id
         self.gender = gender
 
+    def __eq__(self, other):
+        return self.name == other.name and self.id == other.id\
+            and isinstance(other, Staff)
+
 
 patient1 = Patients("Charles", 459234, "Male")
-patient2 = Patients("Charles", 4593234, "Male")
+patient2 = Patients("Charles", 876323, "Male")
 patient3 = Patients("Marylene", 459234, "Female")
 patient4 = Patients("Charles", 459234, "Male")
-patient4 = Staff("Charles", 459234, "Male")
+patient5 = Staff("Charles", 459234, "Male")
 
 print2(patient1 == patient2, patient3 == patient1, 
-    patient1 == patient4, patient4 == patient1)
+    patient1 == patient4)
+print2("$"*20)
+print2(patient1 == patient5, patient5 == patient1)
