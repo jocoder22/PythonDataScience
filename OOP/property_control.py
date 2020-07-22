60dd2c92630809e7175e003b0df84a48076c5fc8
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-22 13:17:27 -0400

    Update property_control.py

--- OOP/property_control.py
+++ OOP/property_control.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 from printdescribe import print2
+
+class Worker:
+    
+    def __init__(self, name, newsalary):
+        self.name = name
+        self._salary = newsalary
+
+    
+    def _salary(self, bonus = 1):
+        amount = self.wage * self.hour  * bonus
+        # self.salary = amount
+        return amount
