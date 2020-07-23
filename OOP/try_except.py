655b354d13ad5b67784367e763a48fa25c7172d8
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-23 09:06:40 -0400

    Update try_except.py

--- OOP/try_except.py
+++ OOP/try_except.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from printdescribe import 
+from printdescribe import print2
 
 class SalaryError(ValueError):
   pass
@@ -11,5 +11,25 @@ class HourError(ValueError):
   def __init__(self):
     ValueError.__init__(self)
     print(HourError._message)
+    
+    
+    
+    
+class Worker:
+  
+  def __init__(self, name, wage = 15.53, hour = 8):
+        self.name = name
+        self.wage = wage
+        
+        if hour > 60:
+          raise HourError
+        self.hour = hour
+        
+        self.salary = Worker._salary()
+    
+    def _salary(self, bonus = 1):
+        amount = self.wage * self.hour  * bonus
+        # self.salary = amount
+        return amount
 
 
