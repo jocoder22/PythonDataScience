704721e052d0c1f078e7fc96cdffdfdf4fb23c8d
Okechukwu Okigbo <okigbookey@gmail.com>
2020-07-23 09:00:49 -0400

    Update try_except.py

--- OOP/try_except.py
+++ OOP/try_except.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 from printdescribe import 
 
+class SalaryError(ValueError):
+  pass
+
+class HourError(ValueError):
+  
+  _message = "Hours out of range!"
+  
+  def __init__(self):
+    ValueError.__init__(self)
+    print(HourError._message)
+
 
