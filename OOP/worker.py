e41510b462ab36d44bda7135090db14b0577f82c
jocoder22 <okigbookey@gmail.com>
2020-07-24 20:30:31 -0400

    Update

--- OOP/worker.py
+++ OOP/worker.py
@@ -43,14 +43,27 @@ class Staff(Worker):
         amt = Worker._salary(self, bonus = totalbenefits)
         return amt
         
-    
-    def __str__(self):
-        
-        tt = Worker.__str__(self)
 
-        return tt + f"Total Compensation: {self.compensation}"
+class SeniorStaff(Staff):
+    
+    def __init__(self, name, wage = 22.29, hour = 7.5, compensation = 0.35):
+        Staff.__init__(self, name, wage, hour)
+        self.compensation = compensation
+        self.salary = Staff._salary(self)
+    
+    
+    # def _salary(self, bonus=1):
+    #     totalbenefits = bonus + self.compensation
+    #     amt = Worker._salary(self, bonus = totalbenefits)
+    #     return amt
+
+    # def __str__(self):
         
-        #  """
+    #     tt = Worker.__str__(self)
+
+    #     return tt + f"Total Compensation: {self.compensation}"
+        
+    #     #  """
      
 class Employee:
     def __init__(self, name, salary=30000):
@@ -75,20 +88,18 @@ class Manager(Employee):
         Employee.give_raise(self, new_amount)
 
     
-    
-
-    # def __repr__(self):
-    #     return f"Worker('{self.name}', {self.salary}, '{self.project}')"
+    def __repr__(self):
+        return f"Manager('{self.name}', {self.salary}, '{self.project}')"
 
 
-    # def __str__(self):
-    #     strr = f"""
-    #     Worker:
-    #         Name: {self.name}
-    #         Total salary: ${self.salary}
-    #         Projet: {self.project}
-    #     """
-    #     return strr
+    def __str__(self):
+        strr = f"""
+        Manager:
+            Name: {self.name}
+            Total salary: ${self.salary}
+            Projet: {self.project}
+        """
+        return strr
     
     
 mngr = Manager("Ashta Dunbar", 78500)
@@ -101,5 +112,6 @@ print(mngr.salary)
 worker1 = Worker("Amy")
 staff1 = Staff("Ashley")
 staff2 = Staff("Bruny", wage=20.0)
-print2(staff1.salary, worker1.salary, staff2.salary, repr(staff2), staff2)
+staff2_s = SeniorStaff("Senior Bruny", wage=20.0)
+print2(staff1.salary, worker1.salary, staff2.salary, repr(staff2), staff2, mngr, staff2_s)
     
