5b49546a5b08bed62addb85a430269c41c0fcd4a
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-03 13:06:14 -0400

    Update newfile.py

--- OOP/newfile.py
+++ OOP/newfile.py
@@ -5,3 +5,11 @@ b = np.array([[9],[17],[5]])
 m. p = np.linalg.inv(A.T @ A) @ A.T @ b
 
 
+X = np.array([2,4,1])
+y = np.array([9,17,5])
+m = ((np.mean(X) * np.mean(y) - np.mean(X*y)) / ((np.mean(X)**2) - np.mean(X**2)))
+b = np.mean(y) - m * np.mean(X)
+
+print(m,b)
+
+
