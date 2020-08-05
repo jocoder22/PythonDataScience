7ee0723e4215e7d1a23291b886e6d7891dc06f44
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-05 10:23:21 -0400

    Update pca101.py

--- PCA/pca101.py
+++ PCA/pca101.py
@@ -2,14 +2,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import requests
+# import requests
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 import seaborn as sns
+from printdescribe import print2
 
-sp = '\n\n'
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data'
 
 
@@ -20,7 +20,7 @@ dataset = pd.read_csv(url, names=colname, na_values=['na','Na', '?'],
                 skipinitialspace=True, comment='\t', sep=" ", quotechar='"')
 
 dataset.drop(columns='Car_name', inplace=True)
-print(dataset.isna().sum())
+print2(dataset.isna().sum())
 dataset.dropna(inplace=True) 
 
 
@@ -39,8 +39,8 @@ sns.pairplot(pcomp_df)
 plt.show()
 
 # Inspect the explained variance ratio per component
-print(pcompf.explained_variance_ratio_)
-print(pca.explained_variance_ratio_.cumsum())
+print2(pcompf.explained_variance_ratio_)
+print2(pca.explained_variance_ratio_.cumsum())
 
 
 
@@ -54,8 +54,8 @@ vectors = pipe1.steps[1][1].components_.round(3)
 
 
 # Print feature effects
-print('PC 1 effects = ' + str(dict(zip(colname[1:8], vectors[0]))))
-print('PC 2 effects = ' + str(dict(zip(colname[1:8], vectors[1]))))
-print('PC 3 effects = ' + str(dict(zip(colname[1:8], vectors[2]))))
-print('PC 4 effects = ' + str(dict(zip(colname[1:8], vectors[3]))))
-print('PC 5 effects = ' + str(dict(zip(colname[1:8], vectors[4]))))
+print2('PC 1 effects = ' + str(dict(zip(colname[1:8], vectors[0]))))
+print2('PC 2 effects = ' + str(dict(zip(colname[1:8], vectors[1]))))
+print2('PC 3 effects = ' + str(dict(zip(colname[1:8], vectors[2]))))
+print2('PC 4 effects = ' + str(dict(zip(colname[1:8], vectors[3]))))
+print2('PC 5 effects = ' + str(dict(zip(colname[1:8], vectors[4]))))
