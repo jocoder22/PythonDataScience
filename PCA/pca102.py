b65f9ee7c8746a3b7af1283a64bfd2dde5a5f38c
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-05 12:10:52 -0400

    Update pca102.py

--- PCA/pca102.py
+++ PCA/pca102.py
@@ -34,3 +34,27 @@ scaled_data[:20]
 scaler =  StandardScaler()
 dataset = scaler.fit_transform(data.T)
 print(dataset[:20])
+
+pca = PCA()
+pca.fit(scaled_data)
+pca_data = pca.transform(scaled_data)
+
+per_var = np.round(pca.explained_variance_ratio_*100, 1)
+labels = ["PC"+str(i) for i in range(1,len(per_var)+1)]
+plt.bar(labels,per_var)
+plt.ylabel("Percentage of Explained Variance")
+plt.xlabel("Pricipal Component")
+plt.title("Scree Plot")
+plt.show()
+
+pca_df = pd.DataFrame(pca_data, index=[*wt, *ko], columns=labels)
+
+plt.scatter(pca_df.PC1, pca_df.PC2)
+
+for sample in pca_df.index:
+    plt.annotate(sample, (pca_df.PC1.loc[sample], pca_df.PC2.loc[sample]))
+    
+plt.ylabel(f"PC2 - {per_var[1]}")
+plt.xlabel(f"PC1 - {per_var[0]}")
+plt.title("PCA Plot")
+plt.show()
