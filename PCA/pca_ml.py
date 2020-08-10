b169d169b9ff2a1dc6902bc7537151e852e70b52
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-10 09:43:11 -0400

    Update pca_ml.py

--- PCA/pca_ml.py
+++ PCA/pca_ml.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+# Import different modules for using with the notebook
+from IPython.display import display
+from IPython.display import Image
+from IPython.display import HTML
+
+from printdescribe import print2
+
 
