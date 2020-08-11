0bc06a4d16b9c372a66f74bb50d20501e1f224fd
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-11 11:13:15 -0400

    Update lda101.py

--- PCA/lda101.py
+++ PCA/lda101.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
+# Import different modules for using with the notebook
+from IPython.display import display
+from IPython.display import Image
+from IPython.display import HTML
+
+from printdescribe import 
