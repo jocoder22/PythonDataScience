7fb3144c3adbe07abb08d813d8ff387bd2281530
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-12 07:24:54 -0400

    Update lda102.py

--- PCA/lda102.py
+++ PCA/lda102.py
@@ -28,9 +28,9 @@ FIGSIZE = (12, 8)
 
 features, targets = load_wine(return_X_y=True)
 
-# create train/test split using 20% test size
+# create train/test split using 25% test size
 X_train, X_test, y_train, y_test = train_test_split(features, targets,
-                                                    test_size=0.20,
+                                                    test_size=0.25,
                                                     random_state=RANDOMSTATE)
 
 
@@ -49,3 +49,15 @@ for i, label in enumerate(np.unique(y_test)):
     plt.scatter(pca_result[:,0][labels==label],pca_result[:,1][labels==label], 
                 c=colors[i], marker=markers[i], lw=1)
 plt.title("The projection onto 2 PCA components");
+
+
+# Fit to data and predict using pipelined scaling, LDA.
+lda2 = LDA(n_components=2)
+lda2.fit(X_train,y_train)
+lda_result = lda2.transform(X_test)
+
+# plot
+for i, label in enumerate(np.unique(y_test)):
+    plt.scatter(lda_result[:,0][y_test==label],lda_result[:,1][y_test==label], 
+                c=colors[i], marker=markers[i], lw=1)
+plt.title("The projection onto 2 LDA components");
