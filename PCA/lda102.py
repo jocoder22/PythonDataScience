a05465c59564f07e251434b3c0bae72d7c7dc53d
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-12 06:53:28 -0400

    Update lda102.py

--- PCA/lda102.py
+++ PCA/lda102.py
@@ -11,3 +11,24 @@ from IPython.display import Image
 from IPython.display import HTML
 
 from printdescribe import 
+
+rom sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn import metrics
+from sklearn.datasets import load_wine
+from sklearn.pipeline import make_pipeline
+print(__doc__)
+
+
+RANDOMSTATE = 142
+FIGSIZE = (12, 8)
+
+
+features, targets = load_wine(return_X_y=True)
+
+# create train/test split using 20% test size
+X_train, X_test, y_train, y_test = train_test_split(features, targets,
+                                                    test_size=0.20,
+                                                    random_state=RANDOMSTATE)
