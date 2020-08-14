86fc29ec76e8fc5fae44ec4ee930f46b854ae5e2
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-14 10:59:38 -0400

    Update gmm.py

--- cluster_classifier/gmm.py
+++ cluster_classifier/gmm.py
@@ -21,6 +21,7 @@ from sklearn.pipeline import make_pipeline
 print(__doc__)
 
 RANDOMSTATE = 42
+show = plt.show()
 
 # download the data
 iris = datasets.load_iris()
@@ -74,51 +75,54 @@ test_accuracy = np.mean(pred_test.ravel() == y_test.ravel()) * 100
 print2(f"Test accuracy: {np.round(test_accuracy,2)}")
 
 
-
-
-for n, color in enumerate(colors):
-  #     plot the original data
+ for n, color in enumerate(colors):
+  # plot the original data
     data = iris.data[iris.target == n]
-    plt.scatter(data[:, 0], data[:, 1], s=2.8, color=color,
-                label=iris.target_names[n])
+    plt.scatter(data[:, 0], data[:, 1], s=2.8, color=color,label=iris.target_names[n])
     
-  #     plot the prediction
+  # plot the test data
     data = X_test[y_test == n]
-    plt.scatter(data[:, 0], data[:, 1], marker='x', color=color)
+    plt.plot(data[:, 0], data[:, 1], 'x', color=color)
+    
+  # plot the prediction
+    data = X_test[pred_test == n]
+    plt.scatter(data[:, 0], data[:, 1], marker='o', color="white", edgecolor=color)
+    
+ show;
     
     
-# plot
-for i, label in enumerate(np.unique(y_test)):
-    plt.plot(pca_result[:,0][labels==label],pca_result[:,1][labels==label], 
-                col[int(i)], label=labelnames[i])
-plt.title("The projection onto 2 PCA components")
-plt.legend(loc='best', shadow=False, scatterpoints=3)
-plt.show();
+# # plot
+# for i, label in enumerate(np.unique(y_test)):
+#     plt.plot(pca_result[:,0][labels==label],pca_result[:,1][labels==label], 
+#                 col[int(i)], label=labelnames[i])
+# plt.title("The projection onto 2 PCA components")
+# plt.legend(loc='best', shadow=False, scatterpoints=3)
+# plt.show();
 
 
-# Fit to data and predict using pipelined scaling, LDA.
-lda = LDA(n_components=2)
-lda.fit(X_train,y_train)
-lda_result = lda.transform(X_test)
+# # Fit to data and predict using pipelined scaling, LDA.
+# lda = LDA(n_components=2)
+# lda.fit(X_train,y_train)
+# lda_result = lda.transform(X_test)
 
-# plot
-for i, label in enumerate(np.unique(y_test)):
-    plt.scatter(lda_result[:,0][y_test==label],lda_result[:,1][y_test==label], 
-                c=colors[i], marker=markers[i], lw=1, label=labelnames[i])
-plt.title("The projection onto 2 LDA components with scaling")
-plt.legend(loc='best', shadow=False, scatterpoints=3)
-plt.show();
+# # plot
+# for i, label in enumerate(np.unique(y_test)):
+#     plt.scatter(lda_result[:,0][y_test==label],lda_result[:,1][y_test==label], 
+#                 c=colors[i], marker=markers[i], lw=1, label=labelnames[i])
+# plt.title("The projection onto 2 LDA components with scaling")
+# plt.legend(loc='best', shadow=False, scatterpoints=3)
+# plt.show();
 
 
-# Fit to data and predict without using pipelined scaling, LDA.
-lda2 = LDA(n_components=2)
-lda2.fit(X_train,y_train)
-lda_result = lda2.transform(X_test)
+# # Fit to data and predict without using pipelined scaling, LDA.
+# lda2 = LDA(n_components=2)
+# lda2.fit(X_train,y_train)
+# lda_result = lda2.transform(X_test)
 
-# plot
-for i, label in enumerate(np.unique(y_test)):
-    plt.scatter(lda_result[:,0][y_test==label],lda_result[:,1][y_test==label], 
-                c=colors[i], marker=markers[i], lw=1, label=labelnames[i])
-plt.title("The projection onto 2 LDA components without scaling")
-plt.legend(loc='best', shadow=False, scatterpoints=3)
-show;
+# # plot
+# for i, label in enumerate(np.unique(y_test)):
+#     plt.scatter(lda_result[:,0][y_test==label],lda_result[:,1][y_test==label], 
+#                 c=colors[i], marker=markers[i], lw=1, label=labelnames[i])
+# plt.title("The projection onto 2 LDA components without scaling")
+# plt.legend(loc='best', shadow=False, scatterpoints=3)
+# show;
