7058957acf4b991a9805d16b0c339fe98734b458
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-14 08:50:36 -0400

    Update gmm.py

--- cluster_classifier/gmm.py
+++ cluster_classifier/gmm.py
@@ -10,6 +10,15 @@ from sklearn.mixture import GMM
 
 from printdescribe import print2
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+from sklearn import metrics
+from sklearn.datasets import load_wine
+from sklearn.pipeline import make_pipeline
+print(__doc__)
+
+
 # download the data
 iris = datasets.load_iris()
 
