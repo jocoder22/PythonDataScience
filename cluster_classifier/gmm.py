2a15172882a19821272759cf46a6a5b26e74a3f4
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-14 08:44:54 -0400

    Update gmm.py

--- cluster_classifier/gmm.py
+++ cluster_classifier/gmm.py
@@ -1,1 +1,9 @@
 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+from sklearn import datasets
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.externals.six.moves import xrange
+from sklearn.mixture import GMM
