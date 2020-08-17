a1ddd4643a32122a7386cd9b0bcfcd92cb21573b
jocoder22 <okigbookey@gmail.com>
2020-08-16 22:11:19 -0400

    Update

--- ml_finance/group1.py
+++ ml_finance/group1.py
@@ -116,3 +116,19 @@ residuals.plot(kind='kde')
 plt.show()
 print(residuals.describe())
 
+X = log_returns['MSFT'].values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)+1]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+    model = ARIMA(history, order=(1,0,0))
+    model_fit = model.fit(disp=0)
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
+    history.append(obs)
+error = mean_squared_error(test, predictions)
+print('Test MSE: %.3f' % error)
+
