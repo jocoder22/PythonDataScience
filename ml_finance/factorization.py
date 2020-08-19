562456f8fbedd5a5bfd7406ca8c9cf90b72a6128
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-19 09:08:50 -0400

    Update factorization.py

--- ml_finance/factorization.py
+++ ml_finance/factorization.py
@@ -9,13 +9,6 @@ from printdescribe import print2
 
 np.random.seed(42)
 
-import pandas_datareader.data as dr
-import matplotlib
-import matplotlib.pyplot as plt 
-import math
-import statsmodels.tsa.stattools as ts
-import numpy as np 
-
 # create matrix
 A = A = np.linspace(1,9,9).reshape(-1,3)
 B = np.arange(10,26).reshape(-1,4)
