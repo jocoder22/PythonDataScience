5bcfd28f2bb76780b6159d3cf99b8d909b505a0a
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-19 07:14:02 -0400

    Update factorization.py

--- ml_finance/factorization.py
+++ ml_finance/factorization.py
@@ -2,6 +2,8 @@
 # Importing Libraries 
 import numpy as np 
 import pandas as pd 
+from printdescribe import print2
+
 import pandas_datareader.data as dr
 import matplotlib
 import matplotlib.pyplot as plt 
@@ -9,6 +11,10 @@ import math
 import statsmodels.tsa.stattools as ts
 import numpy as np 
 
-# PLU decomposition; used for square matrix
+# create matrix
 A = A = np.linspace(1,9,9).reshape(-1,3)
 B = np.arange(10,26).reshape(-1,4)
+
+# PLU decomposition; used for square matrix
+P,L, U = lu(A)
+print2(P,L,U)
