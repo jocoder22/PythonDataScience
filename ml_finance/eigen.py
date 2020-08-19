1ea0589105df2bb3f20c878d2aad18878cedeb83
Okechukwu Okigbo <okigbookey@gmail.com>
2020-08-19 09:06:57 -0400

    Update eigen.py

--- ml_finance/eigen.py
+++ ml_finance/eigen.py
@@ -1,1 +1,11 @@
+# Importing Libraries 
+import numpy as np 
+import pandas as pd 
+from scipy.linalg import lu, qr, cholesky
+from numpy import linalg as LA
+
+from printdescribe import print2
+
+np.random.seed(42)
+
 
