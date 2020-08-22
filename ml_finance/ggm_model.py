3705fd4d3f41edd29258539efd12209e892cd7ff
jocoder22 <okigbookey@gmail.com>
2020-08-22 19:24:22 -0400

    Update

--- ml_finance/ggm_model.py
+++ ml_finance/ggm_model.py
@@ -21,4 +21,27 @@ print2(digits.keys())
 number_digits = len(np.unique(digits.target))
 print2(number_digits)
 
+# Create a regular PCA model 
+pca = PCA(n_components=2).fit(digits.data)
+
+# Fit and transform the data to the model
+reduced_data_pca = pca.transform(digits.data)
+
+# Don't change the code in this block
+colors = ['black', 'blue', 'purple', 'yellow', 'white',
+          'red', 'lime', 'cyan', 'orange', 'gray']
+
+# plt.figure(figsize=[12, 7])
+for i in range(len(colors)):
+    x = reduced_data_pca[:, 0][digits.target == i]
+    y = reduced_data_pca[:, 1][digits.target == i]
+    plt.scatter(x, y, marker='o', s=20, facecolors=colors[i], edgecolors='k')
+#     plt.scatter(x, y, marker='o', s=55, facecolors=colors[i], edgecolors='k')
+    
+# PCA Scatter Plot
+plt.legend(digits.target_names, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel('First Principal Component')
+plt.ylabel('Second Principal Component')
+plt.title("Regular PCA Scatter Plot")
+plt.show()
 
