09aba6f50d41162f449e5475dddf840c94eec10f
jocoder22 <okigbookey@gmail.com>
2020-08-22 19:32:12 -0400

    Update

--- ml_finance/ggm_model.py
+++ ml_finance/ggm_model.py
@@ -7,11 +7,14 @@ from sklearn import datasets
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 
 from printdescribe import print2
 plt.rcParams["figure.figsize"] = 8,6
+
+rd42 = 42
 
 # Load in the `digits` data
 digits = datasets.load_digits()
@@ -45,3 +48,61 @@ plt.ylabel('Second Principal Component')
 plt.title("Regular PCA Scatter Plot")
 plt.show()
 
+# Create a regular LDA model 
+lda = LDA(n_components=2).fit(digits.data, digits.target)
+
+# Fit and transform the data to the model
+reduced_data_lda = lda.transform(digits.data)
+
+# Don't change the code in this block
+colors = ['black', 'blue', 'purple', 'yellow', 'white',
+          'red', 'lime', 'cyan', 'orange', 'gray']
+
+for i in range(len(colors)):
+    x = reduced_data_lda[:, 0][digits.target == i]
+    y = reduced_data_lda[:, 1][digits.target == i]
+    plt.scatter(x, y, marker='o', s=20, facecolors=colors[i], edgecolors='k')
+    
+# LDA Scatter Plot
+plt.legend(digits.target_names, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.xlabel('First LDA Component')
+plt.ylabel('Second LDA Component')
+plt.title("LDA Scatter Plot")
+plt.show()
+
+# Split the `digits` data into training and test sets
+X_train, X_test, y_train, y_test, images_train, images_test = train_test_split(digits.data, 
+                digits.target, digits.images, test_size=0.25, random_state=rd42)
+
+# Number of training features
+n_samples, n_features = X_train.shape
+
+# Print out `n_samples`
+print('Number of samples:', n_samples)
+
+# Print out `n_features`
+print('Number of features:', n_features)
+
+# Number of Training labels
+n_digits = len(np.unique(y_train))
+print ('Number of training labels:', n_digits)
+
+# Inspect `y_train`
+print('Number of labled data:', len(y_train))
+
+
+# finding number of clusters using elbow method
+distortions = []
+n_clusters = range(1, 50)
+
+for idx in n_clusters:
+    kmeans = KMeans(n_clusters=idx, init='k-means++', random_state=42)
+    kmeans.fit(X_train)
+    distortions.append(kmeans.inertia_)
+
+# Create a line plot of num_clusters and distortions
+plt.plot(n_clusters, distortions, 'ro-')
+plt.title('Number of Clusters: The Elbow Method ', size=15) 
+plt.xlabel('Number of clusters', size=12)
+plt.ylabel('Distortions', size=12)
+plt.show();
