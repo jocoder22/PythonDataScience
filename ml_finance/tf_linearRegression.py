2fb5f71370c8bd77025afb39c3b8ff825d39d87d
jocoder22 <okigbookey@gmail.com>
2020-09-03 03:49:00 -0400

    Update

--- ml_finance/tf_linearRegression.py
+++ ml_finance/tf_linearRegression.py
@@ -9,11 +9,59 @@ from printdescribe import print2
 print2(" ")
 
 # Generate data
-x_train = np.linspace(0, 1, 800)
-y_train = 0.2789 * x_train + 8.84 + 0.01 * np.random.randn(x_train.shape[0])
+x_train = np.linspace(0, 1, 100) 
+y_train =  0.2 * x_train + 8.89 + 0.21 * np.random.randn(x_train.shape[0])
 
 plt.plot(x_train, y_train, 'r.')
 plt.title('Generated Data for Regression')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
+
+
+tf.reset_default_graph()
+
+# Set hyperparameter
+learning_rate = 0.001
+
+# Model parameters (variables). 
+# Initialised with values not close to the correct values.
+w = tf.Variable([-3.9], dtype=tf.float32)
+b = tf.Variable([3.6], dtype=tf.float32)
+
+# Model input (placeholders)
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+
+# Linear model
+linear_model = w * x + b
+
+# loss, sum of squares errors
+mse = tf.reduce_sum(tf.square(linear_model - y)) 
+
+# Gradient, with multiple outputs
+grad = tf.gradients(mse, [w, b])
+
+# Gradient descent update
+# update the weights and bias
+update_w = tf.assign(w, w - learning_rate * grad[0])
+update_b = tf.assign(b, b - learning_rate * grad[1])
+
+# Define init Op
+init_op = tf.global_variables_initializer()
+
+# Execution
+with tf.Session() as sess:
+    sess.run(init_op)
+
+    # Descend for 1000 steps
+    for i in range(1000):
+        # print loss at every 100th step
+        if i%100 == 0:
+            print('mse = ', sess.run(mse, feed_dict = {x:x_train, y:y_train}))
+            
+        sess.run([update_w, update_b], feed_dict = {x:x_train, y:y_train})
+
+    # evaluate training accuracy
+    curr_W, curr_b, curr_mse = sess.run([w, b, mse], feed_dict = {x:x_train, y:y_train})
+    print(f"w: {curr_W} b: {curr_b} loss: {curr_mse}")
