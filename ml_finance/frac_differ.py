d69744203e675b5cf7259cec48f9cc468963e5cc
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-09 11:03:13 -0400

    Update frac_differ.py

--- ml_finance/frac_differ.py
+++ ml_finance/frac_differ.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+from printdescribe import print2
+
+sns.relplot(data=pd.Series(np.random.normal(0,1,1000)))
+plt.show();
+
+np.random.seed(4)
+sns.relplot(data=pd.Series(
+    np.cumsum(np.random.normal(0,1,1000))))
+plt.show();
