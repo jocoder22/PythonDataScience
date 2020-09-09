468f284d2c96b23344e1062adab5950f822a98bb
jocoder22 <okigbookey@gmail.com>
2020-09-08 21:03:00 -0400

    Update

--- ml_finance/tensorflow/tf_linearRegression.py
+++ ml_finance/tensorflow/tf_linearRegression.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 # import tensorflow as tf
-
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-
 from printdescribe import print2
+
+tf.disable_v2_behavior()
 print2(" ")
 
 # set matplotlib parameters
