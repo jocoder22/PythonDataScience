9b367b88820307173ca7ae60dea127159501c4ea
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-10 12:15:50 -0400

    Update intro101.py

--- turn_hyperparameter/intro101.py
+++ turn_hyperparameter/intro101.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split as tts
+import matplotlib.pyplot as plt
 
+from printdescribe import print2, describe2
+
+# please install graphviz and pydotplus
+
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
+data = pd.read_excel(path, header=1, index_col=0)
