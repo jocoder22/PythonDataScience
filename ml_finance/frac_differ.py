682758a3af57997b2a0eb28f170dde826621b6f0
jocoder22 <okigbookey@gmail.com>
2020-09-10 04:40:26 -0400

    Update

--- ml_finance/frac_differ.py
+++ ml_finance/frac_differ.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pkgutil
 import mlfinlab
-from mlfinlab.features.fracdiff import FractionalDifferentiation as ff
 
+from mlfinlab.features.fracdiff import FractionalDifferentiation as ff
+from mlfinlab.features.fracdiff import FractionalDifferentiation
+from mlfinlab.features.fracdiff import frac_diff, plot_min_ffd, frac_diff_ffd
+
+from statsmodels.tsa.stattools import adfuller
 import pandas_datareader as pdr
 from datetime import datetime, date
 from printdescribe import print2
@@ -49,5 +53,67 @@ result.dropna(inplace=True)
 print(result.head())
 
 
-sns.relplot(height=8, data=result, kind="line")
+sns.relplot(height=6, data=result, kind="line")
 plt.show();
+
+
+def plot_min_ffd2(series):
+    """
+    Advances in Financial Machine Learning, Chapter 5, section 5.6, page 85.
+
+    References:
+
+    * https://www.wiley.com/en-us/Advances+in+Financial+Machine+Learning-p-9781119482086
+
+    This function plots the graph to find the minimum D value that passes the ADF test.
+
+    It allows to determine d - the amount of memory that needs to be removed to achieve
+    stationarity. This function covers the case of 0 < d << 1, when the original series is
+    "mildly non-stationary."
+
+    The right y-axis on the plot is the ADF statistic computed on the input series downsampled
+    to a daily frequency.
+
+    The x-axis displays the d value used to generate the series on which the ADF statistic is computed.
+
+    The left y-axis plots the correlation between the original series (d=0) and the differentiated
+    series at various d values.
+
+    Examples on how to interpret the results of this function are available in the corresponding part
+    in the book Advances in Financial Machine Learning.
+
+    :param series: (pd.DataFrame) Dataframe that contains a 'close' column with prices to use.
+    :return: (plt.AxesSubplot) A plot that can be displayed or used to obtain resulting data.
+    """
+
+    results = pd.DataFrame(columns=['adfStat', 'pVal', 'lags', 'nObs', '95% conf', 'corr'])
+
+    # Iterate through d values with 0.1 step
+    for d_value in np.linspace(0, 1, 11):
+        # close_prices = np.log(series[['close']]).resample('1D').last()  # Downcast to daily obs
+        close_prices = np.log(series[['close']])
+        close_prices.dropna(inplace=True)
+        print2(" ")
+        print2(close_prices)
+
+        # Applying fractional differentiation
+        differenced_series = frac_diff_ffd(close_prices, diff_amt=d_value, thresh=0.01).dropna()
+
+        # Correlation between the original and the differentiated series
+        corr = np.corrcoef(close_prices.loc[differenced_series.index, 'close'],
+                           differenced_series['close'])[0, 1]
+        # Applying ADF
+        differenced_series = adfuller(differenced_series['close'], maxlag=1, regression='c', autolag=None)
+
+        # Results to dataframe
+        results.loc[d_value] = list(differenced_series[:4]) + [differenced_series[4]['5%']] + [corr]  # With critical value
+
+    # Plotting
+    plot = results[['adfStat', 'corr']].plot(secondary_y='adfStat', figsize=(10, 8))
+    plt.axhline(results['95% conf'].mean(), linewidth=1, color='r', linestyle='dotted')
+
+    return plot
+
+stock.columns = ["close"]
+ax = plot_min_ffd2(stock)
+plt.show()
