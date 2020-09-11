5d52e93bf15f538c4f90210622cb941ebf5ad492
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-11 11:40:06 -0400

    Update intro202.py

--- ml_finance/intro202.py
+++ ml_finance/intro202.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
 
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+
+from printdescribe import print2
+
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
+
+data = pd.read_excel(path,header=1, index_col=0)
+data = data.rename(columns={'default payment next month':"default"})
+
+X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data.iloc[:,-1], stratify=data.iloc[:,-1], test_size=0.3)
