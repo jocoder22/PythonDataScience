d3174c74b8baf66b1097f8dea004f19027dd2509
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-15 12:55:03 -0400

    Update trading_Al.py

--- ml_finance/trading_Al.py
+++ ml_finance/trading_Al.py
@@ -23,3 +23,45 @@ print2(datasets.tail())
 
 datasets['Adj Close'].plot(figsize=(10,5))
 plt.show();
+
+
+# generate SMA crossover trend predictions
+
+# # compute moving averages
+# fast_window = 10
+# slow_window = 30
+
+# # compute moving averages
+# fast_window = 20
+# slow_window = 60
+
+# compute moving averages
+fast_window = 10
+slow_window = 60
+
+# compute moving averages
+# fast_window = 30
+# slow_window = 60
+
+# # compute moving averages
+# fast_window = 10
+# slow_window = 50
+
+# compute moving averages
+# fast_window = 20
+# slow_window = 50
+
+datasets['fast_mavg'] = datasets['Adj Close'].rolling(window=fast_window, min_periods=fast_window, center=False).mean()
+datasets['slow_mavg'] = datasets['Adj Close'].rolling(window=slow_window, min_periods=slow_window, center=False).mean()
+
+# Compute sides
+datasets['side'] = np.nan
+
+# create signal
+long_signals = datasets['fast_mavg'] >= datasets['slow_mavg'] 
+short_signals = datasets['fast_mavg'] < datasets['slow_mavg'] 
+datasets.loc[long_signals, 'side'] = 1
+datasets.loc[short_signals, 'side'] = -1
+
+# Remove Look ahead biase by lagging the signal
+datasets['side'] = datasets['side'].shift(1)
