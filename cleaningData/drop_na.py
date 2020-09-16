ad0ea039a5a6a18f5d99f577aa28ecf3aa176b4d
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-16 10:58:30 -0400

    Update drop_na.py

--- cleaningData/drop_na.py
+++ cleaningData/drop_na.py
@@ -1,5 +1,47 @@
 import numpy as np
 import pandas as pd
+from printdescribe import print2
 
+# download dataset
+url2 = "https://archive.ics.uci.edu/ml/machine-learning-databases/00529/diabetes_data_upload.csv"
+df = pd.read_csv(url2)
 
-data = 
+# expore dataset
+print2(df.shape, df.dtypes, df.columns, df.info(), df.describe())
+
+# Explore categorical features
+for col in [x for x in df.select_dtypes(include=['object'])]:
+    print2(df[col].value_counts(ascending=True, dropna=False))
+    
+    
+# Add nan to dataset
+num = df.Age.max()
+for col in [x for x in df.select_dtypes(include=['object'])]:
+    numm = num - 3
+    df[col] = np.where(df.Age > numm, np.nan, df[col])
+    num = numm
+
+# view the new dataset
+print2(df.isnull().sum())
+print(df.info())
+
+# explore the categorical datasets
+for col in [x for x in df.select_dtypes(include=['object'])]:
+    print2(df[col].value_counts(ascending=True, dropna=False))
+    
+
+# drop nan row
+df.dropna(axis=0)
+
+# At least one or any at all
+df.dropna(axis=0, how ="any")
+
+# All or full nan
+df.dropna(axis=0, how ="all")
+
+# drop nan columns
+df.dropna(axis=1)
+
+# Keep only the col with at least 95% non-NA values.
+_95 = int(df.shape[0] * 0.95)
+df.dropna(axis=0, thresh=_95)
