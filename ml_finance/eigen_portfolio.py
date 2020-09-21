1df32d02d9146f406166ff71f54a2213c0d9c38f
jocoder22 <okigbookey@gmail.com>
2020-09-21 19:59:21 -0400

    Update

--- ml_finance/eigen_portfolio.py
+++ ml_finance/eigen_portfolio.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os
 import numpy as np
 import pandas as pd
@@ -12,13 +13,16 @@ from printdescribe import print2, changepath
 import warnings
 warnings.filterwarnings('ignore')
 
+# select required folder
 patth = r"D:\PythonDataScience\ml_finance"
 
+# import the necessary datasets
 with changepath(patth):
     # dataset3.to_csv("assets3.csv",  compression='gzip')
     dataset3 = pd.read_csv("assets3.csv",  compression='gzip', parse_dates=True, index_col="Date")
     dataset2 = pd.read_csv("assets2.csv",  compression='gzip', parse_dates=True, index_col="Date")
 
+# combine the datasets
 dataset2.rename(columns={"Adj Close": "SPX"}, inplace=True)
 dataset3.drop(columns =["^GSPC"], inplace=True)
 
