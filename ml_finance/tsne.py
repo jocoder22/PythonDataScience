b032cc2df301bafd142d7166ef3f8f3da484b7b1
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-23 07:54:14 -0400

    Update tsne.py

--- ml_finance/tsne.py
+++ ml_finance/tsne.py
@@ -50,3 +50,26 @@ def check__nulls(df):
     
     
 check__nulls(data2)
+
+
+# Get the SPX time series. This now returns a Pandas Series object indexed by date
+spx_index = data2.loc[:, 'SPX']
+
+short_rolling_spx = pd.core.series.Series(np.zeros(len(asset_prices.index)), index=asset_prices.index)
+long_rolling_spx = short_rolling_spx
+
+# Calculate the 20 and 100 days moving averages of log-returns
+short_rolling_spx = spx_index.rolling(20).mean()
+long_rolling_spx = spx_index.rolling(100).mean()
+
+
+# Plot the index and rolling averages
+fig=plt.figure(figsize=(12, 5), dpi= 80, facecolor='w', edgecolor='k')
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(spx_index.index, spx_index, label='SPX Index')
+ax.plot(short_rolling_spx.index, short_rolling_spx, label='20 days rolling')
+ax.plot(long_rolling_spx.index, long_rolling_spx, label='100 days rolling')
+ax.set_xlabel('Date')
+ax.set_ylabel('Log returns')
+ax.legend(loc=2)
+plt.show()
