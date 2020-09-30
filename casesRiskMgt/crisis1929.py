857bebe2f3292586ebab59d7151b2fa515014539
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-30 11:35:55 -0400

    Update crisis1929.py

--- casesRiskMgt/crisis1929.py
+++ casesRiskMgt/crisis1929.py
@@ -8,3 +8,41 @@ import holoviews as hv
 import hvplot.pandas
 
 hv.extension('bokeh')
+
+
+data_ranges = [[1970,1979, 'dat'],
+               [1960,1969, 'dat'],
+               [1950,1959, 'dat'],
+               [1940,1946, 'dat'],
+               [1930,1939, 'dat'],
+               [1920,1929, 'dat'],
+               [1910,1919, 'dat'],
+               [1900,1909, 'dat'],
+               [1888,1899, 'dat']][::-1]
+
+# Download data
+
+def get_decade(start=1920, end=1929, extension='prn'):
+  "specify the starting year of the decade eg. 1900, 2010, 2009"
+  try:
+    link = requests.get(f"https://www.nyse.com/publicdocs/nyse.data/Daily_Share_Volume_{start}-{end}.{extension}")
+    file = os.path.join(" ","Data",f"Daily_Share_Volume_{start}-{end}.{extension}")
+    
+    if link.status_code == 404:
+      raise
+     else:
+      with open(file, 'w') as temp_file:
+        temp_file.write(str(link.content.decode("utf-8")))
+        
+        print2(f"Successfully downloaded {start}-{end}")
+  except:
+    print2("There was an issue with the download \n\
+            You may need a different date range or file extension.\n\
+            Check out https://www.nyse.com/data/transactions-statistics-data-library")
+    
+download_history = [get_decade(decade[0], decade[1], decade[2]) for decade in date_ranges]
+            
+        
+  
+    
+               
