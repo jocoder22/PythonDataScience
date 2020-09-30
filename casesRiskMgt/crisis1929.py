f662c33eecf773c7478be54fd650af4ed03ed57b
Okechukwu Okigbo <okigbookey@gmail.com>
2020-09-30 11:48:00 -0400

    Update crisis1929.py

--- casesRiskMgt/crisis1929.py
+++ casesRiskMgt/crisis1929.py
@@ -41,6 +41,29 @@ def get_decade(start=1920, end=1929, extension='prn'):
             Check out https://www.nyse.com/data/transactions-statistics-data-library")
     
 download_history = [get_decade(decade[0], decade[1], decade[2]) for decade in date_ranges]
+
+# read and format the data
+def load_data(start=1920, end=1929, extension="prn"):
+  
+  # get the path
+  path = os.path.join(" ", "Data",f"Daily_Share_Volume_{start}-{end}.{extension}")
+  
+  if extension = "prn":
+    data = pd.read_csv(path, sep='  ', parse_dates=['Date'], engine='python').iloc[2:, 0:2]
+    data.loc[:, "  Stock U.S Gov't"] = pd.to_numeric(data.loc[:, "  Stock U.S Gov't"], errors='coerce')
+    data.Date = pd.to_datetime(data.Date, format='%Y%m%d', errors="coerce")
+    data.columns = ['Date', 'Volume']
+    return data
+  
+  else:
+    data = pd.read_csv(path)
+    data.iloc[:,0] = data.iloc[:,0].apply(lambda x: str(x).strip(' '))
+    data = data.iloc[:,0].str.split(' ', 1, expand=True)
+    data.columns = ['Date', 'Volume']
+    data.loc[:, "Volume"] = pd.to_numeric(data.loc[:, "Volume"], errors='coerce')
+    data.Date = pd.to_datetime(data.Date, format='%Y%m%d', errors="coerce")
+    return data
+    
             
         
   
