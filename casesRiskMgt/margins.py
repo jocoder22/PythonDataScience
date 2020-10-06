1b4d3d90f841d3822d2dc3d30fdc5224e6870aed
jocoder22 <okigbookey@gmail.com>
2020-10-06 19:38:08 -0400

    Update

--- casesRiskMgt/margins.py
+++ casesRiskMgt/margins.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+import os
+import requests
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from functools import reduce
+import operator
+
+import holoviews as hv
+import hvplot.pandas
+
+from printdescribe import print2
+
+hv.extension('bokeh')
