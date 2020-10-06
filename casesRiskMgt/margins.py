31aff18c6ccf2aac33cc7a952bf93ab4656122ca
jocoder22 <okigbookey@gmail.com>
2020-10-06 19:40:40 -0400

    Update

--- casesRiskMgt/margins.py
+++ casesRiskMgt/margins.py
@@ -16,3 +16,13 @@ from printdescribe import print2
 
 np.random.seed(42)
 hv.extension('bokeh')
+
+
+def plot(mu, sigma, samples):
+    return pd.Series(np.random.normal(mu,sigma, 1000)).cumsum(
+    ).hvplot(title='Random Walks', label=f'{samples}')
+
+def prod(mu, sigma, samples):
+    return reduce(operator.mul, 
+                  list(map(lambda x: plot(mu,sigma, x), 
+                           range(1,samples+1))))
