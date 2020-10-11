ec369ac359d96ac3ea19b3d531ee7ceb705b640b
jocoder22 <okigbookey@gmail.com>
2020-10-10 22:41:45 -0400

    Update

--- casesRiskMgt/margins.py
+++ casesRiskMgt/margins.py
@@ -104,3 +104,5 @@ def simulation_prices(days=100, runs=1000, axis=0):
 
 %%opts Curve [width=500 height=300]
 hv.DynamicMap(simulation_plots, kdims=['days', 'runs']).redim.range(days=(100,500), runs=(5,15)).options(width=900, height=400)
+
+simulations = simulation_prices()
