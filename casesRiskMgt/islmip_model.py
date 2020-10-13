0919af97bbc6289488a07e010f26504af607331f
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-13 14:56:37 -0400

    Update islmip_model.py

--- casesRiskMgt/islmip_model.py
+++ casesRiskMgt/islmip_model.py
@@ -14,3 +14,24 @@ hv.extension('bokeh')
 np.random.seed(42)
 
 
+def i(*args, **kwargs):
+    i = np.linspace(-6,6,100).reshape(-1,1)
+    i = i[i!=0]
+    return i
+
+def IS(i=i(), Z_1=0, df=True):
+    return pd.DataFrame([i, -i+Z_1], index=['Real Interest','Real Output']).T if df else -i+Z_1
+
+
+def LM(i=i(), Z_2=0, df=True):
+    return pd.DataFrame([i, i-Z_2], index=['Real Interest','Real Output']).T if df else i-Z_2
+
+
+def IP(i=i(), Z_3=0, df=True):
+    return pd.DataFrame([i, i-Z_3], index=['Real Interest','Exchange Rate']).T if df else i+Z_3
+
+
+def findIntersection(fun1, fun2, x0):
+    return fsolve(lambda x: fun1(x) - fun2(x), x0
+
+
