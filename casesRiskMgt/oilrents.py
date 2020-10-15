d1555c205557b104c009d3bed346cc0ce50a9428
jocoder22 <okigbookey@gmail.com>
2020-10-14 21:30:50 -0400

    Update

---
+++ casesRiskMgt/oilrents.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+from scipy.optimize import fsolve
+from scipy.stats import iqr
+from scipy import signal
+
+import pandas_datareader.wb as wb
+
+import holoviews as hv
+import hvplot.pandas
+
+hv.extension('bokeh')
+np.random.seed(42)
