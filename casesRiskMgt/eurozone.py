3a2921608ddc5fbcf36d92a26fb384e0352ad450
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-19 09:47:14 -0400

    Update eurozone.py

--- casesRiskMgt/eurozone.py
+++ casesRiskMgt/eurozone.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+from scipy.stats import iqr
+from scipy import signal
 
+import statsmodels.api as sm
+from functools import reduce
+import operator
+
+import pandas_datareader.wb as wb
+
+import holoviews as hv
+import hvplot.pandas
+
+from printdescribe import print2
+
+hv.extension('bokeh')
+np.random.seed(42)
