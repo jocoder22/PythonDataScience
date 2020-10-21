0f11941f447d51b7bcbe0cefe5f619c49c289aa5
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-21 09:27:57 -0400

    Update hedgefund.py

--- casesRiskMgt/hedgefund.py
+++ casesRiskMgt/hedgefund.py
@@ -2,19 +2,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
-from scipy.stats import iqr
-from scipy import signal
 
-import statsmodels.api as sm
 from functools import reduce
-import operator
+from operator import mul
 
+from statsmodels.regression.linear_model import OLS
+from statsmodels.stats.stattools import durbin_watson
+from statsmodels.stats.diagnostic import acorr_ljungbox
+from sklearn import linear_model
+from sklearn.decomposition import PCA
+
+pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader.wb as wb
-import wbdata
 
 import holoviews as hv
+import hvplot
 import hvplot.pandas
+
 
 from printdescribe import print2
 
