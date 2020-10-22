34372c168e854b379dd495df749eef8246fd288c
jocoder22 <okigbookey@gmail.com>
2020-10-22 02:19:54 -0400

    Update

--- casesRiskMgt/hedgefund.py
+++ casesRiskMgt/hedgefund.py
@@ -13,7 +13,10 @@ from sklearn import linear_model
 from sklearn.decomposition import PCA
 
 pd.core.common.is_list_like = pd.api.types.is_list_like
+
 import pandas_datareader as pdr
+from pandas_datareader import data as pdr
+from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 
 import holoviews as hv
 import hvplot
@@ -25,12 +28,12 @@ from printdescribe import print2
 hv.extension('bokeh')
 np.random.seed(42)
 
-apple = pdr.robinhood.RobinhoodHistoricalReader(['AAPL'], retry_count=3, pause=0.1,
-                                                timeout=30, session=None, interval='day',
-                                                span='year').read().reset_index()
+# apple = pdr.robinhood.RobinhoodHistoricalReader(['AAPL'], retry_count=3, pause=0.1,
+#                                                 timeout=30, session=None, interval='day',
+#                                                 span='year').read().reset_index()
 
-dw = durbin_watson(pd.to_numeric(apple.close_price).pct_change().dropna().values)
-print2(f'DW_Statistics: {dw}')
+# dw = durbin_watson(pd.to_numeric(apple.close_price).pct_change().dropna().values)
+# print2(f'DW_Statistics: {dw}')
 
 
 starttime = '2018-01-01'
@@ -42,14 +45,16 @@ dw = durbin_watson(pd.to_numeric(apple.Close).pct_change().dropna().values)
 print2(f'DW_Statistics: {dw}')
 
 
-# tickers = pdr.nasdaq_trader.get_nasdaq_symbols(retry_count=3, timeout=300, pause=None)
-from pandas_datareader import data as pdr
+tickers = pdr.nasdaq_trader.get_nasdaq_symbols(retry_count=3, timeout=300, pause=None)
+
 all_symbols = pdr.get_nasdaq_symbols()
-all_symbols.head()
+print2(all_symbols.head())
 
-from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
+
 symbols = get_nasdaq_symbols()
+print2(symbols.head(), symbols.shape)
 
-from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
+
 symbols = get_nasdaq_symbols()
 etfs = symbols.loc[tickers.ETF == True, :]
+print2(etfs.head(), etfs.shape)
