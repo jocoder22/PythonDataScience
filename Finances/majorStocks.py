7b604ce51f60828ceea346749182379bf7a141ea
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-30 10:42:31 -0400

    Update majorStocks.py

--- Finances/majorStocks.py
+++ Finances/majorStocks.py
@@ -8,3 +8,8 @@ enddate = date.today()
 
 tickers = 'FB AMZN AAPL GOOGL NFLX MSFT ^GSPC'.split()
 portfolio = pdr.get_data_yahoo(tickers, startdate, enddate)['Adj Close']
+
+portfolio.iloc[:,[0,2,4]].plot(figsize=(14,8))
+plt.grid(color='black', which='major', axis='y', linestyle='solid')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+          fancybox=True, shadow=True, ncol=3);
