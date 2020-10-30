e8792fdeff0c7cd263160a8b4c93117d55be8b15
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-30 10:21:12 -0400

    Update majorStocks.py

--- Finances/majorStocks.py
+++ Finances/majorStocks.py
@@ -3,6 +3,8 @@ import pandas as pd
 import pandas_datareader as pdr
 import datetime
 
-startdate = "2000=01-01"
+startdate = datetime(1996, 1, 1)
+enddate = date.today()
 
 ticker = 'FG AMZN AAPL GOOGL NFLX MSFT ^GSPC'.split()
+portfolio = pdr.get_data_yahoo(symbol, startdate, enddate)['Adj Close']
