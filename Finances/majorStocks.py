e48d3b9709a8b0e543c9c4fab7c75bddb457462a
Okechukwu Okigbo <okigbookey@gmail.com>
2020-10-30 12:16:04 -0400

    Update majorStocks.py

--- Finances/majorStocks.py
+++ Finances/majorStocks.py
@@ -22,3 +22,43 @@ portfolio.iloc[:,[1,3,5,6]].plot(figsize=(14,8))
 plt.grid(color='black', which='major', axis='y', linestyle='solid')
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
           fancybox=True, shadow=True, ncol=4);
+
+
+def stock_analysis(df):
+    startdate = "2012-01-01"
+    labels = ["Adjusted Close", "Volume Traded", "Value Traded"]
+    
+    if type(df) == str:
+        stock = pdr.get_data_yahoo(df, startdate)
+        stock['valueTraded'] = stock.Volume *  stock['Adj Close']
+        
+        fig, axs = plt.subplots(3, sharex=True, figsize=(14,12))
+        plt.grid(color='black', which='major', axis='y', linestyle='solid')
+
+        stock.loc[:,['Adj Close']].plot(ax=axs[0], label="Adjusted Close", title=f'{labels[0]} for {df}', grid=True)
+        stock.loc[:,['Volume']].plot(ax=axs[1], label="Volume Traded", title=f'{labels[0]} for {df}', grid=True)
+        stock.loc[:,['valueTraded']].plot(ax=axs[2], label="Value Traded", title=f'{labels[0]} for {df}', grid=True)
+        
+        fig.tight_layout()
+        fig.subplots_adjust(top=0.88)
+#         plt.grid(color='black', which='major', axis='y', linestyle='solid')
+        plt.show()
+
+        
+    elif isinstance(df, list):
+        portfolio = pdr.get_data_yahoo(df, startdate)
+        
+        for name in df:
+            fig, axs = plt.subplots(3, sharex=True, figsize=(14,12))
+            
+            portfolio['Adj Close'][name].plot(ax=axs[0], label=labels[0], title=f'{labels[0]} for {name}', grid=True)
+            portfolio['Volume'][name].plot(ax=axs[1], label=labels[1], title=f'{labels[1]} for {name}', grid=True)
+            valueTraded = portfolio['Volume'][name] * portfolio['Adj Close'][name]
+            valueTraded.plot(ax=axs[2], label=labels[2], title=f'{labels[2]} for {name}', grid=True)
+            
+            fig.tight_layout()
+            fig.subplots_adjust(top=0.88)
+            plt.show()
+            print("\n\n\n")
+                    
+stock_analysis('AAPL')        
