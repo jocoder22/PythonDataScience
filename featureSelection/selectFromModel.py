7ae730f6388a52a2a10d1db375d40a5f66a4a2ef
Okechukwu Okigbo <okigbookey@gmail.com>
2020-11-02 14:20:30 -0500

    Update selectFromModel.py

--- featureSelection/selectFromModel.py
+++ featureSelection/selectFromModel.py
@@ -1,1 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+from sklearn.datasets import load_diabetes
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LassoCV
+
+diabetes = load_diabetes()
+
+X = diabetes.data
+y = diabetes.target
+
+feature_names = diabetes.feature_names
+print(feature_names)
+
+# Now we want to select the two features which are the most important. SelectFromModel() allows for setting the threshold. 
+# Only the features with the coef_ higher than the threshold will remain. 
+# Here, we want to set the threshold slightly above the third highest coef_ calculated by LassoCV() from our data.
+
+clf = LassoCV().fit(X, y)
+importance = np.abs(clf.coef_)
+print(importance)
+
+
+idx_third = importance.argsort()[-3]
+threshold = importance[idx_third] + 0.01
+
+idx_features = (-importance).argsort()[:2]
+name_features = np.array(feature_names)[idx_features]
+print('Selected features: {}'.format(name_features))
+
+sfm = SelectFromModel(clf, threshold=threshold)
+sfm.fit(X, y)
+X_transform = sfm.transform(X)
+
+n_features = sfm.transform(X).shape[1]
+
+
+plt.title(
+    "Features from diabets using SelectFromModel with "
+    "threshold %0.3f." % sfm.threshold)
+feature1 = X_transform[:, 0]
+feature2 = X_transform[:, 1]
+plt.plot(feature1, feature2, 'r.')
+plt.xlabel("First feature: {}".format(name_features[0]))
+plt.ylabel("Second feature: {}".format(name_features[1]))
+plt.ylim([np.min(feature2), np.max(feature2)])
+plt.show()
