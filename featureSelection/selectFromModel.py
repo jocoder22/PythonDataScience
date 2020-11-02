08000e0f984a768892cdd7b33ae084d093b21230
Okechukwu Okigbo <okigbookey@gmail.com>
2020-11-02 14:44:15 -0500

    Update selectFromModel.py

--- featureSelection/selectFromModel.py
+++ featureSelection/selectFromModel.py
@@ -124,3 +124,28 @@ for i in range(3):
     plt.title("F-test={:.2f}, MI={:.2f}".format(f_test[i], mi[i]),
               fontsize=16)
 plt.show()
+
+
+np.random.seed(0)
+X = np.random.rand(1000, 6)
+# y = X[:, 0] + np.sin(6 * np.pi * X[:, 3]) + 0.1 * np.random.randn(1000)
+# y = X[:, 0] + np.sin(6 * np.pi) + 0.1 * np.random.randn(1000)
+y =  np.sin(6 * np.pi) + 0.1 * np.random.randn(1000)
+
+f_test, _ = f_regression(X, y)
+f_test /= np.max(f_test)
+
+mi = mutual_info_regression(X, y)
+mi /= np.max(mi)
+
+plt.figure(figsize=(15, 5))
+for i in range(6):
+    plt.subplot(2, 3, i + 1)
+    plt.scatter(X[:, i], y, edgecolor='black', s=20)
+    plt.xlabel("$x_{}$".format(i + 1), fontsize=14)
+    if i == 0:
+        plt.ylabel("$y$", fontsize=14)
+    plt.title("F-test={:.2f}, MI={:.2f}".format(f_test[i], mi[i]),
+              fontsize=16)
+    plt.subplots_adjust(hspace=0.7)
+plt.show()
