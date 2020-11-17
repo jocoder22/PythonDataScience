05e811952b34705e0415e5593b6439b4552f297d
jocoder22 <okigbookey@gmail.com>
2020-11-16 23:38:40 -0500

    Update

--- casesRiskMgt/finalgroup.py
+++ casesRiskMgt/finalgroup.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 import pandas_datareader.wb as wb
 import matplotlib.pyplot as plt
+
+from printdescribe import print2, changepath
+
+
+data_r = pd.read_excel("greece_quarterly_30Y_reduced_20201102.xlsx", sheet_name="Reduced")
