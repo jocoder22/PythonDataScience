07d83c7526fec208254cf8520518db9652bbbc92
jocoder22 <okigbookey@gmail.com>
2020-11-17 00:12:17 -0500

    Update

--- casesRiskMgt/finalgroup.py
+++ casesRiskMgt/finalgroup.py
@@ -17,3 +17,18 @@ print2(data_r2.head(), data_r2.info())
 
 data = data_r2.iloc[:, [0,10,12,27,9]]
 print2(data.head)
+
+# M4 Money Plot
+money = wb.download(indicator='GC.TAX.TOTL.CN', country=['GR','GRC'], 
+                    start=pd.to_datetime('1990', yearfirst=True), end=pd.to_datetime('2020', yearfirst=True)
+                   , freq='Q')
+money = money.reset_index()
+print(money.head(10))
+# money_plot = money.iloc[::-1,:].hvplot.line(x='year', y='FM.LBL.BMNY.GD.ZS', by='country', title='Broad money (% of GDP)')
+
+data4 = data.reset_index()
+data4[["Quarter", "Year"]] = data4['Name'].str.split(expand=True)
+print2(data4.head())
+
+result = pd.merge(data4, money, left_on="Year", right_on="year", how="outer")
+cleandata = result.dropna()
