83436c9caf7ce81602461f34e7cc472c468048e3
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-17 11:38:38 -0400

    Create Modern_Portfolio_theory.py

---
+++ Finances/Modern_Portfolio_theory.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import numpy as np
+import pandas as pd
+import pandas_datareader as pdr
+from datetime import date, datetime
+
+startdate = datetime(2015, 1, 1)
+enddate = date.today()
