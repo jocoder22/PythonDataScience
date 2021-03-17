0dbfbf496dc31d41b1358b0c845676bc31003121
Okechukwu Okigbo <okigbookey@gmail.com>
2021-03-17 11:54:07 -0400

    Update Modern_Portfolio_theory.py

--- Finances/Modern_Portfolio_theory.py
+++ Finances/Modern_Portfolio_theory.py
@@ -4,7 +4,8 @@ import pandas as pd
 import pandas_datareader as pdr
 from datetime import date, datetime
 
-startdate = datetime(2015, 1, 1)
+# startdate = datetime(2015, 1, 1)
+startdate = datetime(2018, 9, 12)
 enddate = date.today()
 
 folders = ['Apple', 'MicroSoft', 'Google', 'Netflix', 'Tesla', 'Amazon', 'Toyota', 'JPMorgan', 
@@ -16,3 +17,8 @@ folders = ['Apple', 'MicroSoft', 'Google', 'Netflix', 'Tesla', 'Amazon', 'Toyota
 symbols = ['AAPL', 'MSFT', 'GOOGL', 'NFLX', 'TSLA', 'AMZN', 'TM', 'JPM', 'C', 'WMT', 'TGT', 'FDX',
             'UPS', 'WBA', 'DIS', 'PFE', 'CVS', 'T', 'KO', 'BA', 'SEDG', 'AMD', 'TWLO', 'EXPI', 'HD',
               'F', 'PVH', 'TWTR', 'CRM', 'BABA', 'NIO', 'AAPL', 'BMY']
+
+coldict = {symbols[i]:folders[i] for i in range(len(symbols))}
+
+df = pdr.get_data_yahoo(symbols, startdate, enddate)['Adj Close']
+df.head()
