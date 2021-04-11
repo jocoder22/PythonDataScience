13f151f733e55cd9f3887cefd00b133cbdf696f1
jocoder22 <okigbookey@gmail.com>
2021-04-11 19:28:30 -0400

    Update

--- beautifulSoup/session2.py
+++ beautifulSoup/session2.py
@@ -14,7 +14,6 @@ session = requests.Session()
 # to see more https://www.whatismybrowser.com/
 
 url = "https://en.wikipedia.org/wiki/List_of_Nobel_laureates"
-
 page = session.get(url).text
 
 # make a BeautifulSoup object
