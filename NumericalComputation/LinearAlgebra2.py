60020dfa7401b79d1839459e2f2f8d8c9d1b2b5d
Okechukwu Okigbo <okigbookey@gmail.com>
2021-04-30 13:51:53 -0400

    Update LinearAlgebra2.py

--- NumericalComputation/LinearAlgebra2.py
+++ NumericalComputation/LinearAlgebra2.py
@@ -1,1 +1,24 @@
+# import sympy 
+from sympy import *
+init_printing(use_unicode=True)
 
+# form Matrix
+Ma = Matrix([[1, 3, 9, 4], [2, 6, 5,-2], [4, 1, 3, -7], [5, 0, 4, 6]])
+Mb = Matrix([[2, 3, 2, -1], [-2, -1, 5,-2], [2, 0, 3, -1], [5, -2, 2, 4]])
+
+# Matrix addition and substration : + , -
+
+Ma + Mb
+Ma - Mb
+
+# Matrix Multiplication: *
+
+Ma * Mb
+
+# self Multiplicaton
+Ma*Ma
+Ma**2
+
+# rows and columns: Zero Indexed
+Ma.row(1)
+Ma.col(1)
