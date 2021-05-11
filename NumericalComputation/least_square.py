4c33d4dc19c0c8bcfaa202f58a98ce98d99594f5
Okechukwu Okigbo <okigbookey@gmail.com>
2021-05-11 14:23:10 -0400

    Update least_square.py

--- NumericalComputation/least_square.py
+++ NumericalComputation/least_square.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sympy import *
 import pandas as pd
+from scipy.linalg import lu
+from IPython.display import display, Latex
 
 
 bb = np.array([[1,5],[2,3],[3,6]])
@@ -8,3 +10,7 @@ tt = np.append(np.ones(3).reshape(-1,1),bb, axis = 1)
 
 ttTtt = tt.T@tt
 ttTb = tt.T@b
+
+
+ttaa = np.append(ttTtt, ttTb.reshape(-1,1), axis=1)
+p, l, u = lu(ttaa)
