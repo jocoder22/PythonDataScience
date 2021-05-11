4a8158391f41df7f14083bc5663bd0c04375c582
Okechukwu Okigbo <okigbookey@gmail.com>
2021-05-11 14:30:49 -0400

    Update least_square.py

--- NumericalComputation/least_square.py
+++ NumericalComputation/least_square.py
@@ -4,21 +4,33 @@ import pandas as pd
 from scipy.linalg import lu
 from IPython.display import display, Latex
 
-
+# create arrays of independent variables
 bb = np.array([[1,5],[2,3],[3,6]])
-tt = np.append(np.ones(3).reshape(-1,1),bb, axis = 1)
 
+# create array of 1, the intercepts
+icp = np.ones(3).reshape(-1,1)
+
+# append the independent variables to the intercept
+tt = np.append(icp, bb, axis = 1)
+
+# the normal equation is A.T * A * x = A.T * b
 ttTtt = tt.T@tt
 ttTb = tt.T@b
 
-
+# append the results
 ttaa = np.append(ttTtt, ttTb.reshape(-1,1), axis=1)
+
+# find the LU decomposition
 p, l, u = lu(ttaa)
 
-
+# use the Lowe triangle to extract the beta-coefficients
 result = []
 for i in range(len(l)):
     result.append(l[i][0])
 
-
+# create
 parameter_symbols = [rf'$\beta_{b}$' for b in range(len(result))]
+
+df = pd.DataFrame([result],index = ["Results"], columns=parameter_symbols)
+
+print(df)
