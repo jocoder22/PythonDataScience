5ed868f8fc37f2e3252ed2ef31dd0348ac34c4dc
jocoder22 <okigbookey@gmail.com>
2021-06-01 19:49:19 -0400

    Update

--- datavisualization/candlestick.py
+++ datavisualization/candlestick.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import numpy as np
 import pandas as pd
 import matplotlib.ticker as mticker
 import mplfinance as mpf
@@ -12,11 +13,63 @@ from datetime import datetime, date
 # aapl = yf.download('BABA', '2020-1-1','2021-2-18')
 
 
-symbol = 'BABA' # 'AAPL' #'AMZN'  
+symbol = "ZV" #'BABA' # 'AAPL' #'AMZN'  
 starttime = datetime(2021, 1, 1)
 endtime = date.today()
 aapl = pdr.get_data_yahoo(symbol, starttime, endtime)
 print(aapl.head())
+
+
+d = [12,26,9]
+w = [5,35,5]
+
+dayy = False
+if dayy == True:
+    dd = d
+else: dd = w
+
+macd = aapl.Close.ewm(span=dd[0]).mean() - aapl.Close.ewm(span=dd[1]).mean()
+signal = macd.ewm(span=dd[2]).mean()
+aapl['macd_diff'] = macd - signal
+aapl['MACD'] = macd 
+aapl['MACDsig'] = signal
+
+
+fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize =(24,10))
+ax1.grid(alpha=0.7); ax2.grid(alpha=0.7)
+ax1.set_title("Candlestick"); ax2.set_title("MACD")
+
+color = ["green" if close_price > open_price else "red" for close_price, open_price in zip(aapl.Close, aapl.Open)]
+ax1.bar(x=aapl.index, height=np.abs(aapl.Open-aapl.Close), bottom=np.min((aapl.Open,aapl.Close), axis=0), width=0.6, color=color)
+ax1.bar(x=aapl.index, height=aapl.High - aapl.Low, bottom=aapl.Low, width=0.1, color=color)
+
+# plt.title(f'MACD chart {symbol}')
+color2 = ["green" if close_price > open_price else "red" for close_price, open_price in zip(aapl.MACD, aapl.MACDsig)]
+ax2.plot( aapl['MACD'], label='MACD')
+ax2.plot( aapl['MACDsig'], label='MACDsig')
+# ax2.plot( aapl['macd_diff'],  label='MACDhist')
+ax2.bar( aapl.index, aapl['macd_diff'], snap=False, color = color2, width=0.6, label='MACDhist')
+ax2.legend()
+
+plt.show()
+
+
+def candlestick(t, o, h, l, c):
+    plt.figure(figsize=(12,4))
+    color = ["green" if close_price > open_price else "red" for close_price, open_price in zip(c, o)]
+    plt.bar(x=t, height=np.abs(o-c), bottom=np.min((o,c), axis=0), width=0.6, color=color)
+    plt.bar(x=t, height=h-l, bottom=l, width=0.1, color=color)
+
+candlestick(
+    aapl.index,
+    aapl.Open,
+   aapl.High,
+   aapl.Low,
+   aapl.Close
+)
+
+plt.grid(alpha=0.9)
+plt.show()
 
 # mpf.plot(aapl)
 mpf.plot(aapl, type='candle')
@@ -45,11 +98,8 @@ fplt.show()
 fplt.background = "#fff"
 ax, ax2 = fplt.create_plot('Apple MACD', rows=2)
 fplt.background = "#fff"
-# plot macd with standard colors first
-macd = aapl.Close.ewm(span=12).mean() - aapl.Close.ewm(span=26).mean()
-signal = macd.ewm(span=9).mean()
-aapl['macd_diff'] = macd - signal
 
+# plot macd with standard colors first
 fplt.background = "#fff"
 fplt.volume_ocv(aapl[['Open','Close','macd_diff']], ax=ax2, colorfunc=fplt.strength_colorfilter)
 fplt.background = "#fff"
@@ -73,3 +123,14 @@ fplt.show()
 
 
 # https://pypi.org/project/finplot/
+
+aapl['MACD'] = macd 
+aapl['MACDsig'] = signal
+plt.title(f'MACD chart {symbol}')
+plt.plot( aapl['MACD'].fillna(0), label='MACD')
+plt.plot( aapl['MACDsig'].fillna(0), label='MACDsig')
+plt.plot( aapl['macd_diff'].fillna(0), label='MACDhist')
+plt.bar( aapl.index, aapl['macd_diff'].fillna(0), width=0.1, snap=False, label='MACDhist')
+plt.legend()
+
+plt.show()
