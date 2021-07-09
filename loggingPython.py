0be69b04d13388064c753d760b68788b3a2f4176
Okechukwu Okigbo <okigbookey@gmail.com>
2021-07-09 11:44:41 -0400

    Update loggingPython.py

--- loggingPython.py
+++ loggingPython.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def xtimesPower(x,r,n):
+  """ Compute the x times n to power r
+  
+  """
+  return x * n ** r
+
 
