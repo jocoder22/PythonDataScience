a6397016b4dc5cdc03e2e19447c6395b6d56525c
Okechukwu Okigbo <okigbookey@gmail.com>
2021-07-09 11:59:33 -0400

    Update loggingPython.py

--- loggingPython.py
+++ loggingPython.py
@@ -4,10 +4,10 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def xtimesPower(x,r,n):
+def xtimesPower(x, n, p):
   """ Compute the x times n to power r
   
   """
-  return x * n ** r
+  return x * n ** p
 
-
+logging.info(f"The result of {3} times {4} to the {5}th power is {xtimesPower(3, 4, 5)}")
