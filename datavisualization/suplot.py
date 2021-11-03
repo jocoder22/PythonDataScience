20b64a48e8b9d4fff81d419c717818e3177d5759
jocoder22 <okigbookey@gmail.com>
2021-11-03 00:37:48 -0400

    Changes to be committed:  	new file:   suplot.py

---
+++ datavisualization/suplot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import numpy as np 
+import matplotlib.pyplot as plt
+# plt.rc('figure', figsize=[10,8], dpi=200)
+# plt.rcParams.keys()
+
+# plt.ion()
+
+x1 = np.arange(0, 4, 0.1)
+plt.plot(x1, x1, label='Linear')
+plt.plot(x1, np.sin(x1), label='sin(x)')
+plt.plot(x1, x1**2, label='Quadratic')
+plt.plot(x1, np.cos(x1), label='cos(x)')
+
+
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.title('My graphic display I')
+plt.legend()
+
+plt.show()
+plt.close()
+
+# Ploting multiple plots: using subplot
+groups = ['group1', 'group2', 'group3']
+sales = [590, 800, 5600]
+members = [13, 25, 100]
+totalHr = [214, 689, 3280]
+
+plt.figure(1, figsize=(10.5, 4), facecolor='green',
+           edgecolor='r')
+plt.subplot(231)
+plt.bar(groups, sales)
+plt.subplot(232)
+plt.scatter(groups, members)
+plt.subplot(233)
+plt.plot(groups, totalHr, 'r^')
+plt.subplot(234)
+plt.plot(x1, x1**3, color='orange', linestyle='-.')
+plt.subplot(2,3,5)
+plt.plot(x1, x1**4, color='cyan', linestyle=':', marker='.')
+plt.subplot(236)
+plt.plot(x1, x1**5)
+
+plt.show()
