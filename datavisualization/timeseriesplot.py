8c595f2073b59fd950502046adbac9137b58e47a
jocoder22 <okigbookey@gmail.com>
2021-11-03 01:32:35 -0400

    Update

--- datavisualization/timeseriesplot.py
+++ datavisualization/timeseriesplot.py
@@ -9,4 +9,13 @@ symbols = "KO XOM T VOD JPM C NVDA".split()
 
 assets =  yf.download(tickers = symbols[0], start="2020-01-01", auto_adjust = True, progress=False)
 
-print(assets.head())
+print(assets.head(), assets.index)
+
+fig, axes = plt.subplots(2, 1, figsize=(14, 8), sharex=True,
+                gridspec_kw={'height_ratios': [3, 1]}, tight_layout = {"pad":3})
+# fig.tight_layout(pad=3)
+
+axes[0].plot(assets.index, assets["Close"], color='blue', linewidth=2, label='Price')
+axes[1].bar(assets.index, assets["Volume"], color='darkgrey', width=3, label='Volume')
+plt.show()
+
